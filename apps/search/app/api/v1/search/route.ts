b5772d41e6e45fc622e9da74de3e49dc779ef5ad
proemian <brian@proemial.ai>
2023-11-27 16:43:30 +0100

    Add telemetry

--- apps/search/app/api/v1/search/route.ts
+++ apps/search/app/api/v1/search/route.ts
@@ -2,30 +2,41 @@ import { NextResponse } from "next/server";
 import { OpenAlexPaper } from "@/app/api/v1/search/oapaper";
 
 export const runtime = "edge";
+
+// ,publication_date:>2023-10-16,publication_date:<2023-11-16
 const baseUrl = "https://api.openalex.org/works?filter=is_oa:true";
 
 export async function POST(req: Request) {
-  const { q } = await req.json();
+  const begin = DateMetrics.now();
 
-  // ,publication_date:>2023-10-16,publication_date:<2023-11-16
-  const query = `${baseUrl},abstract.search:${encodeURIComponent(q)}`;
-  console.log("api/v1/search", q);
+  try {
+    const { q } = await req.json();
+    const query = `${baseUrl},abstract.search:${encodeURIComponent(q)}`;
 
-  const response = await fetchSomeResults<OpenAlexPaper>(query, 3, (o) => ({
-    title: o.title,
-    abstract: fromInvertedIndex(o.abstract_inverted_index),
-  }));
+    const response = await fetchSomeResults<OpenAlexPaper>(query, 3, (o) => ({
+      title: o.title,
+      abstract: fromInvertedIndex(o.abstract_inverted_index),
+    }));
 
-  return NextResponse.json(response);
+    return NextResponse.json(response);
+  } finally {
+    Log.metrics(begin, "api/v1/search");
+  }
 }
 
 function fromInvertedIndex(index: { [key: string]: number[] }) {
-  const tokens = [] as string[];
+  const begin = DateMetrics.now();
 
-  // @ts-ignore
-  Object.keys(index).forEach((k) => index[k].forEach((a) => (tokens[a] = k)));
+  try {
+    const tokens = [] as string[];
 
-  return tokens.join(" ");
+    // @ts-ignore
+    Object.keys(index).forEach((k) => index[k].forEach((a) => (tokens[a] = k)));
+
+    return tokens.join(" ");
+  } finally {
+    Log.metrics(begin, "fromInvertedIndex");
+  }
 }
 
 async function fetchSomeResults<T>(
@@ -33,15 +44,38 @@ async function fetchSomeResults<T>(
   count: number,
   selector?: (item: T) => object,
 ) {
-  const response = await fetchJson<{ results: Array<T> }>(query);
-  const some = response.results.slice(0, count);
+  const begin = DateMetrics.now();
 
-  return !selector ? some : some.map(selector);
+  try {
+    const response = await fetchJson<{ results: Array<T> }>(query);
+    const some = response.results.slice(0, count);
+
+    return !selector ? some : some.map(selector);
+  } finally {
+    Log.metrics(begin, "fetchSomeResults");
+  }
 }
 
 async function fetchJson<T>(url: string) {
-  const response = await fetch(url);
-  const json = await response.json();
+  const begin = DateMetrics.now();
 
-  return json as T;
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+
+    return json as T;
+  } finally {
+    Log.metrics(begin, "fetchJson");
+  }
 }
+
+const Log = {
+  metrics: (begin: number, message: string) => {
+    console.log(`[${DateMetrics.elapsed(begin)}] ${message}`);
+  },
+};
+
+const DateMetrics = {
+  now: () => new Date().getTime(),
+  elapsed: (begin: number) => DateMetrics.now() - begin,
+};
