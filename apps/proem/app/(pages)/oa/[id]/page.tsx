f3d90a133f93e20205df5ec2564686617108ecfb
proemian <brian@proemial.ai>
2023-12-07 17:44:14 +0100

    Add opengraph asset

---
+++ apps/proem/app/(pages)/oa/[id]/page.tsx
@@ -0,0 +1,42 @@
+type Props = {
+  params: { id: string };
+  searchParams: { text?: string };
+};
+
+export async function generateMetadata({ params, searchParams }: Props) {
+  const title = `Proem - ${params.id}`;
+
+  let description = searchParams.text;
+  // if(!description) {
+  //     const { text } = await PapersDao.getGptSummary(params.id, 'sm');
+  //     const {sanitized} = sanitize(text);
+  //     description = sanitized;
+  // }
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: "article",
+      images: [
+        {
+          url: `/api/og/${params.id}?text=${description}`,
+          width: 400,
+          height: 200,
+          alt: description,
+        },
+      ],
+    },
+  };
+}
+
+export default async function ReaderPage({ params, searchParams }: Props) {
+  return (
+    <div>
+      <div>{params.id}</div>
+      <div>{searchParams.text}</div>
+    </div>
+  );
+}
---
+++ apps/proem/app/api/og/[id]/route.tsx
@@ -0,0 +1,69 @@
+import { ImageResponse } from "next/og";
+import assetImg1 from "@/assets/asset-bg-1.png";
+import assetImg2 from "@/assets/asset-bg-2.png";
+import assetImg3 from "@/assets/asset-bg-3.png";
+
+export const runtime = "edge";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  try {
+    const url = new URL(request.url);
+    const { searchParams } = new URL(url);
+    const text = searchParams.has("text")
+      ? searchParams.get("text")
+      : "placeholder";
+
+    const fontData = await fetch(
+      new URL("@/assets/AnekMalayalam-SemiBold.ttf", import.meta.url),
+    ).then((res) => res.arrayBuffer());
+
+    return new ImageResponse(
+      (
+        <div
+          tw="flex flex-col w-full h-full items-center justify-center text-white"
+          style={{
+            backgroundImage: `url("${url.protocol}//${url.host}${image(
+              params.id,
+            )}")`,
+            backgroundSize: "cover",
+            backgroundRepeat: "none",
+            fontFamily: "AnekMalayalam",
+          }}
+        >
+          <div
+            tw="flex w-full md:items-center p-8 text-left text-2xl"
+            style={{
+              textShadow: "rgb(255, 102, 255) 0px 0px 4px",
+              // overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis',
+            }}
+          >
+            {text}
+          </div>
+        </div>
+      ),
+      {
+        width: 400,
+        height: 200,
+        fonts: [
+          {
+            name: "AnekMalayalam",
+            data: fontData,
+            style: "normal",
+          },
+        ],
+      },
+    );
+  } catch (e: any) {
+    return new Response("Failed to generate image", { status: 500 });
+  }
+}
+
+function image(id: string) {
+  const lastNum = Number(id.charAt(id.length - 1));
+  if (lastNum < 3) return assetImg1.src;
+  if (lastNum < 6) return assetImg2.src;
+  return assetImg3.src;
+}
