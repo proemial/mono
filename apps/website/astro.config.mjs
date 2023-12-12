1b952513c91dbd350505c01e0caad5fa73a708d7
proemian <brian@proemial.ai>
2023-12-12 10:34:24 +0100

    remove output: 'server'

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-    output: 'server',
     adapter: vercel({
         webAnalytics: {
             enabled: true,
