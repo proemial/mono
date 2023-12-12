9c15a9be8a2ab8e80b1d7f55c0ef0a93bd57207e
proemian <brian@proemial.ai>
2023-12-12 10:36:29 +0100

    change to output: 'hybrid'

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
+    output: 'hybrid',
     adapter: vercel({
         webAnalytics: {
             enabled: true,
