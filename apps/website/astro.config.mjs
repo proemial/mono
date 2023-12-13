c10e86f830621d6c16c7c6abf8aaafcddabe07e8
Runi Ellingsgaard <runi@usestudio.dk>
2023-12-13 11:37:09 +0100

    Trying to make the email capture with serverless functions, went on to install react to try to follow a guide from the astro docs - went on to just making a simple fetch post with vanilla js and getting a cors error.

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
-    output: 'hybrid',
-    adapter: vercel({
-        webAnalytics: {
-            enabled: true,
-        },
-    }),
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  }),
+  integrations: [react()]
 });
