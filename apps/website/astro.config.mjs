430659b5733da8cc913e350b4da9ca56576ccaa1
proemian <brian@proemial.ai>
2023-12-14 13:50:35 +0100

    Reenable analytics

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server'
+  output: 'hybrid',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
