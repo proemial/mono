4b953ddc2faa141d6f27f463d78d5fefd838bec7
Rasmus Hjulskov <hello@rasmushjulskov.com>
2023-12-18 14:50:31 +0100

    feat(proem): remove cache from wip neon insert

--- apps/proem/app/api/neon/post/route.ts
+++ apps/proem/app/api/neon/post/route.ts
@@ -1,11 +1,15 @@
 import { db } from "@proemial/database/db";
 import { testTable } from "@proemial/database/schema/test";
+import { unstable_noStore as noStore } from "next/cache";
 
 export async function GET() {
+  noStore();
   const number = Math.floor(Math.random() * 10000);
   const result = await db
     .insert(testTable)
-    .values({ name: "test", id: number, value: number });
+    .values({ name: "test", id: number, value: number })
+    .onConflictDoNothing()
+    .returning();
 
   return Response.json({ result });
 }
