6412c057e509f6a9cc8fa19655c8bf6463304a7a
Rasmus Hjulskov <hello@rasmushjulskov.com>
2023-12-19 12:58:13 +0100

    feat: corrected dependencies between projects

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from "drizzle-kit";
 import dotenv from "dotenv";
-dotenv.config({ path: "apps/proem/.env.local" });
+// TODO! currently tightly coupled with apps/proem
+dotenv.config({ path: "../../apps/proem/.env.local" });
 
 export default {
-  schema: "packages/database/schema/*",
-  out: "packages/database/drizzle",
+  schema: "./schema/*",
+  out: "./drizzle",
   driver: "pg",
   dbCredentials: {
     connectionString: `${process.env.DATABASE_URL!}?sslmode=require`,
