52ceea2b65f3bef9711ef74df817c8d57f313fd0
Rasmus Hjulskov <hello@rasmushjulskov.com>
2023-12-20 21:19:53 +0100

    fix redirect url and rely on that as state for wether the drawer is open or not

--- apps/proem/app/utils/url.ts
+++
@@ -1,7 +0,0 @@
-export function getLocation() {
-  const isBrowser = () => typeof window !== "undefined";
-  const location =
-    isBrowser() && window.location.pathname + window.location.search;
-
-  return location ? location : "/";
-}
