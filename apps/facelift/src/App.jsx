7c46e15ee50657945a67795179b48e0e53fe9ab2
proemian <brian@proemial.ai>
2023-12-20 15:35:16 +0100

    fix(facelift): Show feed

--- apps/facelift/src/App.jsx
+++ apps/facelift/src/App.jsx
@@ -1,27 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import '../src/components/component-styles.css'
-import FeedView from './components/FeedView'
-import HubView from './components/HubView'
-import DesktopSidebar from './components/DesktopSidebar'
-import DesktopNotificationWidget from './components/DesktopNotificationWidget'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "../src/components/component-styles.css";
+import FeedView from "./components/FeedView";
+import HubView from "./components/HubView";
+import DesktopSidebar from "./components/DesktopSidebar";
+import DesktopNotificationWidget from "./components/DesktopNotificationWidget";
 
 function App() {
-  const [showFeed, setShowFeed] = useState(false);  
+  const [showFeed, setShowFeed] = useState(true);
   return (
     <>
-    <DesktopSidebar></DesktopSidebar>
-    <DesktopNotificationWidget></DesktopNotificationWidget>
-    {showFeed ? (
-      <FeedView></FeedView>
-    ): <HubView></HubView> }
-      
-      
+      <DesktopSidebar></DesktopSidebar>
+      <DesktopNotificationWidget></DesktopNotificationWidget>
+      {showFeed ? <FeedView></FeedView> : <HubView></HubView>}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
