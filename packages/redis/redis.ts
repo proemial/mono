0161841423b86a044c793ba28cf31506cd7a779b
proemian <brian@proemial.ai>
2023-12-24 07:17:42 +0100

    fix(proemial): workaround for weird typing issue during build

--- packages/redis/redis.ts
+++ packages/redis/redis.ts
@@ -1,4 +1,7 @@
 import { OpenAlexPapers } from "./adapters/papers";
+// Unused import fixes `Open
+// The inferred type of "X" cannot be named without a reference to "Y". This is likely not portable. A type annotation is necessary.`
+import { OpenAlexPaper } from "@proemial/models/open-alex";
 
 export const Redis = {
   papers: OpenAlexPapers,
