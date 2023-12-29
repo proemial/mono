be606c8c75131eed401dbf4a0b4b9c00a58a750d
proemian <brian@proemial.ai>
2023-12-29 12:43:04 +0100

    hack(proem): toggle drawer

--- apps/proem/app/components/login/state.ts
+++ apps/proem/app/components/login/state.ts
@@ -1,17 +1,22 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
 export type DrawerState = {
-  isOpen: boolean,
-  setOpen: (open: boolean) => void,
-  toggle: () => void,
-  open: () => void,
-  close: () => void,
+  isOpen: boolean;
+  setOpen: (open: boolean) => void;
+  toggle: () => void;
+  open: () => void;
+  close: () => void;
 };
 
-export const useDrawerState = create<DrawerState>(set => ({
+export const useDrawerState = create<DrawerState>((set) => ({
   isOpen: false,
-  setOpen: (isOpen) => set(() => ({isOpen})),
-  toggle: () => set((state) => ({isOpen: !state.isOpen})),
-  open: () => set(() => ({isOpen: true})),
-  close: () => set(() => ({isOpen: false})),
-}))
+  setOpen: (isOpen) => set(() => ({ isOpen })),
+  toggle: () => set((state) => ({ isOpen: !state.isOpen })),
+  open: () => {
+    const searchParams = new URLSearchParams(window.location.search);
+    searchParams.set("redirect_url", window.location.toString());
+    window.location.search = searchParams.toString();
+    set(() => ({ isOpen: true }));
+  },
+  close: () => set(() => ({ isOpen: false })),
+}));
