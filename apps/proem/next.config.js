f16392b5f0ad713b1bc998f37cbdc7fed6ea7196
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-01-10 20:20:03 +0100

    add rewrite for gpt.proem.ai

--- apps/proem/next.config.js
+++ apps/proem/next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/utils"],
+  async rewrites() {
+    return {
+      beforeFiles: [
+        // if the host is `app.acme.com`,
+        // this rewrite will be applied
+        {
+          source: "/:path*",
+          has: [
+            {
+              type: "host",
+              value: "gpt.proem.ai",
+            },
+          ],
+          destination: "https://chat.openai.com/g/g-SxYGBAT0I-science-proems",
+        },
+      ],
+    };
+  },
 };
