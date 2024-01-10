558a79195f67e796d3016ce9a10ee8fce616a8d3
Malthe Hartmann <malthart@icloud.com>
2024-01-10 20:10:10 +0100

    Dirty Merge

--- apps/proem/app/(pages)/oa/[id]/components/panels/bot/bot.tsx
+++ apps/proem/app/(pages)/oa/[id]/components/panels/bot/bot.tsx
@@ -98,7 +98,7 @@ export function InsightsBot({ paper, suggestions }: Props) {
           </div>
         </TabsContent>
         <TabsContent value="authors">
-          <div className="bg-[#2F2F2F] flex flex-col leading-snug gap-3 mb-2 py-4 px-4 rounded-sm border border-[#3C3C3C] self-end">
+          {/* <div className="bg-[#2F2F2F] flex flex-col leading-snug gap-3 mb-2 py-4 px-4 rounded-sm border border-[#3C3C3C] self-end">
             <div className="flex flex-row items-center gap-3">
               <Image
                 src="/avatars/sara.png"
@@ -126,7 +126,7 @@ export function InsightsBot({ paper, suggestions }: Props) {
                 </p>
               </div>
             </div>
-          </div>
+          </div> */}
 
           {/* TODO! For Rasmus: Render out a list of authors with this styling inside of
           the div with bg - (2F2F2F) */}
