0a336904428b248f839099fb8ba5325da34a8a29
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-01-10 22:43:56 +0100

    add track event to search papers api

--- apps/search/app/api/v2/search/route.ts
+++ apps/search/app/api/v2/search/route.ts
@@ -1,5 +1,6 @@
 import { Time } from "@proemial/utils/time";
 import { fetchPapers } from "@/app/api/v2/search/search";
+import { track } from "@vercel/analytics/server";
 import { NextResponse } from "next/server";
 
 export const runtime = "edge";
@@ -10,6 +11,11 @@ export async function POST(req: Request) {
   const begin = Time.now();
   try {
     const response = await fetchPapers(q, count, tokens);
+    await track("api:search-papers", {
+      q,
+      count,
+      tokens,
+    });
 
     return NextResponse.json(response);
   } finally {
