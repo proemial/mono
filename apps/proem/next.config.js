3ba96bde28055f6c5c4489366998bc2eb982d848
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-01-10 21:18:01 +0100

    Remove commented out code for rewrites in next.config.js

--- apps/proem/next.config.js
+++ apps/proem/next.config.js
@@ -1,22 +1,22 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/utils"],
-  async rewrites() {
-    return {
-      beforeFiles: [
-        // if the host is `app.acme.com`,
-        // this rewrite will be applied
-        {
-          source: "/:path*",
-          has: [
-            {
-              type: "host",
-              value: "(?<gpt>.*)\\..*",
-            },
-          ],
-          destination: "https://chat.openai.com/g/g-SxYGBAT0I-science-proems",
-        },
-      ],
-    };
-  },
+  // async rewrites() {
+  //   return {
+  //     beforeFiles: [
+  //       // if the host is `app.acme.com`,
+  //       // this rewrite will be applied
+  //       {
+  //         source: "/:path*",
+  //         has: [
+  //           {
+  //             type: "host",
+  //             value: "(?<gpt>.*)\\..*",
+  //           },
+  //         ],
+  //         destination: "https://chat.openai.com/g/g-SxYGBAT0I-science-proems",
+  //       },
+  //     ],
+  //   };
+  // },
 };
