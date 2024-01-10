62b02b30af902a94a9d4f0fe993cf02621f3d5a7
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-01-10 23:10:36 +0100

    move gpt subdomain from rewrite to redirect

--- apps/proem/next.config.js
+++ apps/proem/next.config.js
@@ -2,19 +2,18 @@
 module.exports = {
   transpilePackages: ["@repo/utils"],
   async redirects() {
-    return {
-      beforeFiles: [
-        {
-          source: "/:path*",
-          has: [
-            {
-              type: "host",
-              value: "gpt.proem.ai",
-            },
-          ],
-          destination: "https://chat.openai.com/g/g-SxYGBAT0I-science-proems",
-        },
-      ],
-    };
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "gpt.proem.ai",
+          },
+        ],
+        destination: "https://chat.openai.com/g/g-SxYGBAT0I-science-proems",
+        permanent: false,
+      },
+    ];
   },
 };
