ddb5ea50cc25bf38a44feb4128c48fc5d855c6c0
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-01-10 22:31:13 +0100

    enable vercel analytics on search

--- apps/search/app/layout.tsx
+++ apps/search/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import { ReactNode } from "react";
 
@@ -5,6 +6,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
