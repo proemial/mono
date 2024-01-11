ae5f59f733c64353c09d135833e8a8036310e659
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-01-11 11:50:05 +0100

    Update search route to validate API key

--- apps/search/app/api/v2/search/route.ts
+++ apps/search/app/api/v2/search/route.ts
@@ -2,13 +2,18 @@ import { Time } from "@proemial/utils/time";
 import { fetchPapers } from "@/app/api/v2/search/search";
 import { track } from "@vercel/analytics/server";
 import { NextResponse } from "next/server";
+import { Env } from "@proemial/utils/env";
 
 export const runtime = "edge";
 
 export async function POST(req: Request) {
   const { q, count, tokens } = await req.json();
-  const auth = req.headers.get("authorization");
-  console.log({ auth });
+  const apiKey = req.headers.get("authorization");
+  console.log({ auth: apiKey });
+
+  if (apiKey !== `Basic ${Env.get("GPT_API_KEY")}`) {
+    console.log("auth failed");
+  }
 
   const begin = Time.now();
   try {
