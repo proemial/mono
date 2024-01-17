2081ac363c4e237f5d35bce5c1a281f0857ea123
Jon Sorensen <jon@proemial.ai>
2024-01-17 09:59:02 +0100

    feat(feed): fetch latest papers dynamically based on OA's `created_date`

--- apps/proem/app/(pages)/oa/[id]/fetch-paper.tsx
+++ apps/proem/app/(pages)/oa/[id]/fetch-paper.tsx
@@ -5,8 +5,13 @@ import {
   openAlexFields,
   OpenAlexPaper,
   OpenAlexWorkMetadata,
+  OpenAlexWorksHit,
+  OpenAlexWorksSearchResult,
 } from "@proemial/models/open-alex";
 import { fromInvertedIndex } from "@proemial/utils/string";
+import { fetchJson } from "@proemial/utils/fetch";
+import { Env } from "@proemial/utils/env";
+import dayjs from "dayjs";
 
 export const fetchPaper = cache(async (id: string): Promise<OpenAlexPaper> => {
   console.log("fetchPaper");
@@ -32,3 +37,32 @@ export const fetchPaper = cache(async (id: string): Promise<OpenAlexPaper> => {
   }
   return paper;
 });
+
+export const fetchLatestPaperIds = async (): Promise<string[]> => {
+  const today = dayjs().format('YYYY-MM-DD')
+  const twoWeeksAgo = dayjs(today).subtract(2, 'week').format('YYYY-MM-DD')
+  const select = [
+    'id',
+    'publication_date',
+  ].join(',')
+  const filter = [
+    'type:article',
+    'has_abstract:true',
+    `from_created_date:${twoWeeksAgo}`,
+    `publication_date:>${twoWeeksAgo}`, // We do not want old papers that were added recently
+    `publication_date:<${today}`,       // We do not want papers published in the future
+  ].join(',')
+  const sort = 'from_created_date:desc'
+  const oaApiKey = Env.get('OPENALEX_API_KEY')
+  // This will include 25 paper IDs (one pagination page), which seems appropriate
+  // for a feed.
+  const oaPapers = await fetchJson<OpenAlexWorksSearchResult>(
+    `${baseOaUrl}?select=${select}&filter=${filter}&sort=${sort}&api_key=${oaApiKey}`,
+  );
+  return oaPapers?.results
+    .sort(sortByPublicationDateDesc)
+    .map((result) => result.id.replace('https://openalex.org/', '')) ?? []
+}
+
+const sortByPublicationDateDesc = (a: OpenAlexWorksHit, b: OpenAlexWorksHit) =>
+  b.publication_date.localeCompare(a.publication_date)
