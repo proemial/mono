9e485c21a5c03252464a9347e41c22dafe61b5a3
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-01-18 21:28:02 +0100

    Add convert-query-parameters.spec.ts and update convert-query-parameters.ts

--- apps/proem/app/api/bot/answer-engine/convert-query-parameters.ts
+++ apps/proem/app/api/bot/answer-engine/convert-query-parameters.ts
@@ -1,5 +1,7 @@
-export function convertToOASearchString() {
-  const query =
-    "title.search:(%22vaccines%22),abstract.search:(%22vaccines%22%20OR%20%22immunization%22%20OR%22vaccination%22%20OR%20%22cause%22%20OR%20%22lead%20to%22%20OR%20%22Autism%20Spectrum%20Disorder%22%20OR%20%22ASD%22)";
-  return query;
+export function convertToOASearchString(title: string, abstract: string[]) {
+  const query = `title.search:("${title}"),abstract.search:(${abstract
+    .map((abstract) => `"${abstract}"`)
+    .join(" OR ")})`;
+
+  return encodeURIComponent(query);
 }
