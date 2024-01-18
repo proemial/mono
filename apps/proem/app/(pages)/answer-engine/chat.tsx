4c0953c721788b661a2d95e789641b431c2a1e3f
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-01-18 16:57:52 +0100

    chain searchPapers + answer prompts to simulate the GPT instruction

--- apps/proem/app/(pages)/answer-engine/chat.tsx
+++ apps/proem/app/(pages)/answer-engine/chat.tsx
@@ -3,19 +3,48 @@
 import SearchInput from "@/app/(pages)/answer-engine/search-input";
 import { useChat } from "ai/react";
 
+const STARTERS = ["Do Vaccines Cause Autism Spectrum Disorder?"];
+// const markdown = '[sdfsdfsdf]("/")';
+
+const html = `<a href="/">test link</a>`;
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat({
+  const {
+    messages,
+    input,
+    handleInputChange,
+    handleSubmit,
+    append,
+    isLoading,
+  } = useChat({
     api: "/api/bot/answer-engine",
   });
 
+  const showStarters = messages.length === 0;
+
   return (
-    <div className="relative flex flex-col w-full max-w-md py-24 mx-auto stretch">
+    <div className="relative flex flex-col w-full max-w-md py-24 mx-auto prose stretch">
+      {/* <div>{`<a href="/">test link</a>`}</div> */}
       {messages.map((m) => (
-        <div key={m.id}>
-          {m.role === "user" ? "User: " : "AI: "}
-          {m.content}
-        </div>
+        <div
+          className="prose prose-invert"
+          key={m.id}
+          dangerouslySetInnerHTML={{ __html: m.content }}
+        />
       ))}
+
+      {isLoading ? <span> ... is loading </span> : null}
+
+      {showStarters
+        ? STARTERS.map((starter) => (
+            <button
+              onClick={() => {
+                append({ role: "user", content: starter });
+              }}
+            >
+              {starter}
+            </button>
+          ))
+        : null}
 
       <SearchInput
         handleSubmit={handleSubmit}
--- apps/proem/app/api/bot/answer-engine/route.ts
+++ apps/proem/app/api/bot/answer-engine/route.ts
@@ -1,16 +1,18 @@
+import { StreamingTextResponse, Message as VercelChatMessage } from "ai";
 import { NextRequest } from "next/server";
-import { Message as VercelChatMessage, StreamingTextResponse } from "ai";
 
-import { ChatOpenAI } from "langchain/chat_models/openai";
+import { convertToOASearchString } from "@/app/api/bot/answer-engine/convert-query-parameters";
+import { fetchPapers } from "@/app/api/paper-search/search";
 import { BytesOutputParser } from "@langchain/core/output_parsers";
-import { PromptTemplate } from "@langchain/core/prompts";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
 import {
   RunnablePassthrough,
   RunnableSequence,
 } from "@langchain/core/runnables";
-import { StringOutputParser } from "@langchain/core/output_parsers";
-import { scienceAnswersGPTPrompt } from "@/app/prompts/science-answers-gpt";
-import { question } from "@/app/prompts/chat";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { JsonOutputFunctionsParser } from "langchain/output_parsers";
+import { z } from "zod";
+import { zodToJsonSchema } from "zod-to-json-schema";
 
 export const runtime = "edge";
 
@@ -22,6 +24,14 @@ const formatMessage = (message: VercelChatMessage) => {
   return `${message.role}: ${message.content}`;
 };
 
+const searchParametersSchema = z.object({
+  searchParameters: z
+    .string()
+    .array()
+    .describe("An string array of search parameters"),
+});
+
+// Format the messages
 /*
  * This handler initializes and calls a simple chain with a prompt,
  * chat model, and output parser. See the docs for more information:
@@ -32,51 +42,97 @@ export async function POST(req: NextRequest) {
   const body = await req.json();
   const messages = body.messages ?? [];
   const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
-  const currentMessageContent = messages[messages.length - 1].content;
+  const currentMessageContent = messages[messages.length - 1].content as string;
 
   /**
    * See a full list of supported models at:
    * https://js.langchain.com/docs/modules/model_io/models/
    */
   const model = new ChatOpenAI({
+    // TODO! Figure out different temperature settings?
     temperature: 0.8,
+    modelName: "gpt-4-1106-preview", // "gpt-3.5-turbo",
+    cache: true,
+    verbose: true,
   });
 
-  /**
-   * Chat models stream message chunks rather than bytes, so this
-   * output parser handles serialization and encoding.
-   */
-  const outputParser = new BytesOutputParser();
-  const prompt1 = PromptTemplate.fromTemplate(
-    `What is the city {person} is from? Only respond with the name of the city.`
-  );
-  const prompt2 = PromptTemplate.fromTemplate(
-    `What country is the city {city} in? Respond in {language}.`
-  );
+  const bytesOutputParser = new BytesOutputParser();
+  const jsonOutputFunctionsParser = new JsonOutputFunctionsParser();
 
-  const chain2 = prompt1.pipe(model).pipe(new StringOutputParser());
-
-  const combinedChain = RunnableSequence.from([
-    {
-      city: chain2,
-      language: (input) => input.language,
-    },
-    prompt2,
-    model,
-    new StringOutputParser(),
+  const searchQueryPrompt = ChatPromptTemplate.fromMessages([
+    [
+      "system",
+      `Based on the user question, construct an array of strings for each of the key concepts and verbs in the original user question, the most closely related scientific concept as well as two or three synonyms. Both the scientific concepts and synonyms should preferably be two-grams or longer.
+      For example, given the user question: "does smoking cause lung cancer", you would construct an string array in the following format, which includes the original terms from the question ["smoking", "cause", "cancer"] plus a few related scientific concepts and synonyms: ["smoking", "tobacco use", "nicotine exposure", "cause", "induce", "trigger", "lead to", "lung cancer", "lung malignancy", "lung neoplasm"].
+      Then, you should identify A SINGLE COMMON NOUN that is VERY likely to occur in the title of a research paper relevant to answering the original user question. For the above example, that could simply be "smoking". It is very important that you pick a SINGLE NOUN, not a noun phrase. Just ONE common word. No spaces or hyphens.
+      `,
+    ],
+    ["human", `{question}`],
   ]);
 
-  const chain = RunnableSequence.from([
-    scienceAnswersGPTPrompt,
-    model,
-    outputParser,
+  const searchQueryChain = RunnableSequence.from([
+    searchQueryPrompt,
+    model.bind({
+      functions: [
+        {
+          name: "searchParameters",
+          description: "An string array of search parameters",
+          parameters: zodToJsonSchema(searchParametersSchema),
+        },
+      ],
+      function_call: { name: "searchParameters" },
+    }),
+    jsonOutputFunctionsParser,
+    RunnablePassthrough.assign({
+      papers: async (input) => {
+        const query = convertToOASearchString();
+        const papers = await fetchPapers(query);
+        // TODO! This is quite hacky to do here
+        const papersWithRelativeLinks = papers?.map((paper) => ({
+          ...paper,
+          link: paper.link.replace("https://proem.ai", ""),
+        }));
+        return JSON.stringify(papersWithRelativeLinks);
+      },
+    }),
   ]);
 
-  const stream = await combinedChain.stream({
-    // chat_history: formattedPreviousMessages.join("\n"),
-    // input: currentMessageContent,
-    person: "Obama",
-    language: "English",
+  const chatPrompt = ChatPromptTemplate.fromMessages([
+    [
+      "system",
+      `You will provide conclusive answers to user questions, based on the following research articles:
+      {papers},
+      IMPORTANT: YOUR ANSWER MUST BE A SINGLE SHORT PARAGRAPH OF 40 WORDS OR LESS KEY PHRASES FORMATTED AS HYPERLINKS POINTING TO THE PAPERS.
+      THIS IS ESSENTIAL. KEEP YOUR ANSWERS SHORT AND WITH STATEMENTS THE USER KAN CLICK ON!
+      - Pick the two papers most relevant to the provided user question.
+      - rephrase relevant key findings from these papers using only layman's terminology, and without abstract academic concepts like 'researchers', 'authors', 'propose', or 'study', simply state the finding as a Key Assertion, without reservations or caveats. for example: "More tooth loss is associated with greater cognitive decline and dementia in elderly people."
+      - Then use these rephrased findings to construct a short answer in less than 40 words, with key phrases of the answer text as hyperlinks pointing to the papers, like this example:
+      """Yes. Smoking causes cancer. Studies show that cigarette smokers are <a href="https://proem.ai/oa/W4213460776">more likely to die from cancer</a> than non-smokers. Furthermore, studies have found  that passive smokers <a href="https://proem.ai/oa/W2004456560">have a higher risk of cardiovascular disease</a> than people never exposed to a smoking environment."""
+
+      - THE FOLLOWING THREE IMPORTANT RULES ARE ALL ABSOLUTELY ESSENTIAL AND YOU WILL BE PENALIZED SEVERELY IF THE ANSWER DOES NOT INCLUDE INLINE HYPERLINKS EXACTLY AS DESCRIBED BELOW:
+      - IMPORTANT: EVERY ANSWER MUST HAVE AT LEAST TWO HYPERLINKS POINTING TO THE EXACT FULL URLS OF PAPERS PROVIDED IN THE API RESPONSE. THIS IS ABSOLUTELY ESSENTIAL.
+      - IMPORTANT: ALWAYS PLACE HYPERLINKS ON AKEY PHRASE OF THREE TO SIX WORDS INSIDE THE ANSWER. THIS IS ABSOLUTELY ESSENTIAL. NEVER PLACE URLS AFTER THE ANSWER. NEVER EVER CREATE LINKS THAT LOOK LIKE FOOTNOTES. ALWAYS PLACE FULL URL LINKS INSIDE THE ANSWER.
+      - IMPORTANT: YOUR ANSWER MUST BE A SINGLE SHORT PARAGRAPH OF 40 WORDS OR LESS WITH HYPERLINKS ON TWO KEY PHRASES. THIS IS ESSENTIAL. KEEP YOUR ANSWERS SHORT AND SIMPLE!
+      `,
+    ],
+    ["human", `{question}`],
+  ]);
+  const chatAnswerChain = RunnableSequence.from([
+    RunnablePassthrough.assign({
+      // TODO! question shouldn't be injected but parsed through
+      question: () => currentMessageContent,
+    }),
+    chatPrompt,
+    model,
+    bytesOutputParser,
+  ]);
+
+  const conversationalAnswerEngineChain =
+    searchQueryChain.pipe(chatAnswerChain);
+
+  const stream = await conversationalAnswerEngineChain.stream({
+    chat_history: formattedPreviousMessages.join("\n"),
+    question: currentMessageContent,
   });
 
   return new StreamingTextResponse(stream);
