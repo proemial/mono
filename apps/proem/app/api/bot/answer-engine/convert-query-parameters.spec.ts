4ecfc2b4b0badaf03e36d92e8de8b59c5180b78d
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-01-18 22:25:53 +0100

    invoke first part of the chain to await the search query

--- apps/proem/app/api/bot/answer-engine/convert-query-parameters.spec.ts
+++ apps/proem/app/api/bot/answer-engine/convert-query-parameters.spec.ts
@@ -13,6 +13,7 @@ it("can convert title and abstracts to OA friendly query format", () => {
     ])
   ).toEqual(
     "title.search%3A(%22vaccines%22)%2Cabstract.search%3A(%22vaccines%22%20OR%20%22immunization%22%20OR%20%22vaccination%22%20OR%20%22cause%22%20OR%20%22lead%20to%22%20OR%20%22Autism%20Spectrum%20Disorder%22%20OR%20%22ASD%22)"
+    // Original flawed string from GPT for reference
     // "title.search:(%22vaccines%22),abstract.search:(%22vaccines%22%20OR%20%22immunization%22%20OR%22vaccination%22%20OR%20%22cause%22%20OR%20%22lead%20to%22%20OR%20%22Autism%20Spectrum%20Disorder%22%20OR%20%22ASD%22)"
   );
 });
--- apps/proem/app/api/bot/answer-engine/route.ts
+++ apps/proem/app/api/bot/answer-engine/route.ts
@@ -77,7 +77,7 @@ export async function POST(req: NextRequest) {
     ["human", `{question}`],
   ]);
 
-  const searchQueryChain = RunnableSequence.from([
+  const fetchPapersChain = RunnableSequence.from([
     searchQueryPrompt,
     model.bind({
       functions: [
@@ -91,24 +91,6 @@ export async function POST(req: NextRequest) {
       function_call: { name: "searchParameters" },
     }),
     jsonOutputFunctionsParser,
-    RunnablePassthrough.assign({
-      papers: async (input) => {
-        console.log(input);
-        const query = convertToOASearchString(
-          input.keyConcept,
-          input.relatedConcepts
-        );
-        console.log({ query });
-        const papers = await fetchPapers(query);
-        console.log(papers.length);
-        // TODO! This is quite hacky to do here
-        const papersWithRelativeLinks = papers?.map((paper) => ({
-          ...paper,
-          link: paper.link.replace("https://proem.ai", ""),
-        }));
-        return JSON.stringify(papersWithRelativeLinks);
-      },
-    }),
   ]);
 
   // const fetchPapersChain = RunnableSequence.from([])
@@ -121,7 +103,7 @@ export async function POST(req: NextRequest) {
       IMPORTANT: YOUR ANSWER MUST BE A SINGLE SHORT PARAGRAPH OF 40 WORDS OR LESS KEY PHRASES FORMATTED AS HYPERLINKS POINTING TO THE PAPERS.
       THIS IS ESSENTIAL. KEEP YOUR ANSWERS SHORT AND WITH STATEMENTS THE USER CAN CLICK ON!
       - Pick the two papers most relevant to the provided user question.
-      - Also summarise each of the selected papers into a "microtitle" of 20 words or less with the most significant finding as an engaging tweet capturing the minds of other researchers, using layman's terminology, and without mentioning abstract entities like 'you',  'researchers', 'authors', 'propose', or 'study' but rather stating the finding as a statement of fact, without reservations or caveats. for example: "More tooth loss is associated with greater cognitive decline and dementia in elderly people."
+      - Also summarise each of the selected papers into a "title" of 20 words or less with the most significant finding as an engaging tweet capturing the minds of other researchers, using layman's terminology, and without mentioning abstract entities like 'you',  'researchers', 'authors', 'propose', or 'study' but rather stating the finding as a statement of fact, without reservations or caveats. for example: "More tooth loss is associated with greater cognitive decline and dementia in elderly people."
       - Then use these summaries to construct a short answer in less than 40 words, with key phrases of the answer text as hyperlinks pointing to the papers, like this example:
       """Yes/No. Smoking causes/do not cause cancer. Studies show that cigarette smokers are <a href="https://proem.ai/oa/W4213460776?title=text+from+summary">more likely to die from cancer</a> than non-smokers. Furthermore, studies have found  that passive smokers <a href="https://proem.ai/oa/W2004456560?title=text+from+summary">hae a higher risk of cardiovascular disease</a> than people never exposed to a smoking environment."""
       - The links should be pointing to the returned proem links, with the generated "summaries" appended as a query string to the link
@@ -134,18 +116,35 @@ export async function POST(req: NextRequest) {
     ],
     ["human", `{question}`],
   ]);
-  const chatAnswerChain = RunnableSequence.from([
+  const conversationalAnswerEngineChain = RunnableSequence.from([
+    {
+      // invoking searchQuery chain to prevent streaming in the result
+      searchQuery: (input) => fetchPapersChain.invoke(input),
+      question: (input) => input.question,
+    },
     RunnablePassthrough.assign({
-      // TODO! question shouldn't be injected but parsed through
-      question: () => currentMessageContent,
+      papers: async (input) => {
+        console.log(input);
+        const query = convertToOASearchString(
+          input.searchQuery.keyConcept,
+          input.searchQuery.relatedConcepts
+        );
+        const papers = await fetchPapers(query);
+        // TODO! This is quite hacky to do here
+        const papersWithRelativeLinks = papers?.map((paper) => ({
+          ...paper,
+          link: paper.link.replace("https://proem.ai", ""),
+        }));
+        return JSON.stringify(papersWithRelativeLinks);
+      },
     }),
     chatPrompt,
     model,
     bytesOutputParser,
   ]);
 
-  const conversationalAnswerEngineChain =
-    searchQueryChain.pipe(chatAnswerChain);
+  // const conversationalAnswerEngineChain =
+  //   fetchPapersChain.pipe(chatAnswerChain);
 
   const stream = await conversationalAnswerEngineChain.stream({
     chat_history: formattedPreviousMessages.join("\n"),
