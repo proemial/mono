9582947d34961aa639ad95b7cc5aff8a5a8c1f76
Malthe Hartmann <malthart@icloud.com>
2024-01-18 14:46:31 +0100

    open-graph-css-changes

--- apps/proem/app/api/og/[id]/route.tsx
+++ apps/proem/app/api/og/[id]/route.tsx
@@ -2,6 +2,8 @@ import { ImageResponse } from "next/og";
 import assetImg1 from "@/assets/asset-bg-1.png";
 import assetImg2 from "@/assets/asset-bg-2.png";
 import assetImg3 from "@/assets/asset-bg-3.png";
+import assetGreen from "@/assets/bg-asset-green.png";
+import assetDark from "@/assets/bg-asset-dark.png";
 
 export const runtime = "edge";
 
@@ -18,7 +20,7 @@ export async function GET(
 
     const fontData = await fetch(
       new URL(
-        "@/assets/fonts/anek-malayalam/AnekMalayalam-SemiBold.ttf",
+        "@/assets/fonts/helvetica/Helvetica.ttf",
         import.meta.url
       )
     ).then((res) => res.arrayBuffer());
@@ -26,20 +28,20 @@ export async function GET(
     return new ImageResponse(
       (
         <div
-          tw="flex flex-col w-full h-full items-center justify-center text-white"
+          tw="flex flex-col w-full text-2xl py-23 px-32 h-full items-center justify-center text-white"
           style={{
             backgroundImage: `url("${url.protocol}//${url.host}${image(
               params.id
             )}")`,
             backgroundSize: "cover",
             backgroundRepeat: "none",
-            fontFamily: "AnekMalayalam",
+            fontFamily: "Helvetica",
           }}
         >
           <div
-            tw="flex w-full md:items-center p-8 text-left text-2xl"
+            tw="bg-[#2F2F2F] border border-[#3C3C3C] rounded-sm flex w-full md:items-center p-8 text-left leading-normal text-5xl"
             style={{
-              textShadow: "rgb(255, 102, 255) 0px 0px 4px",
+              // textShadow: "rgb(255, 102, 255) 0px 0px 4px",
               // overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis',
             }}
           >
@@ -48,13 +50,14 @@ export async function GET(
         </div>
       ),
       {
-        width: 400,
-        height: 200,
+        width: 1200,
+        height: 630,
         fonts: [
           {
-            name: "AnekMalayalam",
+            name: "Helvetica",
             data: fontData,
             style: "normal",
+
           },
         ],
       }
@@ -66,7 +69,7 @@ export async function GET(
 
 function image(id: string) {
   const lastNum = Number(id.charAt(id.length - 1));
-  if (lastNum < 3) return assetImg1.src;
-  if (lastNum < 6) return assetImg2.src;
-  return assetImg3.src;
+  if (lastNum < 3) return assetDark.src;
+  if (lastNum < 6) return assetDark.src;
+  return assetDark.src;
 }
