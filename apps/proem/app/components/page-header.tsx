8a5cc5a5e36f729c9a934cada888b251fd3c8e47
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-01-19 16:15:45 +0100

    Refactor PageHeader component

--- apps/proem/app/components/page-header.tsx
+++ apps/proem/app/components/page-header.tsx
@@ -1,18 +1,12 @@
 "use client";
 import { Proem } from "@/app/components/icons/brand/proem";
-import { LOGIN_REDIRECT_URL_PARAM_NAME } from "@/app/components/login/login-drawer";
-import { useDrawerState } from "@/app/components/login/state";
-import { User } from "lucide-react";
 import Link from "next/link";
 
-type Props = {
+type PageHeaderProps = {
   title?: string;
-  isLoggedIn?: boolean;
 };
 
-export function PageHeader({ title = "Proem", isLoggedIn = false }: Props) {
-  const { open } = useDrawerState();
-
+export function PageHeader({ title = "Proem" }: PageHeaderProps) {
   return (
     <>
       <div className="sticky top-0 z-50 bg-[#1A1A1A]">
@@ -23,24 +17,9 @@ export function PageHeader({ title = "Proem", isLoggedIn = false }: Props) {
               <span className="text-[16px] font-sans font-normal">{title}</span>
             </div>
           </Link>
-
         </div>
       </div>
       <hr className="mx-6 h-[1px] border-t-0 bg-white/10" />
     </>
   );
 }
-
-// {isLoggedIn ? (
-//   <Link href="/profile">
-//     <User />
-//   </Link>
-// ) : (
-//   <Link
-//     href={{ query: { [LOGIN_REDIRECT_URL_PARAM_NAME]: "/profile" } }}
-//     onClick={open}
-//   >
-//     {/* TODO! Improve styling */}
-//     Login
-//   </Link>
-// )}
