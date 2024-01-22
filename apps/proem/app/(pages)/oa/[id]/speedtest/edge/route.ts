d202b16782e0a85090e93869a9aa81fc3c2f5c42
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-01-22 15:03:13 +0100

    add speedtest to edge
    (cherry picked from commit 6751ebdffea06e57f9b5b6a9914800ea5fe8fc38)

--- apps/proem/app/(pages)/oa/[id]/speedtest/edge/route.ts
+++ apps/proem/app/(pages)/oa/[id]/speedtest/edge/route.ts
@@ -2,6 +2,8 @@ import { speedTestPaper } from "@/app/(pages)/oa/[id]/speedtest/speedTestPaper";
 
 import { unstable_noStore as noStore } from "next/cache";
 
+export const runtime = "edge";
+
 export async function GET(
   _request: Request,
   { params }: { params: { id: string } }
