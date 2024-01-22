6751ebdffea06e57f9b5b6a9914800ea5fe8fc38
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-01-22 15:03:13 +0100

    add speedtest to edge

---
+++ apps/proem/app/(pages)/oa/[id]/speedtest/speedTestPaper.ts
@@ -0,0 +1,43 @@
+import { db } from "@proemial/database/db";
+import { papers } from "@proemial/database/schema/paper";
+import { OpenAlexPaper } from "@proemial/models/open-alex";
+import { Redis } from "@proemial/redis/redis";
+import { eq } from "drizzle-orm";
+
+async function migrateRedisPaperToNeon(redisPaper: OpenAlexPaper) {
+  try {
+    await db.insert(papers).values({
+      id: redisPaper.id,
+      data: redisPaper.data,
+      datab: redisPaper.data,
+      generated: redisPaper.generated,
+    });
+  } catch (e) {
+    console.log("paper already exists in neon");
+    console.log(e);
+  }
+}
+
+export async function speedTestPaper(paperId: string) {
+  const redisTimerStart = Date.now();
+  const redisPaper = await Redis.papers.get(paperId);
+  const redisTimerEnd = Date.now();
+  const redisDuration = redisTimerEnd - redisTimerStart;
+
+  await migrateRedisPaperToNeon(redisPaper);
+
+  const neonTimerStart = Date.now();
+  const neonPaper = await db
+    .select()
+    .from(papers)
+    .where(eq(papers.id, paperId));
+  const neonTimerEnd = Date.now();
+  const neonDuration = neonTimerEnd - neonTimerStart;
+
+  return {
+    redisDuration,
+    neonDuration,
+    redis: redisPaper,
+    neon: neonPaper,
+  };
+}
