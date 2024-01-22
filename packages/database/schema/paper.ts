8b8e5ca271bb1d4c79185a47d2b59a7a8ea5c2c5
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-01-22 15:10:31 +0100

    streamlined test

--- packages/database/schema/paper.ts
+++ packages/database/schema/paper.ts
@@ -2,7 +2,6 @@ import { json, jsonb, pgTable, text } from "drizzle-orm/pg-core";
 
 export const papers = pgTable("papers", {
   data: json("data"),
-  datab: jsonb("datab"),
   id: text("id").notNull().primaryKey(),
   // TODO! add starters?
   generated: json("generated").default({ title: null }),
