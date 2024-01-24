a3d9ffcb2e6f3cc9cfb6d6bad9a9a44fbbabb1b5
Jon Sorensen <jon@proemial.ai>
2024-01-24 13:57:43 +0100

    refactor: improve answer engine search query prompt (#55)
    This commit improves the prompt for generating OA search parameters for better
    results with function calls. It also attempts to improve the conversation prompt
    slightly.

--- apps/proem/app/api/bot/answer-engine/route.ts
+++ apps/proem/app/api/bot/answer-engine/route.ts
@@ -24,18 +24,16 @@ const formatMessage = (message: VercelChatMessage) => {
   return `${message.role}: ${message.content}`;
 };
 
-const searchParametersSchema = z.object({
+const constructSearchParametersSchema = z.object({
   keyConcept: z
     .string()
-    .describe(
-      "A single common noun that is VERY likely to occur in the title of a research paper relevant to answering the original user question"
-    ),
+    .describe(`A single common noun that is VERY likely to occur in the title of
+    a research paper that contains the answer to the user's question.`),
   relatedConcepts: z
     .string()
     .array()
-    .describe(
-      "The most closely related scientific concepts as well as two or three synonyms for the key concept"
-    ),
+    .describe(`The most closely related scientific concepts as well as two or
+    three synonyms for the key concept. Preferrably two-grams or longer.`),
 });
 
 // Format the messages
@@ -69,10 +67,8 @@ export async function POST(req: NextRequest) {
   const searchQueryPrompt = ChatPromptTemplate.fromMessages([
     [
       "system",
-      `Based on the user question, construct an array of strings for each of the key concepts and verbs in the original user question, the most closely related scientific concept as well as two or three synonyms. Both the scientific concepts and synonyms should preferably be two-grams or longer.
-      For example, given the user question: "does smoking cause lung cancer", you would construct an string array in the following format, which includes the original terms from the question ["smoking", "cause", "cancer"] plus a few related scientific concepts and synonyms: ["smoking", "tobacco use", "nicotine exposure", "cause", "induce", "trigger", "lead to", "lung cancer", "lung malignancy", "lung neoplasm"].
-      Then, you should identify A SINGLE COMMON NOUN that is VERY likely to occur in the title of a research paper relevant to answering the original user question. For the above example, that could simply be "smoking". It is very important that you pick a SINGLE NOUN, not a noun phrase. Just ONE common word. No spaces or hyphens.
-      `,
+      `Construct a set of search parameters that can be used retrieve one or
+      more scientific research papers related to the user's question.`,
     ],
     ["human", `{question}`],
   ]);
@@ -82,13 +78,14 @@ export async function POST(req: NextRequest) {
     model.bind({
       functions: [
         {
-          name: "searchParameters",
-          description:
-            "Search parameters to fetch papers related to the users question",
-          parameters: zodToJsonSchema(searchParametersSchema),
+          name: "constructSearchParameters",
+          description: `A function to construct a set of search parameters to
+          retrieve one or more scientific research papers related to the user's
+          question.`,
+          parameters: zodToJsonSchema(constructSearchParametersSchema),
         },
       ],
-      function_call: { name: "searchParameters" },
+      function_call: { name: "constructSearchParameters" },
     }),
     jsonOutputFunctionsParser,
   ]);
@@ -98,21 +95,50 @@ export async function POST(req: NextRequest) {
   const chatPrompt = ChatPromptTemplate.fromMessages([
     [
       "system",
-      `You will provide conclusive answers to user questions, based on the following research articles:
-      {papers},
-      IMPORTANT: YOUR ANSWER MUST BE A SINGLE SHORT PARAGRAPH OF 40 WORDS OR LESS KEY PHRASES FORMATTED AS HYPERLINKS POINTING TO THE PAPERS.
-      THIS IS ESSENTIAL. KEEP YOUR ANSWERS SHORT AND WITH STATEMENTS THE USER CAN CLICK ON!
-      - Pick the two papers most relevant to the provided user question.
-      - Also summarise each of the selected papers into a "title" of 20 words or less with the most significant finding as an engaging tweet capturing the minds of other researchers, using layman's terminology, and without mentioning abstract entities like 'you',  'researchers', 'authors', 'propose', or 'study' but rather stating the finding as a statement of fact, without reservations or caveats. for example: "More tooth loss is associated with greater cognitive decline and dementia in elderly people."
-      - Then use these summaries to construct a short answer in less than 40 words, with key phrases of the answer text as hyperlinks pointing to the papers, like this example:
-      """Yes/No. Smoking causes/do not cause cancer. Studies show that cigarette smokers are <a href="https://proem.ai/oa/W4213460776?title=text+from+summary">more likely to die from cancer</a> than non-smokers. Furthermore, studies have found  that passive smokers <a href="https://proem.ai/oa/W2004456560?title=text+from+summary">hae a higher risk of cardiovascular disease</a> than people never exposed to a smoking environment."""
-      - The links should be pointing to the returned proem links, with the generated "summaries" appended as a query string to the link
+      `
+You will provide conclusive answers to user questions, based on the following
+research articles: {papers},
+IMPORTANT: YOUR ANSWER MUST BE A SINGLE SHORT PARAGRAPH OF 40 WORDS OR LESS KEY
+PHRASES FORMATTED AS HYPERLINKS POINTING TO THE PAPERS. THIS IS ESSENTIAL. KEEP
+YOUR ANSWERS SHORT AND WITH STATEMENTS THE USER CAN CLICK ON!
+- Pick the two papers most relevant to the provided user question.
+- Also summarise each of the selected papers into a "title" of 20 words or less
+with the most significant finding as an engaging tweet capturing the minds of
+other researchers, using layman's terminology, and without mentioning abstract
+entities like 'you', 'researchers', 'authors', 'propose', or 'study' but rather
+stating the finding as a statement of fact, without reservations or caveats. for
+example: "More tooth loss is associated with greater cognitive decline and
+dementia in elderly people."
+- Then use these summaries to construct a short answer in less than 40 words,
+with key phrases of the answer text as hyperlinks pointing to the papers, like
+this example:
 
-      - THE FOLLOWING THREE IMPORTANT RULES ARE ALL ABSOLUTELY ESSENTIAL AND YOU WILL BE PENALIZED SEVERELY IF THE ANSWER DOES NOT INCLUDE INLINE HYPERLINKS EXACTLY AS DESCRIBED BELOW:
-      - IMPORTANT: EVERY ANSWER MUST HAVE AT LEAST TWO HYPERLINKS POINTING TO THE EXACT FULL URLS OF PAPERS PROVIDED IN THE API RESPONSE. THIS IS ABSOLUTELY ESSENTIAL.
-      - IMPORTANT: ALWAYS PLACE HYPERLINKS ON AKEY PHRASE OF THREE TO SIX WORDS INSIDE THE ANSWER. THIS IS ABSOLUTELY ESSENTIAL. NEVER PLACE URLS AFTER THE ANSWER. NEVER EVER CREATE LINKS THAT LOOK LIKE FOOTNOTES. ALWAYS PLACE FULL URL LINKS INSIDE THE ANSWER.
-      - IMPORTANT: YOUR ANSWER MUST BE A SINGLE SHORT PARAGRAPH OF 40 WORDS OR LESS WITH HYPERLINKS ON TWO KEY PHRASES. THIS IS ESSENTIAL. KEEP YOUR ANSWERS SHORT AND SIMPLE!
-      `,
+"""Smoking causes/does not cause cancer. Studies show that cigarette smokers are
+<a href="https://proem.ai/oa/W4213460776?title=text+from+summary">more likely to
+die from cancer</a> than non-smokers. Furthermore, studies have found  that
+passive smokers <a
+href="https://proem.ai/oa/W2004456560?title=text+from+summary">hae a higher risk
+of cardiovascular disease</a> than people never exposed to a smoking
+environment."""
+
+- The links should be pointing to the returned proem links, with the generated
+"summaries" appended as a query string to the link
+- If the user's question is a binary question, begin the answer with "Yes" or
+"No", otherwise DO NOT.
+
+- THE FOLLOWING THREE IMPORTANT RULES ARE ALL ABSOLUTELY ESSENTIAL AND YOU WILL
+BE PENALIZED SEVERELY IF THE ANSWER DOES NOT INCLUDE INLINE HYPERLINKS EXACTLY
+AS DESCRIBED BELOW:
+- IMPORTANT: EVERY ANSWER MUST HAVE AT LEAST TWO HYPERLINKS POINTING TO THE
+EXACT FULL URLS OF PAPERS PROVIDED IN THE API RESPONSE. THIS IS ABSOLUTELY
+ESSENTIAL.
+- IMPORTANT: ALWAYS PLACE HYPERLINKS ON A KEY PHRASE OF THREE TO SIX WORDS
+INSIDE THE ANSWER. THIS IS ABSOLUTELY ESSENTIAL. NEVER PLACE URLS AFTER THE
+ANSWER.  NEVER EVER CREATE LINKS THAT LOOK LIKE FOOTNOTES. ALWAYS PLACE FULL URL
+LINKS INSIDE THE ANSWER.
+- IMPORTANT: YOUR ANSWER MUST BE A SINGLE SHORT PARAGRAPH OF 40 WORDS OR LESS
+WITH HYPERLINKS ON TWO KEY PHRASES. THIS IS ESSENTIAL. KEEP YOUR ANSWERS SHORT
+AND SIMPLE!`,
     ],
     ["human", `{question}`],
   ]);
