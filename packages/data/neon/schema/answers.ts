28e9f3a12129df708b7eae0a34d6f2c23d599177
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-01-30 14:35:08 +0100

    Add answers and papers schema files

---
+++ packages/data/neon/schema/answers.ts
@@ -0,0 +1,17 @@
+import { papers } from "@/neon/schema/papers";
+import { relations } from "drizzle-orm";
+import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
+
+export const answers = pgTable("answers", {
+  id: serial("id").primaryKey(),
+  question: text("question").notNull(),
+  slug: text("slug").notNull(),
+  // paperIds: integer("paperIds").array(),
+});
+
+export type Answer = typeof answers.$inferSelect;
+export type NewAnswer = typeof answers.$inferInsert;
+
+// export const answersRelations = relations(answers, ({ many }) => ({
+//   paper: many(papers),
+// }));
