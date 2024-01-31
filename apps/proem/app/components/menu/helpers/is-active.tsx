0b77e7b962f4a302122cc409e1edcd67c9c85312
proemian <brian@proemial.ai>
2024-01-31 15:52:47 +0100

    fix: remove reload parameter

--- apps/proem/app/components/menu/helpers/is-active.tsx
+++ apps/proem/app/components/menu/helpers/is-active.tsx
@@ -2,7 +2,7 @@ import { usePathname } from "next/navigation";
 
 export function useIsActive({ href }: { href: string }) {
   const pathname = usePathname();
-  const isHome = pathname === "/" && href === "/?reload=true";
+  const isHome = pathname === "/" && href === "/";
 
   return isHome || pathname.includes(href);
 }
