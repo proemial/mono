a38be9c166a2f57cfdc79b01bf73cc79ee935828
proemian <brian@proemial.ai>
2024-02-06 14:35:52 +0100

    fix: track signout

--- apps/proem/app/(pages)/(app)/profile/components/profile-buttons.tsx
+++ apps/proem/app/(pages)/(app)/profile/components/profile-buttons.tsx
@@ -1,10 +1,19 @@
 "use client";
 import { Button } from "@/app/components/shadcn-ui/button";
 import { SignOutButton } from "@clerk/nextjs";
+import { Tracker } from "@/app/components/analytics/tracker";
 
 export function ProfileButtons() {
+  // TODO: callback from clerk is quirky.
+  //  Tracking before redirect breaks redirecting.
+  //  Tracking after kinda works, but sometimes throws a console error
+  const handleCallback = () => {
+    window.location.replace("/");
+    Tracker.track(`click:profile-logout`);
+  };
+
   return (
-    <SignOutButton signOutCallback={() => window.location.replace("/")}>
+    <SignOutButton signOutCallback={handleCallback}>
       <Button variant="danger">
         <span className="font-sans text-xs font-semibold">Log out</span>
       </Button>
