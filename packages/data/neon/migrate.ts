b879f6bb98f0eacdaba75659f9c906b0e8aa596c
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-02-07 15:17:05 +0100

    add migration script

---
+++ packages/data/neon/migrate.ts
@@ -0,0 +1,12 @@
+import { migrate } from "drizzle-orm/neon-http/migrator";
+import { neonDb } from "./db";
+
+const run = async () => {
+  console.log("start migration...");
+  // this will automatically run needed migrations on the database
+  const migration = await migrate(neonDb, { migrationsFolder: "./drizzle" });
+  console.log({ migration });
+  console.log("finishing up...");
+};
+
+run();
