0cb4ebcea56016f1fd579fde7ba49810fbe35851
proemian <brian@proemial.ai>
2024-02-07 08:46:30 +0100

    refact: reverse keys

--- apps/proem/app/components/analytics/analytics-keys.ts
+++ apps/proem/app/components/analytics/analytics-keys.ts
@@ -2,26 +2,34 @@ export const analyticsKeys = {
   ui: {
     menu: {
       click: {
-        ask: "click:menu-ask",
-        feed: "click:menu-feed",
-        read: "click:menu-read",
-        you: "click:menu-profile",
+        ask: "menu:ask:click",
+        feed: "menu:feed:click",
+        read: "menu:read:click",
+        you: "menu:profile:click",
       },
     },
     header: {
       click: {
-        logo: "click:header-logo",
+        logo: "header:logo:click",
       },
     },
   },
-  read: {
+  ask: {
     click: {
-      feed: "click:read-feed",
-      random: "click:read-random",
-      starter: "click:read-ask-starter",
-    },
-    submit: {
-      ask: "submit:read-ask-input",
+      stop: "ask:stop:click",
+      clear: "ask:clear:click",
     },
   },
+  feed: {},
+  read: {
+    click: {
+      feed: "read:feed:click",
+      random: "read:random:click",
+      starter: "read:ask_starter:click",
+    },
+    submit: {
+      ask: "read:ask_input:submit",
+    },
+  },
+  profile: {},
 };
