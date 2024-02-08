588b9fb132ad46dc47362f55c92f62ecf2559a1f
proemian <brian@proemial.ai>
2024-02-08 15:43:08 +0100

    fix inactive color

--- apps/proem/app/components/shadcn-ui/tab.tsx
+++ apps/proem/app/components/shadcn-ui/tab.tsx
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "text-white/80 font-sans inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-2 ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-[#7DFA86] data-[state=active]:text-black duration-100",
+      "text-white/50 font-sans inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-2 ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-[#7DFA86] data-[state=active]:text-black duration-100",
       className,
     )}
     {...props}
