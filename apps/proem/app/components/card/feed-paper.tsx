13e34a66c1ad0de0aae61e5cfaea94d38c603eb6
proemian <brian@proemial.ai>
2024-02-12 14:02:35 +0100

    feature: conditional rendering of card footer

--- apps/proem/app/components/card/feed-paper.tsx
+++ apps/proem/app/components/card/feed-paper.tsx
@@ -8,6 +8,9 @@ import {
 import { Spinner } from "@/app/components/spinner";
 import { OpenAlexWorkMetadata } from "@proemial/models/open-alex";
 import { Suspense } from "react";
+import { getFeatureFlags } from "@/app/components/feature-flags/server-flags";
+import { Features } from "@/app/components/feature-flags/features";
+import { Concepts } from "@/app/components/card/concepts";
 
 export async function FeedPaper({ id }: { id: string }) {
   const paper = await fetchPaper(id);
@@ -17,6 +20,14 @@ export async function FeedPaper({ id }: { id: string }) {
   }
 
   const data = paper.data as OpenAlexWorkMetadata;
+
+  const flags = await getFeatureFlags([
+    Features.hideTopicsInCards,
+    Features.showSubfieldInCards,
+    Features.showConceptsInCards,
+    Features.showJournalInCards,
+    Features.showOrgInCards,
+  ]);
 
   return (
     <PaperCard>
@@ -28,9 +39,29 @@ export async function FeedPaper({ id }: { id: string }) {
         </Suspense>
       </PaperCardTitle>
 
-      <div className="text-xs text-white/50">
-        {data.topics?.length && data.topics.at(0)?.display_name}
-      </div>
+      {!flags.hideTopicsInCards && (
+        <div className="text-xs text-white/50">
+          {data.topics?.length && data.topics.at(0)?.display_name}
+        </div>
+      )}
+      {flags.showSubfieldInCards && (
+        <div className="text-xs text-white/50">
+          {data.topics?.length && data.topics.at(0)?.subfield?.display_name}
+        </div>
+      )}
+      {flags.showOrgInCards && (
+        <div className="text-xs text-white/50">
+          {data?.primary_location?.source?.host_organization_name}
+        </div>
+      )}
+      {flags.showJournalInCards && (
+        <div className="text-xs text-white/50">
+          {data?.primary_location?.source?.display_name}
+        </div>
+      )}
+      {flags.showConceptsInCards && (
+        <Concepts data={paper.data as OpenAlexWorkMetadata} />
+      )}
     </PaperCard>
   );
 }
