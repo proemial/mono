eaf9b1e3076504a0a9e7d2dab25d4dec21fc9faa
proemian <brian@proemial.ai>
2024-02-12 14:08:08 +0100

    refactor: rename subheader

--- apps/proem/app/components/feature-flags/features.ts
+++ apps/proem/app/components/feature-flags/features.ts
@@ -1,8 +1,8 @@
 export enum Features {
-  hideTopicsInCards = "CARD_SUBHEADER_HIDE_TOPICS",
-  showSubfieldInCards = "CARD_SUBHEADER_SHOW_SUBFIELD",
-  showConceptsInCards = "CARD_SUBHEADER_SHOW_CONCEPTS",
-  showJournalInCards = "CARD_SUBHEADER_SHOW_JOURNAL",
-  showOrgInCards = "CARD_SUBHEADER_SHOW_ORG",
+  hideTopicsInCards = "CARD_FOOTER_HIDE_TOPICS",
+  showSubfieldInCards = "CARD_FOOTER_SHOW_SUBFIELD",
+  showConceptsInCards = "CARD_FOOTER_SHOW_CONCEPTS",
+  showJournalInCards = "CARD_FOOTER_SHOW_JOURNAL",
+  showOrgInCards = "CARD_FOOTER_SHOW_ORG",
   fetchPreprintsOnly = "FEED_FETCH_APPROXIMATED_PREPRINTS_ONLY",
 }
