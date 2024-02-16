6114e7a366d5088374bd62a84c0cc167b5f18b46
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-02-16 15:08:39 +0100

    remove illegal route properties

--- apps/proem/app/(pages)/(app)/share/[shareId]/og/route.tsx
+++ apps/proem/app/(pages)/(app)/share/[shareId]/og/route.tsx
@@ -6,9 +6,9 @@ import { ImageResponse } from "next/og";
 
 export const runtime = "edge";
 
-export const alt = "My images alt text";
-export const size = { width: 1200, height: 630 };
-export const contentType = "image/png";
+// export const alt = "My images alt text";
+// export const size = { width: 1200, height: 630 };
+// export const contentType = "image/png";
 export async function GET(request: Request) {
   try {
     console.log({ request: request.url });
