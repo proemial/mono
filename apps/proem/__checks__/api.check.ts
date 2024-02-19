b6fe8cb12f3b83a80bf222f7464dc70ec408893f
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-02-19 11:40:14 +0100

    add checkly & 101 answer engine page ping

---
+++ apps/proem/__checks__/api.check.ts
@@ -0,0 +1,18 @@
+import { ApiCheck, AssertionBuilder } from 'checkly/constructs'
+
+new ApiCheck('proem-answer-engine-page', {
+  name: 'Proem Answer Engine Page',
+  alertChannels: [],
+  degradedResponseTime: 10000,
+  maxResponseTime: 20000,
+  request: {
+    url: 'https://proem.ai',
+    method: 'GET',
+    followRedirects: true,
+    skipSSL: false,
+    assertions: [
+      AssertionBuilder.statusCode().equals(200),
+    ],
+  },
+  runParallel: true,
+})
