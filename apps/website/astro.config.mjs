57d83e0d2d5df0866747aa48ce9dd93a85d29530
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-02-20 21:21:41 +0100

    add /survey redirect to proemial.ai

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,14 +1,20 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  integrations: [tailwind()],
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    }
-  })
+	redirects: {
+		"/survey": {
+			status: 302,
+			destination: "https://tally.so/r/mB7Q7e",
+		},
+	},
+	output: "server",
+	integrations: [tailwind()],
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
 });
