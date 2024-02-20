9c5f10f5a6535921275eea55a513b86df4d14ccc
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-02-20 21:33:19 +0100

    move redirect to middleware rewrite

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -4,15 +4,10 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	redirects: {
-		"/survey": {
-			status: 302,
-			destination: "https://tally.so/r/mB7Q7e",
-		},
-	},
 	output: "server",
 	integrations: [tailwind()],
 	adapter: vercel({
+		edgeMiddleware: true,
 		webAnalytics: {
 			enabled: true,
 		},
---
+++ apps/website/src/middleware.ts
@@ -0,0 +1,12 @@
+import { rewrite } from "@vercel/edge";
+
+export const config = {
+	matcher: "/survey",
+};
+
+export default function middleware(request: Request) {
+	const url = new URL(request.url);
+	if (url.pathname === "/survey") {
+		return rewrite(new URL('https://tally.so/r/mB7Q7e"'));
+	}
+}
