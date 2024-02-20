80362c764ba16e1813a2e06d61b5a12507075f54
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-02-20 15:40:09 +0100

    add frontpage ping

---
+++ apps/proem/__checks__/frontpage-ping.check.ts
@@ -0,0 +1,31 @@
+/**
+ * This is a Checkly CLI ApiCheck construct. To learn more, visit:
+ * - https://www.checklyhq.com/docs/cli/
+ * - https://www.checklyhq.com/docs/cli/constructs-reference/#apicheck
+ */
+
+import { ApiCheck, Frequency, RetryStrategyBuilder } from "checkly/constructs";
+
+new ApiCheck("frontpage-ping", {
+	name: "frontpage-ping",
+	activated: true,
+	muted: false,
+	shouldFail: false,
+	runParallel: true,
+	locations: ["eu-central-1"],
+	frequency: Frequency.EVERY_5M,
+	maxResponseTime: 20000,
+	degradedResponseTime: 5000,
+	request: {
+		url: "https://proem.ai",
+		method: "GET",
+		followRedirects: true,
+		skipSSL: false,
+	},
+	retryStrategy: RetryStrategyBuilder.linearStrategy({
+		baseBackoffSeconds: 60,
+		maxRetries: 2,
+		maxDurationSeconds: 600,
+		sameRegion: true,
+	}),
+});
