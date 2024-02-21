bdf518bdfebe6d04db316db9d826be1960fac09b
proemian <brian@proemial.ai>
2024-02-21 15:13:02 +0100

    amend :)

--- apps/proem/app/llm/helpers/summarise-result.ts
+++ apps/proem/app/llm/helpers/summarise-result.ts
@@ -1,24 +1,26 @@
 import { ScoreType } from "langsmith/schemas";
 
 export type WithResults = {
-    results: {
-        [key: string]: {
-            feedback: WithFeedback[];
-        };
-    };
+	results: {
+		[key: string]: {
+			feedback: WithFeedback[];
+		};
+	};
 };
 
 type WithFeedback = {
-    key: string;
-    score: ScoreType;
+	key: string;
+	score: ScoreType;
 };
 
 export function summariseRunResults(runResults: WithResults) {
-    const scores = Object.values(runResults.results).map((result) => result.feedback.map((feedback) => feedback.score as number)).flat();
-    const avg = scores.reduce((a, b) => a + b, 0) / scores.length;
+	const scores = Object.values(runResults.results).flatMap((result) =>
+		result.feedback.map((feedback) => feedback.score as number),
+	);
+	const avg = scores.reduce((a, b) => a + b, 0) / scores.length;
 
-    return {
-        scores,
-        avg,
-    };
+	return {
+		scores,
+		avg,
+	};
 }
