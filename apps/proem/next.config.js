77cab40dc4dbecdf509ed1c5b5b6d8e9b32a3903
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-02-21 09:35:43 +0100

    add survey redirect to proem.ai

--- apps/proem/next.config.js
+++ apps/proem/next.config.js
@@ -3,32 +3,37 @@
 const { withSentryConfig } = require("@sentry/nextjs");
 
 const nextConfig = {
-  transpilePackages: ["@repo/utils"],
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "gpt.proem.ai",
-          },
-        ],
-        destination: "https://chat.openai.com/g/g-SxYGBAT0I-science-proems",
-        permanent: false,
-      },
-    ];
-  },
-  sentry: {
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#extend-your-nextjs-configuration
-    hideSourceMaps: true,
-  },
+	transpilePackages: ["@repo/utils"],
+	async redirects() {
+		return [
+			{
+				source: "/:path*",
+				has: [
+					{
+						type: "host",
+						value: "gpt.proem.ai",
+					},
+				],
+				destination: "https://chat.openai.com/g/g-SxYGBAT0I-science-proems",
+				permanent: false,
+			},
+			{
+				source: "/survey",
+				destination: "https://tally.so/r/mB7Q7e",
+				permanent: false,
+			},
+		];
+	},
+	sentry: {
+		// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#extend-your-nextjs-configuration
+		hideSourceMaps: true,
+	},
 };
 
 const sentryWebpackPluginOptions = {
-  org: "proemial",
-  project: "proem",
-  authToken: process.env.SENTRY_AUTH_TOKEN,
+	org: "proemial",
+	project: "proem",
+	authToken: process.env.SENTRY_AUTH_TOKEN,
 };
 
 module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
