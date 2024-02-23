588c22abf8a551ecc28dba6967e2106542fa93b9
proemian <brian@proemial.ai>
2024-02-23 09:17:51 +0100

    Add test route

---
+++ apps/proem/app/api/eval/[name]/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest } from "next/server";
+import { runOnDataset } from "langchain/smith";
+import { answerEngineChain } from "@/app/llm/chains/answer-engine-chain";
+import { CharCountEvaluator } from "@/app/llm/evaluators/string-evaluators";
+import { summariseRunResults } from "@/app/llm/helpers/summarise-result";
+import {
+  LinkCountEvaluator,
+  ValidLinkEvaluator,
+  ValidTitleEvaluator,
+} from "@/app/llm/evaluators/link-evaluators";
+
+// http://127.0.0.1/api/eval/ASK-OnePaperDataSet
+// http://127.0.0.1/api/eval/ASK-RefDataSet
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { name: string } },
+) {
+  console.log("params", params);
+  const results = await runOnDataset(answerEngineChain, params.name, [
+    new CharCountEvaluator(200, 400),
+    new LinkCountEvaluator(2, 2),
+    new ValidLinkEvaluator(2, 2),
+    new ValidTitleEvaluator(20, 50),
+  ]);
+  const { scores, avg } = summariseRunResults(results);
+
+  return Response.json({ avg, scores, results });
+}
