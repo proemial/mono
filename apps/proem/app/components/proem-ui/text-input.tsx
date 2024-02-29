d4e4ba3072e2132b1c7cc8150737005ff8970411
proemian <brian@proemial.ai>
2024-02-29 10:49:08 +0100

    cleanup

--- apps/proem/app/components/proem-ui/text-input.tsx
+++ apps/proem/app/components/proem-ui/text-input.tsx
@@ -1,11 +1,12 @@
 import { Send } from "@/app/components/icons/functional/send";
 import { Button } from "@/app/components/shadcn-ui/button";
 import { Input } from "@/app/components/shadcn-ui/input";
+import { ChangeEvent } from "react";
 
 type Props = {
 	value?: string;
 	disabled?: boolean;
-	onChange?: (e: any) => void;
+	onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 };
 
 export function TextInput({ value, disabled, onChange }: Props) {
@@ -15,7 +16,7 @@ export function TextInput({ value, disabled, onChange }: Props) {
 				type="text"
 				name="q"
 				placeholder="Ask anything"
-				className="relative break-words stretch pr-12"
+				className="relative pr-12 break-words stretch"
 				value={value}
 				onChange={onChange}
 				disabled={disabled}
@@ -31,22 +32,4 @@ export function TextInput({ value, disabled, onChange }: Props) {
 			</Button>
 		</>
 	);
-	// return (
-	//   <>
-	//     <Input
-	//       type="text"
-	//       name="q"
-	//       placeholder="Ask anything"
-	//       className="relative break-words stretch"
-	//     />
-	//     <Button
-	//       variant="send_button"
-	//       size="sm"
-	//       type="submit"
-	//       className="absolute justify-center bg-transparent right-4"
-	//     >
-	//       <Send />
-	//     </Button>
-	//   </>
-	// );
 }
