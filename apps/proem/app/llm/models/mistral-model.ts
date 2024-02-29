6a8231ae66888ba6dd123b5ce3be4286bae4271a
Jon Sorensen <jon@proemial.ai>
2024-02-29 17:17:08 +0100

    add mistral model factory

---
+++ apps/proem/app/llm/models/mistral-model.ts
@@ -0,0 +1,36 @@
+import { ChatMistralAI } from "@langchain/mistralai";
+import { Env } from "@proemial/utils/env";
+import { ModelOptions } from "./model-options";
+
+type MistralModel =
+	| "open-mistral-7b"
+	| "mistral-tiny-2312"
+	| "mistral-tiny"
+	| "open-mixtral-8x7b"
+	| "mistral-small-2312"
+	| "mistral-small"
+	| "mistral-small-2402"
+	| "mistral-small-latest"
+	| "mistral-medium-latest"
+	| "mistral-medium-2312"
+	| "mistral-medium"
+	| "mistral-large-latest"
+	| "mistral-large-2402"
+	| "mistral-embed";
+
+const apiKey = Env.get("MISTRAL_API_KEY");
+
+export const buildMistralModel = (
+	modelName: MistralModel,
+	options?: ModelOptions,
+) =>
+	new ChatMistralAI({
+		apiKey,
+		modelName,
+		temperature: options?.temperature ?? 0.0,
+		cache: options?.cache ?? true,
+		verbose: options?.verbose ?? false,
+		onFailedAttempt: (error) => {
+			console.error(error);
+		},
+	});
