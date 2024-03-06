ac4ef9fdad57bc0c7d91cabc6b143d690d5b5963
Jon Sorensen <jon@proemial.ai>
2024-03-06 17:14:59 +0100

    do not invoke answer engine for unsupported intents

--- apps/proem/app/llm/chains/answer-engine-chain.ts
+++ apps/proem/app/llm/chains/answer-engine-chain.ts
@@ -8,6 +8,7 @@ import {
 import { LangChainChatHistoryMessage } from "../utils";
 import { getGenerateAnswerChain } from "./generate-answer-chain";
 import { getIdentifyIntentChain } from "./identify-intent-chain";
+import { Intent } from "./intent";
 
 type Input = {
 	question: string;
@@ -16,7 +17,7 @@ type Input = {
 };
 type Output = string;
 
-const answerChain = RunnableSequence.from<Input & { intent: string }, Output>([
+const answerChain = RunnableSequence.from<Input & { intent: Intent }, Output>([
 	RunnablePassthrough.assign({
 		papers: (input) => {
 			if (input.papers) {
@@ -28,15 +29,39 @@ const answerChain = RunnableSequence.from<Input & { intent: string }, Output>([
 	getGenerateAnswerChain(),
 ]);
 
-const abortChain = RunnableLambda.from<Input, Output>(
-	() => "We're sorry, but we can't answer that question.",
+const abortChain = RunnableLambda.from<Input & { intent: Intent }, Output>(
+	(input) => {
+		// Static responses for unsupported user intents
+		const prefix = "It looks like you're looking for";
+		const postfix = "I can't help with that yet, but come back again soon!";
+		switch (input.intent) {
+			case "FIND_PAPER":
+				return `${prefix} a specific research paper. ${postfix}`;
+			case "FIND_LATEST_PAPERS":
+				return `${prefix} the latest research papers within a specific domain. ${postfix}`;
+			case "FIND_INFLUENTIAL_PAPERS":
+				return `${prefix} specific popular or influential research papers. ${postfix}`;
+			case "FIND_PAPERS_BY_AUTHOR":
+				return `${prefix} research papers by a specific author. ${postfix}`;
+			case "FIND_PAPERS_CITING_A_GIVEN_PAPER":
+				return `${prefix} research papers citing a specific paper. ${postfix}`;
+			default:
+				return "I didn't quite understand that. Please try again with a different question.";
+		}
+	},
 );
 
-const isSupportedIntent = (input: Input & { intent: string }) =>
-	input.intent !== "0";
+const isSupportedIntent = (input: Input & { intent: Intent }) => {
+	const supportedIntents: Intent[] = [
+		"ANSWER_EVERYDAY_QUESTION",
+		"ANSWER_FOLLOWUP_QUESTION",
+		"EXPLAIN_CONCEPT",
+	];
+	return supportedIntents.includes(input.intent);
+};
 
 const answerIfSupportedIntent = RunnableBranch.from<
-	Input & { intent: string },
+	Input & { intent: Intent },
 	Output
 >([[isSupportedIntent, answerChain], abortChain]);
 
