61ce4152deefd2cc94c18bd57bfc1c78b720c050
Jon Sorensen <jon@proemial.ai>
2024-03-06 17:13:16 +0100

    improve intent identification prompt

--- apps/proem/app/llm/chains/identify-intent-chain.ts
+++ apps/proem/app/llm/chains/identify-intent-chain.ts
@@ -1,5 +1,4 @@
 import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { RunnableSequence } from "@langchain/core/runnables";
 import { StringOutputParser } from "@langchain/core/output_parsers";
 import { buildOpenAIChatModel } from "../models/openai-model";
 import { BaseChatModel } from "@langchain/core/language_models/chat_models";
@@ -8,43 +7,70 @@ const prompt = ChatPromptTemplate.fromMessages<Input>([
 	[
 		"system",
 		`
-		Given a user's question, select one of the following use-cases that best
-		matches the user's intent:
+		Given a user question, identify the action from the list below that best
+		describes the user's expectation based on their question:
 
-		1. Get an answer to an everyday question (backed by scientific research).
-		2. Ask a follow-up question to a previously supplied answer.
-		3. Get an explanation of a key concept.
-		4. Find a specific research article in a database.
-		5. Find the latest research articles within a broad domain.
-		6. Find the most influential research articles on a specific topic.
-		7. Find all research articles by a specific author.
-		8. Find all research articles that cite a specific other research article.
-		0. Unsure or none of the above.
+		---
+		Action: \`ANSWER_EVERYDAY_QUESTION\`
+		Description: Give an answer to an everyday question.
+
+		Action: \`ANSWER_FOLLOWUP_QUESTION\`
+		Description: Give an answer to a previously-asked question.
+
+		Action: \`EXPLAIN_CONCEPT\`
+		Description: Explain a key concept.
+
+		Action: \`FIND_PAPER\`
+		Description: Find a specific research paper.
+
+		Action: \`FIND_LATEST_PAPERS\`
+		Description: Find the latest research papers within a broad domain.
+
+		Action: \`FIND_INFLUENTIAL_PAPERS\`
+		Description: Find the most influential research papers on a specific topic.
+
+		Action: \`FIND_PAPERS_BY_AUTHOR\`
+		Description: Find all research papers by a specific author.
+
+		Action: \`FIND_PAPERS_CITING_A_GIVEN_PAPER\`
+		Description: Find all research papers citing a given research paper.
+
+		Action: \`UNKNOWN\`
+		Description: Unsure or none of the above.
+		---
+
+		Example answer 1:
+
+		---
+		ANSWER_EVERYDAY_QUESTION
+		---
+
+		Example answer 2:
+
+		---
+		ANSWER_FOLLOWUP_QUESTION
+		---
+
+		Example answer 3:
+
+		---
+		EXPLAIN_CONCEPT
+		---
 
 		Rules:
-		- Respond only with the digit of matching item in the list above.
-		- If you are unsure which intent best matches the user's intent, respond
-			with "0".
+		- Respond ONLY with the action of the matching item from the list above.
+		- If you are unsure which action best matches the user's request, respond
+			with \`UNKNOWN\`.
 	`,
 	],
 	["human", "User question: {question}"],
 ]);
 
-const model = buildOpenAIChatModel("gpt-3.5-turbo-0125", "ask", {
-	cache: false,
-	verbose: true,
-});
-
-const stringOutputParser = new StringOutputParser();
+const model = buildOpenAIChatModel("gpt-3.5-turbo-0125", "ask");
 
 type Input = { question: string };
-type Output = string;
 
 export const getIdentifyIntentChain = (modelOverride: BaseChatModel = model) =>
-	RunnableSequence.from<Input, Output>([
-		prompt,
-		model,
-		stringOutputParser,
-	]).withConfig({
+	prompt.pipe(modelOverride).pipe(new StringOutputParser()).withConfig({
 		runName: "IdentifyIntent",
 	});
--- apps/proem/app/llm/evaluators/intent-evaluator.ts
+++ apps/proem/app/llm/evaluators/intent-evaluator.ts
@@ -5,19 +5,19 @@ import { Example, Run } from "langsmith";
 export class IntentEvaluator implements RunEvaluator {
 	async evaluateRun(run: Run, example?: Example): Promise<EvaluationResult> {
 		const output = runOutputAsString(run);
-		const reference = example?.outputs?.output as string | undefined;
-		const result = IntentEvaluator.evaluate(output, reference);
+		const expected = example?.outputs?.output as string | undefined;
+		const result = IntentEvaluator.evaluate(output, expected);
 		return { key: "intent", ...result };
 	}
 
-	static evaluate(output: string | undefined, reference: string | undefined) {
+	static evaluate(output: string | undefined, expected: string | undefined) {
 		if (typeof output === "undefined") {
 			return { value: 0, score: 0, comment: "No model output was found" };
 		}
-		if (typeof reference === "undefined") {
-			return { value: 0, score: 0, comment: "No dataset reference was found" };
+		if (typeof expected === "undefined") {
+			return { value: 0, score: 0, comment: "No expected value was found" };
 		}
-		const comment = `expected: ${reference}, actual: ${output}`;
-		return { value: output, score: output === reference ? 1 : 0, comment };
+		const comment = `expected: ${expected}, actual: ${output}`;
+		return { value: output, score: output === expected ? 1 : 0, comment };
 	}
 }
