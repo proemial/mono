42c4ceeec8d37f0b3efdafc97976b8a501be58ab
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-03-06 10:45:28 +0100

    refactor save-answer to fit changed chains

--- apps/proem/app/api/bot/answer-engine/answer-engine.ts
+++ apps/proem/app/api/bot/answer-engine/answer-engine.ts
@@ -1,7 +1,7 @@
 import { answers } from "@/app/api/bot/answer-engine/answers";
 import { prettySlug } from "@/app/api/bot/answer-engine/prettySlug";
+import { saveAnswer } from "@/app/api/bot/answer-engine/save-answer";
 import { answerEngineChain } from "@/app/llm/chains/answer-engine-chain";
-import { findRun } from "@/app/llm/helpers/find-run";
 import { toLangChainChatHistory } from "@/app/llm/utils";
 import { BytesOutputParser } from "@langchain/core/output_parsers";
 import {
@@ -67,61 +67,3 @@ export async function askAnswerEngine({
 		data,
 	);
 }
-
-const saveAnswer =
-	(
-		question: string,
-		isFollowUpQuestion: boolean,
-		slug: string,
-		userId: string | undefined,
-		data: experimental_StreamData,
-	) =>
-	async (run: Run) => {
-		const hasAnswer = (run: Run) => run.name === "AnswerEngine";
-		const answer = findRun(run, hasAnswer)?.outputs?.output;
-
-		if (!answer) {
-			data.close();
-			throw new Error("Save failure: No answer was found");
-		}
-
-		const hasPapersResponse = (run: Run) => run.name === "FetchPapersTool";
-		const papersResponse = findRun(run, hasPapersResponse)?.outputs
-			?.output as string;
-
-		const hasSearchParamsResponse = (run: Run) =>
-			run.name === "GenerateSearchParams";
-		const searchParamsResponse = findRun(run, hasSearchParamsResponse)
-			?.outputs as { keyConcept: string; relatedConcepts: string[] };
-
-		const papers = isFollowUpQuestion
-			? {}
-			: {
-					relatedConcepts: searchParamsResponse.relatedConcepts,
-					keyConcept: searchParamsResponse.keyConcept,
-					papers: {
-						papers: JSON.parse(papersResponse),
-					},
-			  };
-
-		const insertedAnswer = await answers.create({
-			slug,
-			question,
-			answer,
-			ownerId: userId,
-			...papers,
-		});
-
-		if (!insertedAnswer) {
-			data.close();
-			return;
-		}
-
-		data.append({
-			answers: {
-				shareId: insertedAnswer.shareId,
-				answer: insertedAnswer.answer,
-			},
-		});
-		data.close();
-	};
