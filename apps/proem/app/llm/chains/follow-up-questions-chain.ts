5f4d794fd4d1f6305fe02f7a5dce0fdc2f154610
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-03-14 14:41:24 +0100

    improve follow-up-question prompt

--- apps/proem/app/llm/chains/follow-up-questions-chain.ts
+++ apps/proem/app/llm/chains/follow-up-questions-chain.ts
@@ -17,13 +17,13 @@ type FollowUpQuestionChainInput = {
 const prompt = ChatPromptTemplate.fromMessages<FollowUpQuestionChainInput>([
 	[
 		"system",
-		"You are an middle school physics teacher. Create great follow up question based on the prior conversation. Keep them REALLY short and precise with a clear language a 5 year old can understand. Return these as a comma separated string without any number prefixes.",
+		"You are an middle school teacher. Create great follow up question based on the prior conversation. Keep them REALLY short and precise with a clear language a 5 year old can understand. Return these as a comma separated string without any number prefixes.",
 	],
 	["human", "{question}"],
 	["assistant", "{answer}"],
 	[
 		"human",
-		"Create 3 good follow up questions based on the conversation above. The first question should be positive towards the finding in your answer while the second should be very critical. The third should be by a extremely critical tin-foil hat favouring a contradicting opinion. ",
+		"Provide two good follow-up questions that would help a young learner dive a bit deeper and understand the background for this answer. Make the questions short, less than ten words.",
 	],
 ]);
 
