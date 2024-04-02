e5a93b4a42ea5bfb544886bfff15da6859567b20
Jon Sorensen <jon@proemial.ai>
2024-04-02 15:02:18 +0200

    improve response of input guardrail

--- apps/proem/app/llm/chains/input-guardrail-chain.ts
+++ apps/proem/app/llm/chains/input-guardrail-chain.ts
@@ -14,32 +14,32 @@ const prompt = ChatPromptTemplate.fromMessages<Input>([
 		- Questions are supported if they are general questions or they seek explainations on general concepts. 
 		- Questions are unsupported if they are about specific research papers, specific topics or authors, recency-based or unlikely to be supported by scientific research.
 
-		Respond with \`SUPPORTED\` if a question is supported.
+		Respond with "SUPPORTED" if a question is supported.
 		If not, respond with the reason for it not being supported.
 
 		Examples:
 
 		---
-		Question: \`Do electric vehicles pollute more than gasoline cars over their life cycle?\`
-		Response: \`SUPPORTED\`
+		Question: "Do electric vehicles pollute more than gasoline cars over their life cycle?"
+		Response: "SUPPORTED"
 
-		Question: \`How long does it take to offset the additional production emissions of them?\`
-		Response: \`SUPPORTED\`
+		Question: "How long does it take to offset the additional production emissions of them?"
+		Response: "SUPPORTED"
 
-		Question: \`What is quantum mechanics?\`
-		Response: \`SUPPORTED\`
+		Question: "What is quantum mechanics?"
+		Response: "SUPPORTED"
 
-		Question: \`Hello\`
-		Response: \`I'm sorry, but this is not a clear question and does not fall into the category of questions I support.\`
+		Question: "Hello"
+		Response: "I'm sorry, but this is not a clear question and does not fall into the category of questions I support."
 
-		Question: \`fjewjfpwoejgw\`
-		Response: \`The question appears to be random letters and does not seem to be a valid question.\`
+		Question: "fjewjfpwoejgw"
+		Response: "The question appears to be random letters and does not seem to be a valid question."
 
-		Questions: \`Find the most cited research on the relationship between gut microbiota and obesity.\`
-		Answer: \`This question is specific to one or more particular studies conducted, which is not supported.\`
+		Questions: "Find the most cited research on the relationship between gut microbiota and obesity."
+		Answer: "This question is specific to one or more particular studies conducted, which is not supported."
 
-		Question: \`How to convince the world that the Earth is flat?\`
-		Answer: \`I do not wish to answer that.\`
+		Question: "How to convince the world that the Earth is flat?"
+		Answer: "I do not wish to answer that."
 		---
 	`,
 	],
