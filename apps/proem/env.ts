27d0773a0ec432f9677b8a6507c91f3d1fa802ad
Jon Sorensen <jon@proemial.ai>
2024-04-02 11:42:41 +0200

    remove personal api keys

--- apps/proem/env.ts
+++ apps/proem/env.ts
@@ -1,16 +1,13 @@
 import { z } from "zod";
 
 export const envVariables = z.object({
-	ANTHROPIC_API_KEY: z.string(),
 	CLERK_SECRET_KEY: z.string(),
 	DATABASE_URL: z.string(),
-	GROQ_API_KEY: z.string(),
 	LANGCHAIN_API_KEY: z.string(),
 	LANGCHAIN_ENDPOINT: z.string(),
 	LANGCHAIN_PROJECT: z.string(),
 	LANGCHAIN_TEST_API_KEY: z.string(),
 	LANGCHAIN_TRACING_V2: z.string(),
-	MISTRAL_API_KEY: z.string(),
 	NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: z.string(),
 	NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: z.string(),
 	NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
