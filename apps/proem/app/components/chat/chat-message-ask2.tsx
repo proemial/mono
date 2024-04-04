04e3603161d13f167c2afc2c303cfde049507105
proemian <brian@proemial.ai>
2024-04-04 15:22:23 +0200

    Start migration of ask agent

---
+++ apps/proem/app/components/chat/chat-message-ask2.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import { analyticsKeys } from "@/app/components/analytics/analytics-keys";
+import { Tracker } from "@/app/components/analytics/tracker";
+import {
+	PaperCard,
+	PaperCardTitle,
+	PaperCardTop,
+} from "@/app/components/card/paper-card";
+import { applyLinks } from "@/app/components/chat/apply-links";
+import { LinkButton } from "@/app/components/proem-ui/link-button";
+import {
+	Avatar,
+	AvatarFallback,
+	AvatarImage,
+} from "@/app/components/shadcn-ui/Avatar";
+import type { Message } from "ai/react";
+import { ShareIcon } from "lucide-react";
+import { SinglarThrobber, Throbber } from "../loading/throbber";
+import {
+	FeedbackButtons,
+	type FeedbackButtonsProps,
+} from "./feedback/feedback-buttons";
+
+// TODO! Type as either a user message or a bot message
+export type ChatMessageProps = Partial<FeedbackButtonsProps> & {
+	message?: Message["content"];
+	user?: {
+		fullName: string;
+		initials: string;
+		avatar: string;
+	};
+	isLoading?: boolean;
+	showThrobber?: boolean;
+	onShareHandle?: (() => void) | null;
+};
+
+export function ChatMessage({
+	message,
+	user = { fullName: "you", initials: "U", avatar: "" },
+	onShareHandle,
+	runId,
+	showThrobber,
+}: ChatMessageProps) {
+	const { content, links } = applyLinks(message ?? "");
+
+	return (
+		<div className="w-full mx-[-4px] my-2">
+			<div className="flex justify-between gap-4">
+				<div className="flex gap-3">
+					<Avatar className="w-6 h-6">
+						<AvatarImage src={user.avatar} />
+						<AvatarFallback className="bg-gray-600">
+							{user.initials}
+						</AvatarFallback>
+					</Avatar>
+					<div className="font-bold">{user.fullName}</div>
+				</div>
+
+				<div className="flex items-end gap-3">
+					<FeedbackButtons runId={runId} />
+
+					{onShareHandle && (
+						<ShareIcon
+							onClick={() => {
+								onShareHandle();
+								Tracker.track(analyticsKeys.ask.click.share);
+							}}
+							className="ml-auto"
+						/>
+					)}
+				</div>
+			</div>
+
+			<div className="mt-2 ml-9">
+				<div>
+					{message ? (
+						content
+					) : (
+						<Throbber text="Searching scientific papers..." />
+					)}
+					{showThrobber && <SinglarThrobber />}
+				</div>
+
+				{links.length > 0 && (
+					<div className="pt-3 mt-3 space-y-3 border-t border-[#3C3C3C]">
+						{links.map((link) => (
+							<PaperCard
+								key={link.href}
+								className="pb-3 pt-1 border-0 rounded-lg bg-[#3C3C3C]"
+							>
+								{/* TODO: add date */}
+								<PaperCardTop />
+
+								<PaperCardTitle>{link.title}</PaperCardTitle>
+
+								<LinkButton
+									href={link.href}
+									className="py-[1px] mt-2 no-underline"
+									track={analyticsKeys.ask.click.answerCard}
+								>
+									Learn more...
+								</LinkButton>
+							</PaperCard>
+						))}
+					</div>
+				)}
+			</div>
+		</div>
+	);
+}
---
+++ apps/proem/app/components/chat/chat-messages-ask2.tsx
@@ -0,0 +1,103 @@
+"use client";
+import { STARTERS } from "@/app/(pages)/(app)/(answer-engine)/starters";
+import { useRunOnFirstRender } from "@/app/hooks/use-run-on-first-render";
+import { useChat, type Message } from "ai/react";
+import { useEffect, useRef, type ReactNode } from "react";
+import { PROEM_BOT } from "./bot-user";
+import { ChatMessage } from "./chat-message-ask2";
+import { useChatState } from "./state";
+
+type Props = {
+	message?: string;
+	children?: ReactNode;
+	initialMessages?: Message[];
+	existingShareId?: string;
+};
+
+export function ChatMessages({
+	message,
+	children,
+	initialMessages,
+}: Props) {
+	const { question, clearQuestion, setSuggestions } = useChatState("ask");
+	const {
+		messages,
+		isLoading,
+		append,
+		setMessages,
+		data,
+	} = useChat({
+		sendExtraMessageFields: true,
+		id: "hardcoded",
+		api: "/api/bot/ask2",
+		initialMessages,
+	});
+
+	const messagesDiv = useScroll(messages);
+
+	useRunOnFirstRender(() => {
+		if (message) {
+			setMessages([]);
+			appendQuestion(message);
+		} else if (!initialMessages?.length) {
+			const starters = [...STARTERS]
+				.sort(() => 0.5 - Math.random())
+				.slice(0, 3);
+			setSuggestions(starters);
+		}
+	});
+
+	useEffect(() => {
+		if (question) {
+			appendQuestion(question);
+			clearQuestion();
+			setSuggestions([]);
+		}
+	}, [question, setSuggestions, clearQuestion]);
+
+	const appendQuestion = (question: string) =>
+		append({ role: "user", content: question });
+
+	return (
+		<>
+			{messages?.length > 0 && (
+				<div ref={messagesDiv} className="pb-32">
+					{messages.map((message, i) => {
+						const isMessageFromAI = message.role === "assistant";
+						const isLastMessage = i === messages.length - 1;
+						// Prevent settings prior messages in a conversation to a loading state while we wait for the AI to respond
+						const isLastMessageAndLoading = isLastMessage ? isLoading : false;
+
+						return (
+							<ChatMessage
+								key={message.id}
+								message={message.content}
+								user={isMessageFromAI ? PROEM_BOT : undefined}
+								isLoading={isLastMessageAndLoading}
+								showThrobber={isMessageFromAI && isLoading && isLastMessage}
+							/>
+						);
+					})}
+
+					{isLoading && <ChatMessage user={PROEM_BOT} />}
+				</div>
+			)}
+			{messages.length === 0 && children}
+		</>
+	);
+}
+
+function useScroll(messages: Message[]) {
+	const messagesDiv = useRef<HTMLDivElement>(null);
+	useEffect(() => {
+		if (messagesDiv.current && messages) {
+			messagesDiv.current.scrollIntoView({
+				behavior: "smooth",
+				block: "end",
+				inline: "nearest",
+			});
+		}
+	}, [messages]);
+
+	return messagesDiv;
+}
