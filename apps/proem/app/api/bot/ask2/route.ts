2c8e878713b4052732a23f24310d1f67a473b188
proemian <brian@proemial.ai>
2024-04-05 09:47:16 +0200

    use prod prompt

--- apps/proem/app/api/bot/ask2/route.ts
+++ apps/proem/app/api/bot/ask2/route.ts
@@ -10,9 +10,39 @@ import { NextRequest, NextResponse } from "next/server";
 import { getTools } from "./tools";
 
 const template = `
-You will provide conclusive answers to user questions, based on relevant research articles, retrieved using the provided SearchPapers tool.
-Answers must be grounded in specific research papers.
+You are a helpful high school teacher that provides conclusive answers to a
+user's question, based on scientific research papers provided by the SearchPapers tool and
+your general knowledge..
 
+If the user is asking a general science-related quesion, use the SearchPapers tool and base your answer on the two most relevant research papers
+retrieved. If you find it necessary, include an introduction to the topic of the user's question, using a single sentence.
+
+If you invoke the SearchPapers tool for a specific question, append the links of these two research papers
+beneath your answer, refering to them by number. It is important that you do not append the links if you did not use the SearchPapers tool for a specific answer!.
+
+Rules:
+- Your answer must not exceed 40 words.
+- Your answer must be using layman's terminology, instead of scientific jargon.
+- Act as if you found the research yourself.
+
+Examples:
+
+---
+User question: What are the laws of thermodynamics?
+
+Your answer: The laws of thermodynamics, derived from historical principles and
+refined through scientific inquiry, govern energy transfer and entropy. They are
+essential in understanding both equilibrium and non-equilibrium processes, as
+well as the behavior of systems from classical to quantum scales.
+
+[1](/oa/W2088834980?title=1)[2](/oa/W2074335107?title=2)
+---
+
+User question: Why is this important?
+
+Your answer: The laws of thermodynamics are crucial because they explain how energy behaves and why certain processes occur. They are fundamental 
+for understanding everything from how engines work to the behavior of living organisms
+---
 `;
 
 // Finalise POC
@@ -28,6 +58,10 @@ Answers must be grounded in specific research papers.
 // Improvements
 // - Rename parent run
 // - Extract trace id from agent execution, and add it to the tools
+
+// TODO
+// Improve prompt
+// Return papers
 
 export async function POST(req: NextRequest) {
 	try {
--- apps/proem/app/components/chat/chat-message-ask2.tsx
+++ apps/proem/app/components/chat/chat-message-ask2.tsx
@@ -2,13 +2,7 @@
 
 import { analyticsKeys } from "@/app/components/analytics/analytics-keys";
 import { Tracker } from "@/app/components/analytics/tracker";
-import {
-	PaperCard,
-	PaperCardTitle,
-	PaperCardTop,
-} from "@/app/components/card/paper-card";
 import { applyLinks } from "@/app/components/chat/apply-links";
-import { LinkButton } from "@/app/components/proem-ui/link-button";
 import {
 	Avatar,
 	AvatarFallback,
@@ -81,30 +75,6 @@ export function ChatMessage({
 					)}
 					{showThrobber && <SinglarThrobber />}
 				</div>
-
-				{links.length > 0 && (
-					<div className="pt-3 mt-3 space-y-3 border-t border-[#3C3C3C]">
-						{links.map((link) => (
-							<PaperCard
-								key={link.href}
-								className="pb-3 pt-1 border-0 rounded-lg bg-[#3C3C3C]"
-							>
-								{/* TODO: add date */}
-								<PaperCardTop />
-
-								<PaperCardTitle>{link.title}</PaperCardTitle>
-
-								<LinkButton
-									href={link.href}
-									className="py-[1px] mt-2 no-underline"
-									track={analyticsKeys.ask.click.answerCard}
-								>
-									Learn more...
-								</LinkButton>
-							</PaperCard>
-						))}
-					</div>
-				)}
 			</div>
 		</div>
 	);
