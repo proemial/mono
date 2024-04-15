3cdb36f7822f649b526884b6ddbd08442ab06385
proemian <brian@proemial.ai>
2024-04-15 17:46:55 +0200

    only show ellipsis when truncated

--- apps/proem/app/(pages)/(app)/share/[shareId]/og/answer-sharing-card.tsx
+++ apps/proem/app/(pages)/(app)/share/[shareId]/og/answer-sharing-card.tsx
@@ -16,15 +16,16 @@ export function AnswerSharingCard({
 		[classNameAttr]: tailwindClasses,
 	});
 
-	const truncate = (str: string) => str.length <= maxLength ? str : `${str.substring(0, maxLength) + str?.substring(maxLength).split(" ")[0]} ...`;
-	// #262626, #474747
+	const truncate = (str: string) => str.length <= maxLength ? str : `${str.substring(0, maxLength) + str?.substring(maxLength).split(" ")[0]}`;
+	const truncated = truncate(content);
+
 	return (
 		<div
 			{...twcl("flex flex-col p-12 bg-[#262626] w-full h-full text-white font-sans")}
 		>
 			<div {...twcl("flex flex-col flex-1 mt-2 mb-12")}>
 				<div {...twcl("flex flex-wrap line-clamp-3 text-[40px] leading-1.6 p-8 bg-[#474747] rounded-t-[32px] rounded-br-[32px]")}>
-					{truncate(content)}
+					{truncated}{truncated !== content && " ..."}
 				</div>
 				<img
 					{...twcl("w-[74px] h-[46px]")}
