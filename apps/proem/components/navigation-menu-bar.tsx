3fdf3a3d78f86747746ff3ffdf678aaea0054f64
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-04-15 16:24:14 +0200

    remove unsused code & move custom styling from ShadCN to the our custom component

--- apps/proem/components/navigation-menu-bar.tsx
+++ apps/proem/components/navigation-menu-bar.tsx
@@ -2,17 +2,15 @@
 
 import { Profile } from "@/components/profile";
 import {
+	Button,
 	NavigationMenu,
 	NavigationMenuItem,
-	NavigationMenuLink,
 	NavigationMenuList,
 	cn,
 } from "@proemial/shadcn-ui";
+import { Compass, SquarePen } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import * as React from "react";
-import { ButtonCompass } from "./button-compass";
-import { ButtonEdit } from "./button-edit";
 
 const navigationItems = [
 	{
@@ -33,14 +31,14 @@ export function NavigationMenuBar() {
 	};
 
 	return (
-		<NavigationMenu>
-			<NavigationMenuList>
-				<NavigationMenuItem className="flex-none" key="menu">
+		<NavigationMenu className="sticky top-0 px-0 py-2.5 bg-background">
+			<NavigationMenuList className="justify-between">
+				<NavigationMenuItem>
 					<Profile />
 				</NavigationMenuItem>
-				<div className="flex-grow" />
-				{navigationItems.map((navigationItem, index) => (
-					<NavigationMenuItem key={navigationItem.href}>
+
+				<NavigationMenuItem className="flex">
+					{navigationItems.map((navigationItem, index) => (
 						<Link
 							href={navigationItem.href}
 							className={cn(
@@ -53,50 +51,22 @@ export function NavigationMenuBar() {
 						>
 							{navigationItem.name}
 						</Link>
-					</NavigationMenuItem>
-				))}
-				<div className="flex-grow" />
-				<NavigationMenuItem className="flex-none px-1" key="create">
+					))}
+				</NavigationMenuItem>
+
+				<NavigationMenuItem className="flex-none px-1">
 					{pathname === "/" && (
-						<Link href="#" legacyBehavior passHref>
-							<ButtonEdit handleClick={handleClick} />
-						</Link>
+						<Button onClick={handleClick} variant="ghost">
+							<SquarePen className="size-4" />
+						</Button>
 					)}
 					{pathname !== "/" && (
-						<Link href="#" legacyBehavior passHref>
-							<ButtonCompass handleClick={handleClick} />
-						</Link>
+						<Button onClick={handleClick} variant="ghost">
+							<Compass className="size-4" />
+						</Button>
 					)}
 				</NavigationMenuItem>
 			</NavigationMenuList>
 		</NavigationMenu>
 	);
 }
-
-const ListItem = React.forwardRef<
-	React.ElementRef<"a">,
-	React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-	return (
-		<>
-			<li>
-				<NavigationMenuLink asChild>
-					<a
-						ref={ref}
-						className={cn(
-							"block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-							className,
-						)}
-						{...props}
-					>
-						<div className="text-sm font-medium leading-none">{title}</div>
-						<p className="text-sm leading-snug line-clamp-2 text-muted-foreground">
-							{children}
-						</p>
-					</a>
-				</NavigationMenuLink>
-			</li>
-		</>
-	);
-});
-ListItem.displayName = "ListItem";
