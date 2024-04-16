9ec734f087ef45be5c4f7051eb0f4e6cf483ca24
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-04-16 11:08:19 +0200

    remove bullet prop type from paper-card-discover

--- apps/proem/components/paper-card-discover.tsx
+++ apps/proem/components/paper-card-discover.tsx
@@ -1,7 +1,7 @@
 import { PaperCard, PaperCardProps } from "@/components/paper-card";
 import { Globe } from "lucide-react";
 
-type PaperCardDiscoverProps = PaperCardProps;
+type PaperCardDiscoverProps = Omit<PaperCardProps, "bullet">;
 
 export function PaperCardDiscover(props: PaperCardDiscoverProps) {
 	return <PaperCard {...props} bullet={<Globe className="size-4" />} />;
