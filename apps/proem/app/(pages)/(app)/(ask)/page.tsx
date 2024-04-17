9a1413a5cb485e384a76cef781147fd1ebbe100e
proemian <brian@proemial.ai>
2024-04-17 10:54:23 +0200

    fix landing page

--- apps/proem/app/(pages)/(app)/(ask)/page.tsx
+++ apps/proem/app/(pages)/(app)/(ask)/page.tsx
@@ -11,18 +11,21 @@ export default function AskPage() {
 	];
 
 	return (
-		<div className="flex flex-col flex-grow gap-4 justify-end">
-			<div className="flex flex-col gap-2">
-				<div className="flex justify-end">
-					<SelectContentSelector
-						selector={[
-							{ value: "popular", label: "Popular" },
-							{ value: "trending", label: "Trending", disabled: true },
-							{ value: "curious", label: "Curious", disabled: true },
-						]}
-					/>
+		<div className="flex flex-col gap-10 flex-grow justify-between">
+			<div className="flex flex-col flex-grow gap-10">
+				<div className="flex flex-col flex-grow gap-10" />
+				<div className="flex flex-col gap-2">
+					<div className="flex justify-end">
+						<SelectContentSelector
+							selector={[
+								{ value: "popular", label: "Popular" },
+								{ value: "trending", label: "Trending", disabled: true },
+								{ value: "curious", label: "Curious", disabled: true },
+							]}
+						/>
+					</div>
+					<Suggestions suggestions={starters} />
 				</div>
-				<Suggestions suggestions={starters} />
 			</div>
 			<ChatInput placeholder="Ask a question" />
 		</div>
