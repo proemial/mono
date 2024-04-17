4206c2b7d0612d07b8abcf850385e5cdbc4f2e19
Jon Sorensen <jon@proemial.ai>
2024-04-17 16:46:57 +0200

    fix: svg prop errors in the console

--- apps/proem/components/icons/GanttChart.tsx
+++ apps/proem/components/icons/GanttChart.tsx
@@ -1,10 +1,31 @@
 export function GanttChart() {
 	return (
 		// biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
-		<svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<path d="M1 1H12" stroke="#2B2B2B" stroke-width="1.5" stroke-linecap="round" />
-			<path d="M9 6L17 6" stroke="#2B2B2B" stroke-width="1.5" stroke-linecap="round" />
-			<path d="M1 11H8.53846" stroke="#2B2B2B" stroke-width="1.5" stroke-linecap="round" />
+		<svg
+			width="18"
+			height="12"
+			viewBox="0 0 18 12"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M1 1H12"
+				stroke="#2B2B2B"
+				strokeWidth="1.5"
+				strokeLinecap="round"
+			/>
+			<path
+				d="M9 6L17 6"
+				stroke="#2B2B2B"
+				strokeWidth="1.5"
+				strokeLinecap="round"
+			/>
+			<path
+				d="M1 11H8.53846"
+				stroke="#2B2B2B"
+				strokeWidth="1.5"
+				strokeLinecap="round"
+			/>
 		</svg>
 	);
 }
