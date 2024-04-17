8691dab9475b5900df5be1b1be40813d2d04a3f9
Jon Sorensen <jon@proemial.ai>
2024-04-17 12:28:58 +0200

    simplify navbar and disable link to discover (until prioritized)

--- apps/proem/components/navigation-menu-bar.tsx
+++ apps/proem/components/navigation-menu-bar.tsx
@@ -8,27 +8,30 @@ import {
 	NavigationMenuList,
 	cn,
 } from "@proemial/shadcn-ui";
-import { Compass03, Edit05 } from "@untitled-ui/icons-react";
+import { Edit05 } from "@untitled-ui/icons-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
-const navigationItems = [
-	{
+const navItems = {
+	discover: {
 		name: "Discover",
 		href: "/discover",
 	},
-	{
+	ask: {
 		name: "Ask",
-		href: "/",
 	},
-];
+};
 
 export function NavigationMenuBar() {
 	const pathname = usePathname();
+	const [askHref, setAskHref] = useState<string>("/");
 
-	const handleClick = () => {
-		console.log(pathname);
-	};
+	useEffect(() => {
+		if (pathname === "/" || pathname.includes("/answer")) {
+			setAskHref(pathname);
+		}
+	}, [pathname]);
 
 	return (
 		<NavigationMenu className="sticky top-0 px-0 py-2.5 bg-background">
@@ -38,34 +41,37 @@ export function NavigationMenuBar() {
 				</NavigationMenuItem>
 
 				<NavigationMenuItem className="flex">
-					{navigationItems.map((navigationItem, index) => (
-						<Link
-							key={navigationItem.name}
-							href={navigationItem.href}
-							className={cn(
-								"flex h-7 items-center justify-center rounded-full px-4 text-center text-sm",
-								pathname?.startsWith(navigationItem.href) ||
-									(index === 0 && pathname === "/")
-									? "bg-muted font-medium text-foreground"
-									: "text-foreground",
-							)}
-						>
-							{navigationItem.name}
-						</Link>
-					))}
+					{/* Discover link is disabled until feature is implemented */}
+					<Link
+						href={navItems.discover.href}
+						aria-disabled
+						tabIndex={-1}
+						className={cn(
+							"flex h-7 items-center justify-center rounded-full px-4 text-center text-sm pointer-events-none text-muted-foreground",
+							pathname === navItems.discover.href ? "bg-muted font-medium" : "",
+						)}
+					>
+						{navItems.discover.name}
+					</Link>
+					<Link
+						href={askHref}
+						className={cn(
+							"flex h-7 items-center justify-center rounded-full px-4 text-center text-sm",
+							pathname === askHref
+								? "bg-muted font-medium text-foreground"
+								: "text-foreground",
+						)}
+					>
+						{navItems.ask.name}
+					</Link>
 				</NavigationMenuItem>
 
 				<NavigationMenuItem className="flex-none px-1">
-					{pathname === "/" && (
-						<Button onClick={handleClick} variant="ghost">
+					<Link href="/">
+						<Button variant="ghost">
 							<Edit05 className="size-4" />
 						</Button>
-					)}
-					{pathname !== "/" && (
-						<Button onClick={handleClick} variant="ghost">
-							<Compass03 className="size-4" />
-						</Button>
-					)}
+					</Link>
 				</NavigationMenuItem>
 			</NavigationMenuList>
 		</NavigationMenu>
