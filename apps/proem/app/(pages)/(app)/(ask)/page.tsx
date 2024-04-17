eaf26c0503235581d00f55434e3deccf7c32fe13
Jon Sorensen <jon@proemial.ai>
2024-04-17 16:23:28 +0200

    add logo

--- apps/proem/app/(pages)/(app)/(ask)/page.tsx
+++ apps/proem/app/(pages)/(app)/(ask)/page.tsx
@@ -1,6 +1,8 @@
+import { ProemLogo } from "@/app/components/icons/brand/logo";
 import { ChatInput } from "@/components/chat-input";
 import { SelectContentSelector } from "@/components/select-content-selector";
 import { Suggestions } from "@/components/suggestions";
+import { Header3 } from "@proemial/shadcn-ui";
 import { getThreeRandomStarters } from "./starters";
 
 export default function AskPage() {
@@ -8,9 +10,15 @@ export default function AskPage() {
 	const starters = getThreeRandomStarters();
 
 	return (
-		<div className="flex flex-col gap-10 flex-grow justify-between">
-			<div className="flex flex-col flex-grow gap-10">
-				<div className="flex flex-col flex-grow gap-10" />
+		<div className="flex flex-col gap-4 flex-grow justify-between">
+			<div className="flex flex-col flex-grow justify-center items-center gap-6">
+				<ProemLogo />
+				<div className="text-center">
+					<Header3>Answers based on Scientific</Header3>
+					<Header3>Research</Header3>
+				</div>
+			</div>
+			<div className="flex flex-col gap-10">
 				<div className="flex flex-col gap-2">
 					<div className="flex justify-end">
 						<SelectContentSelector
