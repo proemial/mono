71c6c983a805c342a99b63d3ccb8a0876f9292cc
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-04-18 16:31:32 +0200

    reuse shared FullPageDrawer on privacy & terms modal

--- apps/proem/app/@modal/(.)privacy/page.tsx
+++ apps/proem/app/@modal/(.)privacy/page.tsx
@@ -1,36 +1,12 @@
-"use client"
-import {
-    Button,
-    Drawer,
-    DrawerContent,
-    DrawerHeader,
-    DrawerTitle,
-} from "@proemial/shadcn-ui";
-import { XClose } from "@untitled-ui/icons-react";
-import { useRouter } from "next/navigation";
+"use client";
+
 import Privacy from "@/app/privacy/privacy";
-import { screenMaxWidth } from "@/app/constants";
+import { FullPageDrawer } from "@/components/full-page-drawer";
 
 export default function TermsModal() {
-    const router = useRouter();
-
-    return (
-        <Drawer open>
-            <DrawerContent className={`${screenMaxWidth} w-full h-full mx-auto rounded-none`}>
-                <div className="flex flex-col max-h-full gap-6">
-                    <DrawerHeader className="pt-0 grow-0">
-                        <DrawerTitle className="flex justify-between text-2xl font-normal">
-                            <div>Privacy policy</div>
-                            <Button variant="ghost" className="p-0" onClick={() => router.back()}>
-                                <XClose className="w-6 h-6" />
-                            </Button>
-                        </DrawerTitle>
-                    </DrawerHeader>
-                    <div className="grow px-8 overflow-auto">
-                        <Privacy />
-                    </div>
-                </div>
-            </DrawerContent>
-        </Drawer>
-    );
+	return (
+		<FullPageDrawer title="Privacy policy">
+			<Privacy />
+		</FullPageDrawer>
+	);
 }
--- apps/proem/app/@modal/(.)terms/page.tsx
+++ apps/proem/app/@modal/(.)terms/page.tsx
@@ -1,36 +1,12 @@
-"use client"
-import {
-    Button,
-    Drawer,
-    DrawerContent,
-    DrawerHeader,
-    DrawerTitle,
-} from "@proemial/shadcn-ui";
-import Terms from "../../terms/terms";
-import { XClose } from "@untitled-ui/icons-react";
-import { useRouter } from "next/navigation";
-import { screenMaxWidth } from "@/app/constants";
+"use client";
+
+import Terms from "@/app/terms/terms";
+import { FullPageDrawer } from "@/components/full-page-drawer";
 
 export default function TermsModal() {
-    const router = useRouter();
-
-    return (
-        <Drawer open>
-            <DrawerContent className={`${screenMaxWidth} w-full h-full mx-auto rounded-none`}>
-                <div className="flex flex-col">
-                    <DrawerHeader className="pt-0">
-                        <DrawerTitle className="flex justify-between text-2xl font-normal">
-                            <div>Proemial Terms of Service</div>
-                            <Button variant="ghost" className="p-0" onClick={() => router.back()}>
-                                <XClose className="w-6 h-6" />
-                            </Button>
-                        </DrawerTitle>
-                    </DrawerHeader>
-                    <div className="px-8">
-                        <Terms />
-                    </div>
-                </div>
-            </DrawerContent>
-        </Drawer>
-    );
+	return (
+		<FullPageDrawer title="Proemial Terms of Service">
+			<Terms />
+		</FullPageDrawer>
+	);
 }
