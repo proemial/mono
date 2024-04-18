d16fc96865596e726b9aba2363d5dc7a2d117940
proemian <brian@proemial.ai>
2024-04-18 14:38:32 +0200

    parameterize verbose llm output

--- apps/proem/app/llm/models/model-options.ts
+++ apps/proem/app/llm/models/model-options.ts
@@ -6,12 +6,15 @@ type CommonModelOptions = AsyncCallerParams & {
 	verbose?: boolean;
 };
 
+const dev = process.env.NODE_ENV === "development";
+const verbose = process.env.VERBOSE_LLM === "true";
+
 export const COMMON_MODEL_DEFAULTS: CommonModelOptions = {
 	temperature: 0.8,
-	cache: process.env.NODE_ENV === "development" ? false : true,
-	verbose: process.env.NODE_ENV === "development" ? true : false,
-	maxConcurrency: process.env.NODE_ENV === "development" ? 1 : undefined,
-	maxRetries: process.env.NODE_ENV === "development" ? 0 : undefined,
+	cache: dev ? false : true,
+	verbose: dev && verbose ? true : false,
+	maxConcurrency: dev ? 1 : undefined,
+	maxRetries: dev ? 0 : undefined,
 	onFailedAttempt: (error) => {
 		console.error(error);
 	},
