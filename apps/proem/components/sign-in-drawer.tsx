0bee956849358d1e88545e84d4b6479290abb554
Jon Sorensen <jon@proemial.ai>
2024-04-19 11:16:00 +0200

    underline terms and privacy links

--- apps/proem/components/sign-in-drawer.tsx
+++ apps/proem/components/sign-in-drawer.tsx
@@ -102,8 +102,14 @@ export function SignInDrawer() {
 							</div>
 							<div className="text-gray-400 text-sm">
 								By using Proem, you consent to our{" "}
-								<Link href="/privacy">Privacy Policy</Link> and{" "}
-								<Link href="/terms">Terms of Service</Link>.
+								<Link href="/privacy" className="underline">
+									Privacy Policy
+								</Link>{" "}
+								and{" "}
+								<Link href="/terms" className="underline">
+									Terms of Service
+								</Link>
+								.
 							</div>
 						</div>
 					</div>
