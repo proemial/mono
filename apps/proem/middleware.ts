ba84a9b2f757383c78fc1e12f7f3c6629ea00f90
Jon Sorensen <jon@proemial.ai>
2024-04-22 13:51:44 +0200

    add ratelimiter (prod-only)

--- apps/proem/middleware.ts
+++ apps/proem/middleware.ts
@@ -2,12 +2,26 @@ import { authMiddleware } from "@clerk/nextjs";
 import { geolocation } from "@vercel/edge";
 import { NextResponse } from "next/server";
 import { vercelRegions } from "./app/components/analytics/analytics-keys";
+import { buildRatelimiter } from "./ratelimiter";
 
 // geolocation only works on the edge
 export const runtime = "experimental-edge";
 
+const ratelimiter = buildRatelimiter(5, "10 s");
+
 export default authMiddleware({
 	publicRoutes: ["/(.*)"],
+	beforeAuth: async (request, fetchEvent) => {
+		if (!ratelimiter || !request.ip) {
+			return NextResponse.next();
+		}
+		const { success } = await ratelimiter.limit(request.ip);
+		if (success) {
+			return NextResponse.next();
+		}
+		console.warn(`Warning: Rate limited request from ${request.ip}`);
+		return NextResponse.redirect(new URL("/blocked", request.url));
+	},
 	afterAuth: (auth, request) => {
 		const geo = geolocation(request);
 		const requestHeaders = new Headers(request.headers);
---
+++ apps/proem/ratelimiter.ts
@@ -0,0 +1,28 @@
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+import { z } from "zod";
+
+export const buildRatelimiter = (
+	noOfRequests: number,
+	windowDuration: Parameters<typeof Ratelimit.slidingWindow>["1"],
+) => {
+	const config = getEnvConfig();
+	if (config) {
+		const redis = new Redis({ url: config.endpoint, token: config.password });
+		console.log("Rate limiter configured successfully");
+		return new Ratelimit({
+			redis,
+			limiter: Ratelimit.slidingWindow(noOfRequests, windowDuration),
+		});
+	}
+};
+
+const getEnvConfig = () => {
+	try {
+		const endpoint = z.string().parse(process.env.RATE_LIMITER_ENDPOINT);
+		const password = z.string().parse(process.env.RATE_LIMITER_PASSWORD);
+		return { endpoint, password };
+	} catch (error) {
+		console.log("Rate limiter not configured for this environment");
+	}
+};
