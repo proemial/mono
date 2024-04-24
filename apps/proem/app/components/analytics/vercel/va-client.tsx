828571718ee94b8320e6e2231255a9756b47c15f
proemian <brian@proemial.ai>
2024-04-24 11:20:23 +0200

    fix duplicate view events

--- apps/proem/app/components/analytics/vercel/va-client.tsx
+++ apps/proem/app/components/analytics/vercel/va-client.tsx
@@ -1,27 +1,51 @@
 "use client";
 import { TrackingInput, analyticsTrace, usePathNames } from "@/app/components/analytics/tracking/tracking-profile";
+import { useUser } from "@clerk/nextjs";
 import va from "@vercel/analytics";
 import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 // https://vercel.com/docs/concepts/analytics/custom-events
 export function VaClient({ tracking }: { tracking?: TrackingInput }) {
 	analyticsTrace("[VaClient]");
 
+	const initialized = useInit();
 	const { pathname, trackingKey } = usePathNames();
 
 	useEffect(() => {
-		analyticsTrace("[VaClient] trackPage:", trackingKey, pathname);
-		va.track(trackingKey, {
-			path: pathname,
-		});
-	}, [pathname, trackingKey]);
+		if (initialized) {
+			analyticsTrace("[VaClient] trackPage:", trackingKey, pathname);
+			va.track(trackingKey, {
+				path: pathname,
+			});
+		}
+	}, [initialized, pathname, trackingKey]);
 
 	return (
 		<>
-			<SpeedInsights />
-			<VercelAnalytics />
+			{initialized &&
+				<>
+					<SpeedInsights />
+					<VercelAnalytics />
+				</>
+			}
 		</>
 	);
+}
+
+function useInit() {
+	const { user } = useUser();
+	const [initialized, setInitialized] = useState(false);
+
+	analyticsTrace("[VaClient] useInit");
+
+	useEffect(() => {
+		if (user) {
+			analyticsTrace("[VaClient] initializing");
+			setInitialized(true);
+		}
+	}, [user]);
+
+	return initialized;
 }
