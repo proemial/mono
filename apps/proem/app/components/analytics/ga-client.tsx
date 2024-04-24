fc2a9001b5dbb16ec37abddc439e9bb44734cfda
proemian <brian@proemial.ai>
2024-04-24 10:32:30 +0200

    Add Google TagManager

--- apps/proem/app/components/analytics/ga-client.tsx
+++ apps/proem/app/components/analytics/ga-client.tsx
@@ -3,10 +3,13 @@ import { TrackingInput, analyticsTrace, usePathNames, useTrackingProfile } from 
 import { useUser } from "@clerk/nextjs";
 import { useEffect, useState } from "react";
 import ReactGA from "react-ga4";
+import TagManager from 'react-gtm-module'
 
 // https://www.npmjs.com/package/react-ga4
 export function GaClient({ tracking }: { tracking?: TrackingInput }) {
 	const { trackingProfile } = useTrackingProfile(tracking);
+
+	// Only enabled for registered users or non-eu citizens
 	if (trackingProfile !== "tracked") return null;
 
 	analyticsTrace("[GaClient]");
@@ -35,7 +38,7 @@ function useInit() {
 
 	useEffect(() => {
 		if (user) {
-			analyticsTrace("[GaClient] initializing");
+			analyticsTrace("[GaClient] GA initializing");
 			// const email = user?.primaryEmailAddress?.emailAddress as string;
 			ReactGA.initialize(process.env.NEXT_PUBLIC_GA_ID, {
 				gaOptions: {
@@ -43,9 +46,16 @@ function useInit() {
 				},
 			});
 
+			analyticsTrace("[GaClient] GTM initializing");
+			TagManager.initialize({
+				gtmId: 'GTM-TCS63KTV',
+				dataLayer: {
+					userId: user.id,
+				}
+			})
 			setInitialized(true);
 		}
-	}, [setInitialized, user]);
+	}, [user]);
 
 	return initialized;
 }
