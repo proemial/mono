36b8571ed5dd1c496a8f791e1355293bace04e07
proemian <brian@proemial.ai>
2024-05-08 20:14:27 +0200

    Filter by selected fields

--- apps/proem/app/(pages)/(app)/discover/page.tsx
+++ apps/proem/app/(pages)/(app)/discover/page.tsx
@@ -1,10 +1,21 @@
 import { Feed } from "@/app/(pages)/(app)/discover/feed";
 import { fetchLatestPapers } from "@/app/(pages)/(app)/paper/oa/[id]/fetch-paper";
+import { OaTopics } from "@proemial/models/open-alex-topics";
 import { Loading01 } from "@untitled-ui/icons-react";
 import { Suspense } from "react";
 
-export default async function DiscoverPage() {
-	const fetchedPapersPromise = fetchLatestPapers();
+type Params = {
+	searchParams?: { topic?: string };
+};
+
+export default async function DiscoverPage({ searchParams }: Params) {
+	const topicId = OaTopics.find(
+		(c) =>
+			c.display_name.toLowerCase() ===
+			decodeURI(searchParams?.topic || "").replaceAll("%2C", ","),
+	)?.id;
+
+	const fetchedPapersPromise = fetchLatestPapers(topicId);
 
 	return (
 		<div className="space-y-6">
