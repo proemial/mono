a151a239563f97a7fe8c3e3ec1a7494cfb24bbe4
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-05-13 11:52:00 +0200

    don't enable client to pass limit of fetches to the fetch feed server action

--- apps/proem/app/(pages)/(app)/discover/fetch-feed.ts
+++ apps/proem/app/(pages)/(app)/discover/fetch-feed.ts
@@ -4,9 +4,12 @@ import { fetchPapers } from "@/app/(pages)/(app)/paper/oa/[id]/fetch-paper";
 
 type FetchFeedParams = Required<Parameters<typeof fetchPapers>>;
 
-export async function fetchFeed(...args: FetchFeedParams) {
-	const fetchedPapers = await fetchPapers(...args);
+export async function fetchFeed(
+	params: FetchFeedParams[0],
+	options: Omit<FetchFeedParams[1], "limit">,
+) {
+	const fetchedPapers = await fetchPapers(params, { ...options, limit: 5 });
 	console.log(fetchedPapers.map((p) => p.id));
 
-	return { rows: fetchedPapers, nextOffset: args[1].offset + 1 };
+	return { rows: fetchedPapers, nextOffset: options.offset + 1 };
 }
