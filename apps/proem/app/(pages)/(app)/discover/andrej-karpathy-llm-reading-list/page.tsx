ff8cded4e095702377a4a57a6041853c0e9f6c63
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-05-14 15:07:24 +0200

    update meta data on static feed page

--- apps/proem/app/(pages)/(app)/discover/andrej-karpathy-llm-reading-list/page.tsx
+++ apps/proem/app/(pages)/(app)/discover/andrej-karpathy-llm-reading-list/page.tsx
@@ -1,8 +1,31 @@
 import { fetchReadingList } from "@/app/(pages)/(app)/discover/andrej-karpathy-llm-reading-list/fetch-list";
 import { StaticFeed } from "@/app/(pages)/(app)/discover/andrej-karpathy-llm-reading-list/static-feed";
 import { OpenAlexPaper } from "@proemial/models/open-alex";
+import { Metadata } from "next";
 
 export const dynamic = "force-static";
+
+const title = "Andrej Karpathy's LLM Reading List";
+const url = "https://proem.ai/discover/andrej-karpathy-llm-reading-list";
+const description =
+	"All the papers from Andrej Karpathy's Intro to Large Language Models video.";
+
+export const metadata: Metadata = {
+	title: "Andrej Karpathy's LLM Reading List",
+	description,
+	metadataBase: new URL(url),
+	openGraph: {
+		type: "website",
+		url,
+		title,
+		description,
+		siteName: title,
+	},
+	robots: {
+		index: true,
+		follow: false,
+	},
+};
 
 export default async function AndrejKarpathyLLMReadingList() {
 	const readingList = await fetchReadingList();
