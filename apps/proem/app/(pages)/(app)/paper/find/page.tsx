dfc1bd7c09aefcd7d5e6e6e7f69452be1930c508
proemian <brian@proemial.ai>
2024-05-28 15:42:13 +0200

    more examples

--- apps/proem/app/(pages)/(app)/paper/find/page.tsx
+++ apps/proem/app/(pages)/(app)/paper/find/page.tsx
@@ -35,14 +35,15 @@ export default async function ReaderInputPage() {
 			<div className="mt-2">
 				Examples:
 				<ul className="list-disc mx-8 my-4">
+					<li>https://openalex.org/W4385245566</li>
+					<li className="mb-3">W4385245566</li>
 					<li>https://arxiv.org/abs/1706.03762</li>
 					<li>https://arxiv.org/abs/1706.03762v7</li>
 					<li>1706.03762</li>
-					<li>1706.03762v7</li>
+					<li className="mb-3">1706.03762v7</li>
 					<li>https://doi.org/10.48550/arxiv.2306.04526</li>
-					<li>10.48550/arxiv.2306.04526</li>
+					<li className="mb-3">10.48550/arxiv.2306.04526</li>
 					<li>https://pubmed.ncbi.nlm.nih.gov/37796173</li>
-					<li>W4385245566</li>
 				</ul>
 			</div>
 		</form>
