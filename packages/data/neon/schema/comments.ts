40bc47f19a20ee4fad0b6490a1675aac69f7eab0
Jon Sorensen <jon@proemial.ai>
2024-05-29 11:51:15 +0200

    timestamps on posts and comments

--- packages/data/neon/schema/comments.ts
+++ packages/data/neon/schema/comments.ts
@@ -1,10 +1,11 @@
 import { relations } from "drizzle-orm";
-import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
+import { integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 import { posts } from "./posts";
 
 export const comments = pgTable("comments", {
 	id: serial("id").primaryKey(),
 	content: text("content"),
+	createdAt: timestamp("created_at").notNull().defaultNow(),
 	authorId: text("author_id"),
 	postId: integer("post_id"),
 });
--- packages/data/neon/schema/posts.ts
+++ packages/data/neon/schema/posts.ts
@@ -1,5 +1,5 @@
 import { relations } from "drizzle-orm";
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 import { comments } from "./comments";
 import { papers } from "./papers";
 import { users } from "./users";
@@ -7,6 +7,7 @@ import { users } from "./users";
 export const posts = pgTable("posts", {
 	id: serial("id").primaryKey(),
 	content: text("content").notNull(),
+	createdAt: timestamp("created_at").notNull().defaultNow(),
 	authorId: text("author_id").notNull(),
 	paperId: text("paper_id").notNull(),
 });
