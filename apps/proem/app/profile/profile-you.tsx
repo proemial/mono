5c2a6a3673bd9f6b48010df9beaa3a5021207191
Jon Sorensen <jon@proemial.ai>
2024-06-04 12:23:39 +0200

    show user avatar in drawer

--- apps/proem/app/profile/profile-you.tsx
+++ apps/proem/app/profile/profile-you.tsx
@@ -5,7 +5,11 @@ import {
 } from "@/components/analytics/tracking/tracking-keys";
 import { useClerk, useOrganization, useUser } from "@clerk/nextjs";
 import {
+	Avatar,
+	AvatarFallback,
+	AvatarImage,
 	Header2,
+	Header5,
 	Icons,
 	Table,
 	TableBody,
@@ -19,92 +23,125 @@ import { About } from "./about";
 import { ProfileColorSchemeToggle } from "./profile-color-scheme-toggle";
 
 export function ProfileYou() {
-	const { user } = useUser();
+	const { user, isSignedIn } = useUser();
 	const { signOut } = useClerk();
 	const { membership } = useOrganization();
 
 	return (
-		<div className="space-y-4">
-			<div>
+		<div className="space-y-8">
+			<div className="space-y-6">
 				<div className="select-none">
 					<Header2>Your profile</Header2>
 				</div>
-				<Table className="text-base">
-					<TableBody>
-						{membership && (
-							<TableRow>
-								<TableCell variant="icon">
-									<Icons.organization className="mx-auto size-4" />
-								</TableCell>
-								<TableCell variant="key" className="select-none">
-									Organization
-								</TableCell>
-								<TableCell variant="value">
-									<Link
-										href={`/org/${membership.organization.id}`}
-										onClick={trackHandler(analyticsKeys.ui.menu.click.org)}
-										prefetch={false}
-										className="flex items-center gap-1 mr-1"
-									>
-										{membership.organization.name}
-									</Link>
-								</TableCell>
-							</TableRow>
-						)}
-						{!user && (
-							<SignInDrawer
-								trigger={
+				<div className="space-y-8">
+					{isSignedIn && user && (
+						<div className="space-y-2">
+							<Header5>
+								<div className="opacity-50">Profile</div>
+							</Header5>
+							<div className="flex gap-4 justify-between items-center">
+								<Avatar className="size-9">
+									<AvatarImage src={user.imageUrl} />
+									<AvatarFallback>
+										{getUserInitials(user.fullName ?? "")}
+									</AvatarFallback>
+								</Avatar>
+								<div>{user.fullName}</div>
+							</div>
+						</div>
+					)}
+					<div>
+						<Header5>
+							<div className="opacity-50">Social</div>
+						</Header5>
+						<Table className="text-base">
+							<TableBody>
+								{membership && (
 									<TableRow>
 										<TableCell variant="icon">
-											<LogIn01 className="mx-auto size-4" />
+											<Icons.organization className="mx-auto size-4" />
+										</TableCell>
+										<TableCell variant="key" className="select-none">
+											Organization
+										</TableCell>
+										<TableCell variant="value">
+											<Link
+												href={`/org/${membership.organization.id}`}
+												onClick={trackHandler(analyticsKeys.ui.menu.click.org)}
+												prefetch={false}
+												className="flex items-center gap-1"
+											>
+												{membership.organization.name}
+											</Link>
+										</TableCell>
+									</TableRow>
+								)}
+								{!isSignedIn && (
+									<SignInDrawer
+										trigger={
+											<TableRow>
+												<TableCell variant="icon">
+													<LogIn01 className="mx-auto size-4" />
+												</TableCell>
+												<TableCell variant="key" className="flex">
+													<div
+														className="cursor-pointer"
+														onClick={trackHandler(
+															analyticsKeys.ui.menu.click.signin,
+														)}
+													>
+														Sign in
+													</div>
+												</TableCell>
+											</TableRow>
+										}
+									/>
+								)}
+								<TableRow>
+									<TableCell variant="icon">
+										<Drop className="mx-auto size-4" />
+									</TableCell>
+									<TableCell variant="key" className="select-none">
+										Color scheme
+									</TableCell>
+									<TableCell variant="value">
+										<ProfileColorSchemeToggle />
+									</TableCell>
+								</TableRow>
+								{isSignedIn && (
+									<TableRow>
+										<TableCell variant="icon">
+											{user && <LogOut01 className="mx-auto size-4" />}
 										</TableCell>
 										<TableCell variant="key" className="flex">
 											<div
+												onClick={() => {
+													trackHandler(analyticsKeys.ui.menu.click.signout)();
+													signOut();
+												}}
 												className="cursor-pointer"
-												onClick={trackHandler(
-													analyticsKeys.ui.menu.click.signin,
-												)}
 											>
-												Sign in
+												Sign out
 											</div>
 										</TableCell>
 									</TableRow>
-								}
-							/>
-						)}
-						<TableRow>
-							<TableCell variant="icon">
-								<Drop className="mx-auto size-4" />
-							</TableCell>
-							<TableCell variant="key" className="select-none">
-								Color scheme
-							</TableCell>
-							<TableCell variant="value">
-								<ProfileColorSchemeToggle />
-							</TableCell>
-						</TableRow>
-						{user && (
-							<TableRow>
-								<TableCell variant="icon">
-									{user && <LogOut01 className="mx-auto size-4" />}
-								</TableCell>
-								<TableCell variant="key" className="flex">
-									<div
-										onClick={() => {
-											trackHandler(analyticsKeys.ui.menu.click.signout)();
-											signOut();
-										}}
-										className="cursor-pointer"
-									>
-										Sign out
-									</div>
-								</TableCell>
-							</TableRow>
-						)}
-					</TableBody>
-				</Table>
+								)}
+							</TableBody>
+						</Table>
+					</div>
+				</div>
 			</div>
 			<About />
 		</div>
 	);
 }
+
+const getUserInitials = (fullname: string) => {
+	const names = fullname.split(" ");
+	if (names.length === 1) {
+		return names[0]?.charAt(0).toUpperCase();
+	}
+	// @ts-ignore: Length of `names` is guaranteed to be at least 2
+	const initials = names[0]?.charAt(0) + names[names.length - 1].charAt(0);
+	return initials.toUpperCase();
+};
