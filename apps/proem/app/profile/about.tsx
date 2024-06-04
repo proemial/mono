ed302c16c6702b97d2e3e4dd27ce7b309368066b
Jon Sorensen <jon@proemial.ai>
2024-06-04 10:59:48 +0200

    rearrange drawer menu

---
+++ apps/proem/app/profile/about.tsx
@@ -0,0 +1,145 @@
+"use client";
+
+import {
+	analyticsKeys,
+	trackHandler,
+} from "@/components/analytics/tracking/tracking-keys";
+import {
+	Collapsible,
+	CollapsibleContent,
+	CollapsibleTrigger,
+	Header4,
+	Table,
+	TableBody,
+	TableCell,
+	TableRow,
+} from "@proemial/shadcn-ui";
+import {
+	ChevronDown,
+	ChevronUp,
+	ClipboardCheck,
+	File02,
+	Lock01,
+	MessageSquare02,
+} from "@untitled-ui/icons-react";
+import Link from "next/link";
+import * as React from "react";
+
+const feedback = "https://tally.so/r/wAv8Ve";
+
+export const About = () => {
+	const [isOpen, setIsOpen] = React.useState(false);
+
+	return (
+		<Collapsible open={isOpen} onOpenChange={setIsOpen}>
+			<CollapsibleTrigger className="w-full">
+				<div className="flex gap-2 justify-between items-center my-2">
+					<Header4>About</Header4>
+					<div>
+						{isOpen ? (
+							<ChevronUp className="w-4 h-4" />
+						) : (
+							<ChevronDown className="w-4 h-4" />
+						)}
+					</div>
+				</div>
+			</CollapsibleTrigger>
+			<CollapsibleContent>
+				<Table className="text-base">
+					<TableBody>
+						<TableRow>
+							<TableCell variant="icon">
+								<MessageSquare02 className="mx-auto size-4" />
+							</TableCell>
+							<TableCell variant="key">
+								<Feedback />
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell variant="icon">
+								<File02 className="mx-auto size-4" />
+							</TableCell>
+							<TableCell variant="key">
+								<Link
+									href="/terms"
+									onClick={trackHandler(analyticsKeys.ui.menu.click.terms)}
+									prefetch={false}
+								>
+									Terms of use
+								</Link>
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell variant="icon">
+								<Lock01 className="mx-auto size-4" />
+							</TableCell>
+							<TableCell variant="key">
+								<Link
+									href="/privacy"
+									onClick={trackHandler(analyticsKeys.ui.menu.click.privacy)}
+									prefetch={false}
+								>
+									Privacy policy
+								</Link>
+							</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell variant="icon">
+								<ClipboardCheck className="mx-auto size-4" />
+							</TableCell>
+							<TableCell
+								variant="key"
+								className="w-full flex items-center gap-4 select-none"
+							>
+								<Version />
+							</TableCell>
+						</TableRow>
+					</TableBody>
+				</Table>
+			</CollapsibleContent>
+		</Collapsible>
+	);
+};
+
+function Feedback() {
+	return (
+		<a
+			href={feedback}
+			target="_blank"
+			rel="noreferrer"
+			onClick={trackHandler(analyticsKeys.ui.menu.click.feedback)}
+		>
+			Give feedback
+		</a>
+	);
+}
+
+function Version() {
+	const version = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA ?? "local";
+	return (
+		// Check if anyone is actually clicking this
+		<div
+			className="flex gap-2 w-full justify-between items-center"
+			onClick={trackHandler(analyticsKeys.ui.menu.click.version)}
+		>
+			<div>Version</div>
+			<div className="flex gap-2 items-center">
+				<pre className="text-xs">{version.substring(0, 7)}</pre>
+				<Beta />
+			</div>
+		</div>
+	);
+}
+
+function Beta() {
+	return (
+		<span
+			className="px-1.5 py-1 text-xs font-bold rounded-md text-secondary-foreground bg-secondary"
+			onClick={trackHandler(analyticsKeys.ui.menu.click.version, {
+				value: "button",
+			})}
+		>
+			BETA
+		</span>
+	);
+}
--- apps/proem/app/profile/profile-you.tsx
+++ apps/proem/app/profile/profile-you.tsx
@@ -1,123 +1,36 @@
 "use client";
-import { ProfileColorSchemeToggle } from "@/app/profile/profile-color-scheme-toggle";
 import {
 	analyticsKeys,
 	trackHandler,
 } from "@/components/analytics/tracking/tracking-keys";
 import { useClerk, useOrganization, useUser } from "@clerk/nextjs";
 import {
-	Collapsible,
-	CollapsibleContent,
+	Header2,
 	Icons,
 	Table,
 	TableBody,
 	TableCell,
 	TableRow,
 } from "@proemial/shadcn-ui";
-import {
-	ClipboardCheck,
-	Drop,
-	File02,
-	Lock01,
-	LogIn01,
-	LogOut01,
-	MessageSquare02,
-} from "@untitled-ui/icons-react";
+import { Drop, LogIn01, LogOut01 } from "@untitled-ui/icons-react";
 import Link from "next/link";
-import * as React from "react";
 import { SignInDrawer } from "../../components/sign-in-drawer";
-
-const feedback = "https://tally.so/r/wAv8Ve";
+import { About } from "./about";
+import { ProfileColorSchemeToggle } from "./profile-color-scheme-toggle";
 
 export function ProfileYou() {
-	const [isOpen, setIsOpen] = React.useState(true);
 	const { user } = useUser();
 	const { signOut } = useClerk();
 	const { membership } = useOrganization();
 
 	return (
-		<Collapsible open={isOpen} onOpenChange={setIsOpen}>
-			{/* <CollapsibleTrigger className="w-full">
-				<div className="flex items-center place-content-between">
-					<div className="flex items-center gap-4">
-						<Avatar className="size-6">
-							<AvatarImage src="https://github.com/shadcn.png" />
-							<AvatarFallback>CN</AvatarFallback>
-						</Avatar>
-						<Header4>You</Header4>
-					</div>
-					<div>
-						{isOpen ? (
-							<ChevronUp className="w-4 h-4" />
-						) : (
-							<ChevronDown className="w-4 h-4" />
-						)}
-					</div>
+		<div className="space-y-4">
+			<div>
+				<div className="select-none">
+					<Header2>Your profile</Header2>
 				</div>
-			</CollapsibleTrigger> */}
-			<CollapsibleContent>
 				<Table className="text-base">
 					<TableBody>
-						{/* <TableRow>
-							<TableCell variant="icon">
-								<Mail className="mx-auto size-4" />
-							</TableCell>
-							<TableCell variant="key">Email</TableCell>
-							<TableCell variant="value">johnconner@proemial.ai</TableCell>
-						</TableRow> */}
-						<TableRow>
-							<TableCell variant="icon">
-								<Drop className="mx-auto size-4" />
-							</TableCell>
-							<TableCell variant="key">Color Scheme</TableCell>
-							<TableCell variant="value">
-								<ProfileColorSchemeToggle />
-							</TableCell>
-						</TableRow>
-						<TableRow>
-							<TableCell variant="icon">
-								<MessageSquare02 className="mx-auto size-4" />
-							</TableCell>
-							<TableCell variant="key">
-								<Feedback />
-							</TableCell>
-						</TableRow>
-						<TableRow>
-							<TableCell variant="icon">
-								<ClipboardCheck className="mx-auto size-4" />
-							</TableCell>
-							<TableCell variant="key" className="flex items-center gap-4">
-								<Version />
-							</TableCell>
-						</TableRow>
-						<TableRow>
-							<TableCell variant="icon">
-								<File02 className="mx-auto size-4" />
-							</TableCell>
-							<TableCell variant="key">
-								<Link
-									href="/terms"
-									onClick={trackHandler(analyticsKeys.ui.menu.click.terms)}
-									prefetch={false}
-								>
-									Terms of Use
-								</Link>
-							</TableCell>
-						</TableRow>
-						<TableRow>
-							<TableCell variant="icon">
-								<Lock01 className="mx-auto size-4" />
-							</TableCell>
-							<TableCell variant="key">
-								<Link
-									href="/privacy"
-									onClick={trackHandler(analyticsKeys.ui.menu.click.privacy)}
-									prefetch={false}
-								>
-									Privacy Policy
-								</Link>
-							</TableCell>
-						</TableRow>
 						{membership && (
 							<TableRow>
 								<TableCell variant="icon">
@@ -170,45 +83,21 @@ export function ProfileYou() {
 								}
 							/>
 						)}
+						<TableRow>
+							<TableCell variant="icon">
+								<Drop className="mx-auto size-4" />
+							</TableCell>
+							<TableCell variant="key" className="select-none">
+								Color scheme
+							</TableCell>
+							<TableCell variant="value">
+								<ProfileColorSchemeToggle />
+							</TableCell>
+						</TableRow>
 					</TableBody>
 				</Table>
-			</CollapsibleContent>
-		</Collapsible>
-	);
-}
-
-function Feedback() {
-	return (
-		<a
-			href={feedback}
-			target="_blank"
-			rel="noreferrer"
-			onClick={trackHandler(analyticsKeys.ui.menu.click.feedback)}
-		>
-			Give feedback
-		</a>
-	);
-}
-
-function Version() {
-	const version = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA ?? "local";
-	return (
-		// Check if anyone is actually clicking this
-		<div onClick={trackHandler(analyticsKeys.ui.menu.click.version)}>
-			Version: {version.substring(0, 7)} <Beta />
+			</div>
+			<About />
 		</div>
-	);
-}
-
-function Beta() {
-	return (
-		<span
-			className="px-2 py-1.5 text-xs font-bold rounded-md text-secondary-foreground bg-secondary"
-			onClick={trackHandler(analyticsKeys.ui.menu.click.version, {
-				value: "button",
-			})}
-		>
-			BETA
-		</span>
 	);
 }
