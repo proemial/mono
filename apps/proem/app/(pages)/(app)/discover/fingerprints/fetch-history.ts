122a8bcda6e3f51f99bbe0da21b187da616fc4d6
proemian <brian@proemial.ai>
2024-06-06 11:11:05 +0200

    Prefer papers with multiple reads

--- apps/proem/app/(pages)/(app)/discover/fingerprints/fetch-history.ts
+++ apps/proem/app/(pages)/(app)/discover/fingerprints/fetch-history.ts
@@ -1,45 +1,7 @@
 import { auth } from "@clerk/nextjs/server";
 import { neonDb } from "@proemial/data";
 import { eq } from "drizzle-orm";
-import { users } from "@proemial/data/neon/schema";
-
-const mock = [
-	{
-		paperId: "W4399270899",
-		noOfReads: 6,
-		lastReadAt: "2024-06-03T10:21:05.913Z",
-	},
-	{
-		paperId: "W4399270903",
-		noOfReads: 4,
-		lastReadAt: "2024-06-03T10:20:18.402Z",
-	},
-	{
-		paperId: "W4399325367",
-		noOfReads: 4,
-		lastReadAt: "2024-06-06T07:21:00.924Z",
-	},
-	{
-		paperId: "W4399323734",
-		noOfReads: 3,
-		lastReadAt: "2024-06-05T09:43:06.182Z",
-	},
-	{
-		paperId: "W4399270945",
-		noOfReads: 3,
-		lastReadAt: "2024-06-03T10:21:22.076Z",
-	},
-	{
-		paperId: "W4379958454",
-		noOfReads: 2,
-		lastReadAt: "2024-05-31T11:22:44.111Z",
-	},
-	{
-		paperId: "W4399319410",
-		noOfReads: 1,
-		lastReadAt: "2024-06-06T07:22:21.176Z",
-	},
-];
+import { PaperActivity, users } from "@proemial/data/neon/schema";
 
 export async function getHistory() {
 	const ids = [];
@@ -49,7 +11,8 @@ export async function getHistory() {
 		const user = await neonDb.query.users.findFirst({
 			where: eq(users.id, userId),
 		});
-		const papers = user?.paperActivities.slice(0, 10);
+
+		const papers = filterPapers(user?.paperActivities);
 		if (papers) {
 			for (const paper of papers) {
 				ids.push(paper.paperId);
@@ -57,6 +20,16 @@ export async function getHistory() {
 		}
 	}
 
-	// return mock;
 	return ids;
+}
+
+function filterPapers(papers?: PaperActivity[]) {
+	const maxCount = 10;
+
+	const multiples = papers?.filter((p) => p.noOfReads > 1);
+	if (multiples?.length) {
+		return multiples.slice(0, maxCount);
+	}
+
+	return papers?.slice(0, maxCount);
 }
