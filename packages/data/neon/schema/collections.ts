0c1d753082a9d6a655dd43a33f5a00b980157670
Jon Sorensen <jon@proemial.ai>
2024-06-10 15:17:54 +0200

    generate slug automatically on insert

--- packages/data/neon/schema/collections.ts
+++ packages/data/neon/schema/collections.ts
@@ -1,10 +1,14 @@
 import { relations } from "drizzle-orm";
 import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { nanoid } from "nanoid";
 import { collectionsToPapers } from "./collections-to-papers";
 
 export const collections = pgTable("collections", {
 	id: serial("id").primaryKey(),
-	slug: text("slug").notNull().unique(),
+	slug: text("slug")
+		.notNull()
+		.unique()
+		.$defaultFn(() => nanoid()),
 	ownerId: text("owner_id").notNull(),
 	name: text("name").notNull(),
 	description: text("description"),
