59c4e0b10ec41061a90782bf5f70ae9fe400c4ed
proemian <brian@proemial.ai>
2024-06-11 10:43:17 +0200

    add search filter

--- apps/proem/app/search/find-paper-ids.ts
+++ apps/proem/app/search/find-paper-ids.ts
@@ -1,14 +1,25 @@
 "use server";
 
 import { OpenAlexMeta } from "@proemial/models/open-alex";
+import dayjs from "dayjs";
 
 export async function findPaperIds(query: string) {
 	if (!query.length) {
 		return [];
 	}
 
+	const today = dayjs().format("YYYY-MM-DD");
+	const oaFilter = [
+		"type:types/preprint|types/article",
+		"has_abstract:true",
+		`publication_date:<${today}`, // We do not want papers published in the future
+		"language:en",
+		"open_access.is_oa:true",
+	]
+		.filter((f) => !!f)
+		.join(",");
 	const search = await fetch(
-		`https://api.openalex.org/works?select=id&filter=title.search:${query}`,
+		`https://api.openalex.org/works?select=id&filter=${oaFilter},title.search:${query}`,
 	);
 
 	const json = (await search.json()) as {
