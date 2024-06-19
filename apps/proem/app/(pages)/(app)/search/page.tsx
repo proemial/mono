1f84a46e370787ac762a00729616686569238792
Jon Sorensen <jon@proemial.ai>
2024-06-19 12:20:53 +0200

    fix: missing padding on search page

--- apps/proem/app/(pages)/(app)/search/page.tsx
+++ apps/proem/app/(pages)/(app)/search/page.tsx
@@ -1,4 +1,5 @@
 import { getInternalUser } from "@/app/hooks/get-internal-user";
+import { Main } from "@/components/main";
 import { GoBackAction } from "@/components/nav-bar/actions/go-back-action";
 import { SimpleHeader } from "@/components/nav-bar/headers/simple-header";
 import { NavBarV2 } from "@/components/nav-bar/nav-bar-v2";
@@ -23,20 +24,22 @@ export default async function SearchPage({ searchParams }: Props) {
 			<NavBarV2 action={<GoBackAction />} isInternalUser={isInternal}>
 				<SimpleHeader title="Search" />
 			</NavBarV2>
-			<div className="space-y-6">
-				<SearchForm
-					placeholder="Search for a paper"
-					trackingPrefix="search"
-					value={query}
-				/>
-				{paperIds.map((id, i) => (
-					<div key={i} className="py-5">
-						<Suspense fallback={<div>Loading...</div>}>
-							<Paper id={id} />
-						</Suspense>
-					</div>
-				))}
-			</div>
+			<Main>
+				<div className="space-y-6">
+					<SearchForm
+						placeholder="Search for a paper"
+						trackingPrefix="search"
+						value={query}
+					/>
+					{paperIds.map((id, i) => (
+						<div key={i} className="py-5">
+							<Suspense fallback={<div>Loading...</div>}>
+								<Paper id={id} />
+							</Suspense>
+						</div>
+					))}
+				</div>
+			</Main>
 		</>
 	);
 }
