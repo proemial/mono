01c1784458fdbb144585e3f3966766a109474fa0
Jon Sorensen <jon@proemial.ai>
2024-06-19 12:18:30 +0200

    show "your collection" instead of "for you"
    Acknowledged by design/Thorbjørn.

--- apps/proem/components/nav-bar/headers/select-space-header.tsx
+++ apps/proem/components/nav-bar/headers/select-space-header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { PERSONAL_DEFAULT_COLLECTION_NAME } from "@/app/constants";
 import { getCollections } from "@/app/profile/actions";
 import { useUser } from "@clerk/nextjs";
 import {
@@ -11,10 +12,8 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from "@proemial/shadcn-ui";
-import { ChevronRight } from "@untitled-ui/icons-react";
 import { useParams, useRouter } from "next/navigation";
 import { useQuery } from "react-query";
-import { SimpleHeader } from "./simple-header";
 
 export const SelectSpaceHeader = () => {
 	const { user } = useUser();
@@ -27,26 +26,21 @@ export const SelectSpaceHeader = () => {
 		enabled: !!user?.id,
 	});
 
-	const collectionName = collections?.find(
-		(collection) => collection.slug === params.id,
-	)?.name;
+	const collectionName =
+		collections?.find((collection) => collection.slug === params.id)?.name ??
+		PERSONAL_DEFAULT_COLLECTION_NAME;
+	const defaultCollectionId = user?.id;
 
 	const handleValueChange = (value: string) => {
 		router.push(`/collection/${value}`);
 	};
 
-	if (!collectionName) {
-		return (
-			<div className="flex gap-2 items-center">
-				<SimpleHeader title="For You" />
-				<ChevronRight className="size-4" />
-			</div>
-		);
-	}
-
 	return (
 		<div className="flex gap-1 items-center">
-			<Select onValueChange={handleValueChange} value={params.id as string}>
+			<Select
+				onValueChange={handleValueChange}
+				value={(params.id as string) ?? defaultCollectionId}
+			>
 				<SelectTrigger className="border-none flex gap-2 text-lg">
 					<SelectValue placeholder={collectionName} />
 				</SelectTrigger>
