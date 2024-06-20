d6d8343f5e2391f311a3a60caac697ebcd5fcaa4
Jon Sorensen <jon@proemial.ai>
2024-06-20 11:34:10 +0200

    fix: missing default collection title if user has another collection
    Applies to the case where the default collection does not exist, but
    another collection does. E.g. the user has created one, but not yet used
    the default collection.

--- apps/proem/components/nav-bar/headers/select-space-header.tsx
+++ apps/proem/components/nav-bar/headers/select-space-header.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import {
-	PERSONAL_DEFAULT_COLLECTION_NAME,
-	getPersonalDefaultCollection,
-} from "@/app/constants";
+import { getPersonalDefaultCollection } from "@/app/constants";
 import { Collection } from "@proemial/data/neon/schema";
 import {
 	Select,
@@ -15,7 +12,6 @@ import {
 	SelectValue,
 } from "@proemial/shadcn-ui";
 import { useParams, useRouter } from "next/navigation";
-import { SimpleHeader } from "./simple-header";
 
 type Props = {
 	collections: Collection[];
@@ -25,29 +21,25 @@ type Props = {
 export const SelectSpaceHeader = ({ collections, userId }: Props) => {
 	const params = useParams();
 	const router = useRouter();
-
-	const selectedCollection =
-		collections.find((collection) => collection.slug === params.id) ??
-		getPersonalDefaultCollection(userId);
+	const allCollections = ensureDefaultCollection(collections, userId);
 
 	const handleValueChange = (value: string) => {
 		router.push(`/collection/${value}`);
 	};
 
-	if (collections.length === 0) {
-		return <SimpleHeader title={PERSONAL_DEFAULT_COLLECTION_NAME} />;
-	}
-
 	return (
 		<div className="flex gap-1 items-center">
-			<Select onValueChange={handleValueChange} value={selectedCollection.slug}>
+			<Select
+				onValueChange={handleValueChange}
+				value={(params.id as string | undefined) ?? userId}
+			>
 				<SelectTrigger className="border-none flex gap-2 text-lg">
 					<SelectValue />
 				</SelectTrigger>
 				<SelectContent>
 					<SelectGroup>
 						<SelectLabel>Switch Space</SelectLabel>
-						{collections?.map((collection) => (
+						{allCollections.map((collection) => (
 							<SelectItem key={collection.id} value={collection.slug}>
 								{collection.name}
 							</SelectItem>
@@ -57,4 +49,14 @@ export const SelectSpaceHeader = ({ collections, userId }: Props) => {
 			</Select>
 		</div>
 	);
+};
+
+const ensureDefaultCollection = (collections: Collection[], userId: string) => {
+	const existingDefaultCollection = collections.find(
+		(collection) => collection.slug === userId,
+	);
+	if (existingDefaultCollection) {
+		return collections;
+	}
+	return [getPersonalDefaultCollection(userId), ...collections];
 };
