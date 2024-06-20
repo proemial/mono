5c8d206e61795cd8a6c37d46fca23fdd6b13ac59
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-06-20 10:34:45 +0200

    refactor InfinityScrollList from feed

--- apps/proem/app/(pages)/(app)/discover/feed.tsx
+++ apps/proem/app/(pages)/(app)/discover/feed.tsx
@@ -1,164 +1,36 @@
 "use client";
-import FeedItem, {
-	FeedItemProps,
-} from "@/app/(pages)/(app)/discover/feed-item";
-import { fetchFeedByTopic } from "@/app/(pages)/(app)/discover/fetch-feed";
-import { fetchFeedByFeatures } from "@/app/data/fetch-feed";
+import { FeedItemProps } from "@/app/(pages)/(app)/discover/feed-item";
+import { FeatureBadge } from "@/components/feature-badges";
 import {
-	analyticsKeys,
-	trackHandler,
-} from "@/components/analytics/tracking/tracking-keys";
-import { FeatureBadge, FeatureCloud } from "@/components/feature-badges";
-import { RankedFeature } from "@proemial/repositories/oa/fingerprinting/features";
-import { RankedPaper } from "@proemial/repositories/oa/fingerprinting/rerank";
-import { Icons } from "@proemial/shadcn-ui";
-import { useWindowVirtualizer } from "@tanstack/react-virtual";
-import { ReactNode, useEffect } from "react";
-import { useInfiniteQuery } from "react-query";
+	InfinityScollListProps,
+	InfinityScrollList,
+} from "@/components/infinite-list";
+import { ReactNode } from "react";
 
-// 1-4 is fetched without scrolling
-const initialPageSize = 4;
+type FeedProps = Pick<FeedItemProps, "bookmarks"> &
+	Pick<InfinityScollListProps, "filter" | "debug" | "nocache"> & {
+		children: ReactNode;
+		debug?: boolean;
+	};
 
-const Loader = () => (
-	<div className="w-full h-24 flex justify-center items-center">
-		<Icons.loader />
-	</div>
-);
-
-type Props = Pick<FeedItemProps, "bookmarks"> & {
-	children: ReactNode;
-	filter: { topic?: number; features?: RankedFeature[]; days?: number };
-	debug?: boolean;
-	nocache?: boolean;
-};
-
-export function Feed({ children, filter, debug, nocache, bookmarks }: Props) {
-	const { topic, features, days } = filter;
-
-	const {
-		status,
-		data,
-		isFetchingNextPage,
-		fetchNextPage,
-		hasNextPage,
-		error,
-	} = useInfiniteQuery(
-		topic
-			? `feed_${topic}`
-			: `filter_${days}:${features?.map((f) => f.id).join("|")}`,
-		(ctx) => {
-			const nextOffset = ctx.pageParam;
-			if (nextOffset > initialPageSize) {
-				trackHandler(analyticsKeys.feed.scroll.fetch, {
-					offset: `${nextOffset - initialPageSize}`,
-				})();
-			}
-
-			if (features?.length) {
-				return fetchFeedByFeatures(
-					{ features, days },
-					{ offset: ctx.pageParam },
-					nocache,
-				);
-			}
-			return fetchFeedByTopic({ field: topic }, { offset: ctx.pageParam });
-		},
-		{
-			getNextPageParam: (lastGroup) => {
-				return lastGroup?.nextOffset;
-			},
-			refetchOnWindowFocus: false,
-		},
-	);
-
-	const allRows = data ? data.pages?.flatMap((d) => d.rows) : [];
-	const count = data?.pages.at(0)?.count;
-
-	const rowVirtualizer = useWindowVirtualizer({
-		count: hasNextPage ? allRows.length + 1 : allRows.length,
-		estimateSize: () => 160,
-		overscan: 5,
-	});
-	const items = rowVirtualizer.getVirtualItems();
-
-	useEffect(() => {
-		const [lastItem] = [...items].reverse();
-
-		if (!lastItem) {
-			return;
-		}
-
-		if (
-			lastItem.index >= allRows.length - 1 &&
-			hasNextPage &&
-			!isFetchingNextPage
-		) {
-			fetchNextPage();
-		}
-	}, [hasNextPage, fetchNextPage, allRows.length, isFetchingNextPage, items]);
-
+export function Feed({
+	children,
+	filter,
+	debug,
+	nocache,
+	bookmarks,
+}: FeedProps) {
 	return (
 		<div className="space-y-5 pb-10">
 			<div>{children}</div>
-			<div>{debug && <DebugInfo count={count} />}</div>
 
-			{status === "loading" ? (
-				<Loader />
-			) : status === "error" && error instanceof Error ? (
-				<span>Error: {error?.message}</span>
-			) : (
-				<div
-					className="w-full relative"
-					style={{
-						height: rowVirtualizer.getTotalSize(),
-					}}
-				>
-					<div
-						style={{
-							position: "absolute",
-							top: 0,
-							left: 0,
-							width: "100%",
-							transform: `translateY(${items[0]?.start ?? 0}px)`,
-						}}
-					>
-						{items.map((virtualRow) => {
-							const isLoaderRow = virtualRow.index > allRows.length - 1;
-							const row = allRows[virtualRow.index] as RankedPaper;
-
-							return (
-								<div
-									ref={rowVirtualizer.measureElement}
-									key={virtualRow.key}
-									data-index={virtualRow.index}
-									className="py-5"
-								>
-									{isLoaderRow ? (
-										hasNextPage ? (
-											<Loader />
-										) : null
-									) : row ? (
-										<FeedItem
-											paper={row.paper}
-											fingerprint={row.features}
-											bookmarks={bookmarks}
-										>
-											{debug && (
-												<FeatureCloud
-													features={row.features}
-													sum={row.filterMatchScore}
-												/>
-											)}
-										</FeedItem>
-									) : (
-										<Loader />
-									)}
-								</div>
-							);
-						})}
-					</div>
-				</div>
-			)}
+			<InfinityScrollList
+				filter={filter}
+				debug={debug}
+				nocache={nocache}
+				bookmarks={bookmarks}
+				renderSection={debug ? (count) => <DebugInfo count={count} /> : null}
+			/>
 		</div>
 	);
 }
@@ -181,7 +53,7 @@ function DebugInfo({ count }: { count?: number }) {
 				</div>
 			</div>
 			<div>
-				{!!count && (
+				{Boolean(count) && (
 					<div className="mt-1 text-right">{count} matching papers</div>
 				)}
 			</div>
---
+++ apps/proem/components/infinite-list.tsx
@@ -0,0 +1,167 @@
+"use client";
+import FeedItem, {
+	FeedItemProps,
+} from "@/app/(pages)/(app)/discover/feed-item";
+import { fetchFeedByTopic } from "@/app/(pages)/(app)/discover/fetch-feed";
+import { fetchFeedByFeatures } from "@/app/data/fetch-feed";
+import {
+	analyticsKeys,
+	trackHandler,
+} from "@/components/analytics/tracking/tracking-keys";
+import { FeatureCloud } from "@/components/feature-badges";
+import { RankedFeature } from "@proemial/repositories/oa/fingerprinting/features";
+import { RankedPaper } from "@proemial/repositories/oa/fingerprinting/rerank";
+import { Icons } from "@proemial/shadcn-ui";
+import { useWindowVirtualizer } from "@tanstack/react-virtual";
+import { ReactNode, useEffect } from "react";
+import { useInfiniteQuery } from "react-query";
+
+// 1-4 is fetched without scrolling
+const initialPageSize = 4;
+
+const Loader = () => (
+	<div className="w-full h-24 flex justify-center items-center">
+		<Icons.loader />
+	</div>
+);
+
+export type InfinityScollListProps = Pick<FeedItemProps, "bookmarks"> & {
+	filter: { topic?: number; features?: RankedFeature[]; days?: number };
+	debug?: boolean;
+	nocache?: boolean;
+	renderSection?: ((count?: number) => ReactNode) | null;
+};
+
+export function InfinityScrollList({
+	filter,
+	debug,
+	nocache,
+	bookmarks,
+	renderSection,
+}: InfinityScollListProps) {
+	const { topic, features, days } = filter;
+	const {
+		status,
+		data,
+		isFetchingNextPage,
+		fetchNextPage,
+		hasNextPage,
+		error,
+	} = useInfiniteQuery(
+		topic
+			? `feed_${topic}`
+			: `filter_${days}:${features?.map((f) => f.id).join("|")}`,
+		(ctx) => {
+			const nextOffset = ctx.pageParam;
+			if (nextOffset > initialPageSize) {
+				trackHandler(analyticsKeys.feed.scroll.fetch, {
+					offset: `${nextOffset - initialPageSize}`,
+				})();
+			}
+
+			if (features?.length) {
+				return fetchFeedByFeatures(
+					{ features, days },
+					{ offset: ctx.pageParam },
+					nocache,
+				);
+			}
+			return fetchFeedByTopic({ field: topic }, { offset: ctx.pageParam });
+		},
+		{
+			getNextPageParam: (lastGroup) => {
+				return lastGroup?.nextOffset;
+			},
+			refetchOnWindowFocus: false,
+		},
+	);
+
+	const allRows = data ? data.pages?.flatMap((d) => d.rows) : [];
+	const count = data?.pages.at(0)?.count;
+
+	const rowVirtualizer = useWindowVirtualizer({
+		count: hasNextPage ? allRows.length + 1 : allRows.length,
+		estimateSize: () => 160,
+		overscan: 5,
+	});
+	const items = rowVirtualizer.getVirtualItems();
+	useEffect(() => {
+		const [lastItem] = [...items].reverse();
+
+		if (!lastItem) {
+			return;
+		}
+
+		if (
+			lastItem.index >= allRows.length - 1 &&
+			hasNextPage &&
+			!isFetchingNextPage
+		) {
+			fetchNextPage();
+		}
+	}, [hasNextPage, fetchNextPage, allRows.length, isFetchingNextPage, items]);
+
+	return (
+		<>
+			{renderSection?.(count)}
+
+			{status === "loading" ? (
+				<Loader />
+			) : status === "error" && error instanceof Error ? (
+				<span>Error: {error?.message}</span>
+			) : (
+				<div
+					className="w-full relative"
+					style={{
+						height: rowVirtualizer.getTotalSize(),
+					}}
+				>
+					<div
+						style={{
+							position: "absolute",
+							top: 0,
+							left: 0,
+							width: "100%",
+							transform: `translateY(${items[0]?.start ?? 0}px)`,
+						}}
+					>
+						{items.map((virtualRow) => {
+							const isLoaderRow = virtualRow.index > allRows.length - 1;
+							const row = allRows[virtualRow.index] as RankedPaper;
+
+							return (
+								<div
+									ref={rowVirtualizer.measureElement}
+									key={virtualRow.key}
+									data-index={virtualRow.index}
+									className="py-5"
+								>
+									{isLoaderRow ? (
+										hasNextPage ? (
+											<Loader />
+										) : null
+									) : row ? (
+										<FeedItem
+											paper={row.paper}
+											fingerprint={row.features}
+											bookmarks={bookmarks}
+										>
+											{debug && (
+												<FeatureCloud
+													features={row.features}
+													sum={row.filterMatchScore}
+												/>
+											)}
+										</FeedItem>
+									) : (
+										<Loader />
+									)}
+								</div>
+							);
+						})}
+					</div>
+				</div>
+			)}
+		</>
+	);
+}
