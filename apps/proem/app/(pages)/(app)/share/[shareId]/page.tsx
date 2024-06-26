acfe145d5e287d00abdc09098c74ac976d9ba60e
Jon Sorensen <jon@proemial.ai>
2024-06-26 09:14:14 +0200

    fix: missing nav bar and padding on ask share page

--- apps/proem/app/(pages)/(app)/share/[shareId]/page.tsx
+++ apps/proem/app/(pages)/(app)/share/[shareId]/page.tsx
@@ -2,6 +2,11 @@ import { Answer } from "@/app/(pages)/(app)/(ask)/answer/[slug]/answer";
 import { mapAnswerToAnswerEngine } from "@/app/(pages)/(app)/(ask)/mapAnswerToAnswerEngine";
 import { getBookmarksByUserId } from "@/app/(pages)/(app)/discover/get-bookmarks-by-user-id";
 import { answers } from "@/app/api/bot/answer-engine/answers";
+import { getInternalUser } from "@/app/hooks/get-internal-user";
+import { Main } from "@/components/main";
+import { CloseAction } from "@/components/nav-bar/actions/close-action";
+import { SimpleHeader } from "@/components/nav-bar/headers/simple-header";
+import { NavBarV2 } from "@/components/nav-bar/nav-bar-v2";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
@@ -18,6 +23,7 @@ export default async function SharePage({ params: { shareId } }: Props) {
 	const [sharedAnswer] = await answers.getByShareId(shareId);
 	const { userId } = auth();
 	const bookmarks = userId ? await getBookmarksByUserId(userId) : {};
+	const { isInternal } = getInternalUser();
 
 	if (!sharedAnswer) {
 		redirect("/");
@@ -27,10 +33,20 @@ export default async function SharePage({ params: { shareId } }: Props) {
 		mapAnswerToAnswerEngine(sharedAnswer);
 
 	return (
-		<Answer
-			existingData={existingData}
-			initialMessages={initialMessages}
-			bookmarks={bookmarks}
-		/>
+		<>
+			<NavBarV2
+				action={<CloseAction target="/discover" />}
+				isInternalUser={isInternal}
+			>
+				<SimpleHeader title="Ask" />
+			</NavBarV2>
+			<Main>
+				<Answer
+					existingData={existingData}
+					initialMessages={initialMessages}
+					bookmarks={bookmarks}
+				/>
+			</Main>
+		</>
 	);
 }
