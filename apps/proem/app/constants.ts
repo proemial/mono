ed93cf6fd7d175ef5748a0c5e67deb9addc2ba91
Jon Sorensen <jon@proemial.ai>
2024-06-26 16:05:24 +0200

    correct default space name capitalization

--- apps/proem/app/constants.ts
+++ apps/proem/app/constants.ts
@@ -4,7 +4,7 @@ export const screenMaxWidth = "max-w-screen-md";
 export const screenMaxWidthOrgManagement = "max-w-screen-lg";
 
 export const PAPER_BOT_USER_ID = "paper_bot";
-export const PERSONAL_DEFAULT_COLLECTION_NAME = "For you";
+export const PERSONAL_DEFAULT_COLLECTION_NAME = "For You";
 export const getPersonalDefaultCollection = (userId: string) =>
 	({
 		id: userId,
