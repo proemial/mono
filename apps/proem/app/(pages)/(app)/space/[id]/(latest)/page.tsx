978eab1698cd02ffa7432527d6d425d5641f4f37
Jon Sorensen <jon@proemial.ai>
2024-06-27 12:16:12 +0200

    use reading history for default ("for you") user space

--- apps/proem/app/(pages)/(app)/space/[id]/(latest)/page.tsx
+++ apps/proem/app/(pages)/(app)/space/[id]/(latest)/page.tsx
@@ -1,6 +1,7 @@
 import { getBookmarksByUserId } from "@/app/(pages)/(app)/space/(discover)/get-bookmarks-by-user-id";
 import { StreamList } from "@/app/(pages)/(app)/space/[id]/(latest)/stream-list";
 import { FEED_DEFAULT_DAYS } from "@/app/data/fetch-by-features";
+import { getBookmarksAndHistory } from "@/app/data/fetch-history";
 import { auth } from "@clerk/nextjs";
 import { getFeatureFilter } from "@proemial/repositories/oa/fingerprinting/features";
 import { fetchFingerprints } from "@proemial/repositories/oa/fingerprinting/fetch-fingerprints";
@@ -34,7 +35,8 @@ export default async function LatestPage({ params }: PageProps) {
 		);
 	}
 
-	const fingerprints = paperIds ? await fetchFingerprints(paperIds) : [];
+	const history = await getBookmarksAndHistory(userId);
+	const fingerprints = await fetchFingerprints(...history);
 	const { filter: features } = getFeatureFilter(fingerprints);
 	return (
 		<StreamList
