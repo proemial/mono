0d88b363db53b4a5a7054837e3bcbf4031020efe
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-06-28 13:01:22 +0200

    [DEV-463]: add toggle-search-action button

---
+++ apps/proem/components/nav-bar/actions/toggle-search-action.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import {
+	analyticsKeys,
+	trackHandler,
+} from "@/components/analytics/tracking/tracking-keys";
+import { routes } from "@/routes";
+import { cn } from "@proemial/shadcn-ui";
+import { Plus } from "@untitled-ui/icons-react";
+import { usePathname, useRouter } from "next/navigation";
+
+export const ToggleSearchAction = () => {
+	const router = useRouter();
+	const pathname = usePathname();
+	const isSearchPage = pathname.includes(routes.search);
+
+	const handleAction = () => {
+		const [_, space, collectionId] = pathname.split("/");
+
+		if (isSearchPage) {
+			trackHandler(analyticsKeys.ui.header.click.close);
+			router.push(`/${space}/${collectionId}`);
+		} else {
+			trackHandler(analyticsKeys.ui.header.click.search);
+			router.push(`/${space}/${collectionId}${routes.search}`);
+		}
+	};
+
+	return (
+		<div className="p-1 cursor-pointer" onClick={handleAction}>
+			<Plus
+				className={cn("transition-all duration-150 ease-out size-5", {
+					"rotate-45": isSearchPage,
+				})}
+			/>
+		</div>
+	);
+};
