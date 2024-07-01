2057897b1fb28b85017f7b4a2c70577274734739
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-07-01 13:00:48 +0200

    refactor: split up bookmarks & history helper

--- apps/proem/app/data/fetch-history.ts
+++ apps/proem/app/data/fetch-history.ts
@@ -8,37 +8,45 @@ import { getBookmarksByCollectionId } from "../(pages)/(app)/space/(discover)/ge
 // Max no. of papers from read history to use in filter
 const MAX_COUNT = 30;
 
-export async function getBookmarksAndHistory(
-	user?: string,
-): Promise<Array<string[]>> {
-	const { userId } = auth();
-	const uid = user ?? userId;
-
-	if (uid) {
-		const user = await neonDb.query.users.findFirst({
-			where: eq(users.id, uid),
-		});
-
-		const bookmarks = uid ? await getBookmarksByCollectionId(uid) : {};
-		const bookmarkIds = Object.keys(bookmarks);
-		const readHistoryIds = sortAndFilter(user?.paperActivities ?? [])?.map(
-			(paper) => paper.paperId,
-		);
-
-		return [bookmarkIds, readHistoryIds];
-	}
-
-	return [];
-}
-
 function sortAndFilter(readHistory: PaperActivity[]) {
 	const maxCount = MAX_COUNT;
 
 	const millis = (date: string) => dayjs(date).valueOf();
 
-	const sortedHistory = readHistory
+	const sortedHistory = [...readHistory]
 		?.sort((a, b) => millis(b.lastReadAt) - millis(a.lastReadAt))
 		.sort((a, b) => b.noOfReads - a.noOfReads);
 
 	return sortedHistory?.slice(0, maxCount);
+}
+
+async function fetchUser(user?: string) {
+	const { userId } = auth();
+	const uid = user ?? userId;
+
+	if (!uid) {
+		return;
+	}
+
+	return neonDb.query.users.findFirst({
+		where: eq(users.id, uid),
+	});
+}
+
+export async function getBookmarksAndHistory(
+	userId: Parameters<typeof fetchUser>[0],
+): Promise<Array<string[]>> {
+	const user = await fetchUser(userId);
+
+	if (!user) {
+		return [];
+	}
+
+	const bookmarks = await getBookmarksByCollectionId(user.id);
+	const bookmarkIds = Object.keys(bookmarks);
+	const readHistoryIds = sortAndFilter(user.paperActivities ?? [])?.map(
+		(paper) => paper.paperId,
+	);
+
+	return [bookmarkIds, readHistoryIds];
 }
