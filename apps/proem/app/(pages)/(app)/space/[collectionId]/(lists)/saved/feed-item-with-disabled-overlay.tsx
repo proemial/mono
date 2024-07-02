a5ba5dfef5c945e003551cb31464c89bb7b85216
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-07-02 14:01:42 +0200

    style: adjusted undo bookmark removal overlay

--- apps/proem/app/(pages)/(app)/space/[collectionId]/(lists)/saved/feed-item-with-disabled-overlay.tsx
+++ apps/proem/app/(pages)/(app)/space/[collectionId]/(lists)/saved/feed-item-with-disabled-overlay.tsx
@@ -22,13 +22,12 @@ export function FeedItemWithDisabledOverlay({
 			{isDisabled && (
 				<div className="absolute top-0 left-0 z-10 flex items-center justify-center w-full h-full">
 					{customCollectionId && (
-						<div className="flex items-center gap-5">
-							<p className="font-medium">This paper has been removed.</p>
+						<div className="bg-background shadow rounded-xl flex justify-between gap-5 items-center py-0.5 pl-2.5">
+							<span className="text-sm">This paper has been removed</span>
+
 							<Button
-								type="button"
-								variant="default"
-								size="sm"
-								className="bg-white pointer-events-auto dark:bg-primary drop-shadow-xl hover:drop-shadow-lg"
+								variant="ghost"
+								className="p-2.5 text-sm underline"
 								onClick={async () => {
 									onBookmarkToggleClick(false);
 									await togglePaperInCollection({
@@ -47,7 +46,7 @@ export function FeedItemWithDisabledOverlay({
 			)}
 			<div
 				className={cn({
-					"opacity-50 pointer-events-auto  blur-sm": isDisabled,
+					"opacity-20 pointer-events-auto blur-sm": isDisabled,
 				})}
 			>
 				<FeedItem
