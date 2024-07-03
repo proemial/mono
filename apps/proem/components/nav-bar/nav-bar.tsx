be5ed2ca04d68e39d24338d6968fb57894ff6596
Jon Sorensen <jon@proemial.ai>
2024-07-03 13:16:38 +0200

    Reapply "replace menu button with left chevron in reader"
    This reverts commit 3f987aaf4f0df57faa62a0077f9a644ef7c39879.

--- apps/proem/components/nav-bar/nav-bar.tsx
+++ apps/proem/components/nav-bar/nav-bar.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { Profile } from "@/app/profile/profile";
+import { routes } from "@/routes";
 import {
 	NavigationMenu,
 	NavigationMenuItem,
 	NavigationMenuList,
 } from "@proemial/shadcn-ui";
+import { useParams, usePathname } from "next/navigation";
 import { ReactNode } from "react";
+import { GoToSpaceAction } from "./actions/go-to-space-action";
 
 type Props = {
 	/**
@@ -22,11 +25,14 @@ type Props = {
 };
 
 export const NavBar = ({ children, action }: Props) => {
+	const pathname = usePathname();
+	const params = useParams<{ id?: string }>();
+	const isReaderPage = params.id && pathname.includes(routes.paper);
 	return (
 		<NavigationMenu>
 			<NavigationMenuList className="justify-between flex-nowrap">
 				<NavigationMenuItem>
-					<Profile />
+					{isReaderPage ? <GoToSpaceAction /> : <Profile />}
 				</NavigationMenuItem>
 				<NavigationMenuItem className="truncate">{children}</NavigationMenuItem>
 				<NavigationMenuItem>{action}</NavigationMenuItem>
