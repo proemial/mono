7a68ce3ee98ec3233eca37313343e78831fcc504
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-07-29 14:39:40 +0200

    [DEV-504]: add themed header to NavBar

--- apps/proem/components/nav-bar/nav-bar.tsx
+++ apps/proem/components/nav-bar/nav-bar.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { Profile } from "@/app/profile/profile";
+import { Theme } from "@/components/theme";
 import { routes } from "@/routes";
 import {
 	NavigationMenu,
 	NavigationMenuItem,
 	NavigationMenuList,
 } from "@proemial/shadcn-ui";
+import { hex2rgba } from "@proemial/utils/color";
 import { useParams, usePathname } from "next/navigation";
 import { ReactNode } from "react";
 import { GoToSpaceAction } from "./actions/go-to-space-action";
@@ -24,19 +26,62 @@ type Props = {
 	action?: ReactNode;
 };
 
+const bgColor = "#F5F5F5";
+
 export const NavBar = ({ children, action }: Props) => {
 	const pathname = usePathname();
-	const params = useParams<{ id?: string }>();
+	const params = useParams<{ id?: string; collectionId?: string }>();
 	const isReaderPage = params.id && pathname.includes(routes.paper);
+	const seed = params.collectionId ?? "";
+	const image = Theme.image(seed);
+	const color = Theme.color(seed);
+	const hasTheme = false;
+
 	return (
-		<NavigationMenu>
-			<NavigationMenuList className="justify-between flex-nowrap">
-				<NavigationMenuItem>
-					{isReaderPage ? <GoToSpaceAction /> : <Profile />}
-				</NavigationMenuItem>
-				<NavigationMenuItem className="truncate">{children}</NavigationMenuItem>
-				<NavigationMenuItem>{action}</NavigationMenuItem>
-			</NavigationMenuList>
-		</NavigationMenu>
+		<>
+			<NavigationMenu className="z-20">
+				<div
+					className="absolute top-0 left-0 w-full h-full"
+					style={{
+						background: hasTheme
+							? `url('/backgrounds/${
+									image?.regular
+								}'),linear-gradient(${hex2rgba(color, 1)} 0px, ${hex2rgba(
+									color,
+									1,
+								)} 58px, transparent 72px)`
+							: bgColor,
+						maskImage:
+							"linear-gradient(black 0px, black 54px, transparent 72px)",
+						backgroundPosition: "top",
+						backgroundAttachment: "fixed",
+						backgroundSize: "50%",
+					}}
+				/>
+				<NavigationMenuList className="justify-between flex-nowrap">
+					<NavigationMenuItem>
+						{isReaderPage ? <GoToSpaceAction /> : <Profile />}
+					</NavigationMenuItem>
+					<NavigationMenuItem className="truncate">
+						{children}
+					</NavigationMenuItem>
+					<NavigationMenuItem>{action}</NavigationMenuItem>
+				</NavigationMenuList>
+			</NavigationMenu>
+
+			{hasTheme ? (
+				<div
+					className="w-full h-60 -top-32 -mt-16 left-0 sticky -mb-32"
+					style={{
+						backgroundImage: `url('/backgrounds/${image?.regular}')`,
+						backgroundColor: color,
+						maskImage: "linear-gradient(to top, transparent 0px, black 48px)",
+						backgroundPosition: "top",
+						backgroundAttachment: "fixed",
+						backgroundSize: "50%",
+					}}
+				/>
+			) : null}
+		</>
 	);
 };
