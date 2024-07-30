717e3772caae3fa922bb73c0ad30b8d2afb80098
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-07-30 13:32:56 +0200

    [DEV-509]: refactor background image to image element for mobile support

--- apps/proem/components/nav-bar/nav-bar.tsx
+++ apps/proem/components/nav-bar/nav-bar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { screenMaxWidth } from "@/app/constants";
 import { useInternalUser } from "@/app/hooks/use-user";
 import { Profile } from "@/app/profile/profile";
 import { Theme } from "@/components/theme";
@@ -8,8 +9,10 @@ import {
 	NavigationMenu,
 	NavigationMenuItem,
 	NavigationMenuList,
+	cn,
 } from "@proemial/shadcn-ui";
 import { hex2rgba } from "@proemial/utils/color";
+import Image from "next/image";
 import { useParams, usePathname } from "next/navigation";
 import { ReactNode } from "react";
 import { GoToSpaceAction } from "./actions/go-to-space-action";
@@ -43,23 +46,23 @@ export const NavBar = ({ children, action }: Props) => {
 		<>
 			<NavigationMenu className="z-20 bg-transparent">
 				<div
-					className="absolute top-0 left-0 w-full h-full"
+					className="absolute top-0 left-0 w-full h-full overflow-hidden"
 					style={{
 						background: hasTheme
-							? `url('/backgrounds/${
-									image?.regular
-								}'),linear-gradient(${hex2rgba(color, 1)} 0px, ${hex2rgba(
+							? `linear-gradient(${hex2rgba(color, 1)} 0px, ${hex2rgba(
 									color,
 									1,
 								)} 58px, transparent 72px)`
 							: bgColor,
 						maskImage:
 							"linear-gradient(black 0px, black 54px, transparent 72px)",
-						backgroundPosition: "top",
-						backgroundAttachment: "fixed",
-						backgroundSize: "300px",
 					}}
-				/>
+				>
+					{image?.regular && (
+						<ThemeBackgroundImage src={`/backgrounds/${image.regular}`} />
+					)}
+				</div>
+
 				<NavigationMenuList className="justify-between flex-nowrap">
 					<NavigationMenuItem>
 						{isReaderPage ? <GoToSpaceAction /> : <Profile />}
@@ -73,17 +76,37 @@ export const NavBar = ({ children, action }: Props) => {
 
 			{hasTheme ? (
 				<div
-					className="w-full h-60 -top-32 -mt-16 left-0 sticky -mb-36"
+					className="w-full h-60 -top-32 -mt-[72px] left-0 sticky -mb-36"
 					style={{
-						backgroundImage: `url('/backgrounds/${image?.regular}')`,
 						backgroundColor: color,
 						maskImage: "linear-gradient(to top, transparent 0px, black 48px)",
-						backgroundPosition: "top",
-						backgroundAttachment: "fixed",
-						backgroundSize: "300px",
 					}}
-				/>
+				>
+					<div className="fixed top-0 left-0 w-full">
+						<div
+							className={cn(
+								"mx-auto w-full h-60 overflow-hidden",
+								screenMaxWidth,
+							)}
+						>
+							{image?.regular && (
+								<ThemeBackgroundImage src={`/backgrounds/${image.regular}`} />
+							)}
+						</div>
+					</div>
+				</div>
 			) : null}
 		</>
 	);
 };
+
+const ThemeBackgroundImage = ({ src }: { src: string }) => (
+	<Image
+		src={src}
+		alt="Theme background image"
+		quality={100}
+		className="w-full min-w-72"
+		width={600}
+		height={600}
+	/>
+);
