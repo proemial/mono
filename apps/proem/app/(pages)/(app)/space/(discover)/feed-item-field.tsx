0d8fcdd542c227db94d93e6aa7b6ed01ea1e27a7
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-07-31 14:03:50 +0200

    refactor: extract getFieldFromOpenAlexTopics util for reuseability

--- apps/proem/app/(pages)/(app)/space/(discover)/feed-item-field.tsx
+++ apps/proem/app/(pages)/(app)/space/(discover)/feed-item-field.tsx
@@ -1,36 +1,20 @@
 "use client";
 
-import { oaFieldIconMap } from "@/app/data/oa-fields";
 import { OpenAlexTopic } from "@proemial/repositories/oa/models/oa-paper";
 import { useMemo } from "react";
+import { getFieldFromOpenAlexTopics } from "./get-field-from-open-alex-topics";
 
-export function FeedItemField({ topics }: { topics?: OpenAlexTopic[] }) {
-	const fields =
-		topics?.map((topic) => ({
-			id: topic.field.id,
-			score: topic.score,
-		})) ?? [];
+export function FeedItemField({ topics = [] }: { topics?: OpenAlexTopic[] }) {
+	const field = useMemo(() => getFieldFromOpenAlexTopics(topics), [topics]);
 
-	const field = useMemo(() => {
-		if (fields.length === 0) {
-			return undefined;
-		}
-		const field = fields.reduce((prev, current) =>
-			prev.score > current.score ? prev : current,
-		);
-		return oaFieldIconMap[field.id];
-	}, [fields]);
-
-	if (field) {
-		return (
-			<div className="flex items-center gap-2">
-				{field.icon}
-				<div className="text-xs uppercase line-clamp-1">
-					{field.displayName}
-				</div>
-			</div>
-		);
+	if (!field) {
+		return null;
 	}
 
-	return undefined;
+	return (
+		<div className="flex items-center gap-2">
+			{field.icon}
+			<div className="text-xs uppercase line-clamp-1">{field.displayName}</div>
+		</div>
+	);
 }
---
+++ apps/proem/app/(pages)/(app)/space/(discover)/get-field-from-open-alex-topics.ts
@@ -0,0 +1,14 @@
+import { oaFieldIconMap } from "@/app/data/oa-fields";
+import { OpenAlexTopic } from "@proemial/repositories/oa/models/oa-paper";
+
+export function getFieldFromOpenAlexTopics(topics: OpenAlexTopic[]) {
+	if (topics.length === 0) {
+		return null;
+	}
+
+	const field = topics.reduce((prev, current) =>
+		prev.score > current.score ? prev : current,
+	);
+
+	return oaFieldIconMap[field.field.id];
+}
