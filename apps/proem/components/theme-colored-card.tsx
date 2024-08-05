d3b3d7ac7dd814dd6719661d765e9533b486f190
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-08-05 11:44:34 +0200

    [DEV-526]: adjusted design of background gradients in themed card view

--- apps/proem/components/theme-colored-card.tsx
+++ apps/proem/components/theme-colored-card.tsx
@@ -17,31 +17,31 @@ export const ThemeColoredCard = ({
 	return (
 		<div
 			className={cn(
-				"p-3 rounded-2xl relative overflow-hidden",
+				"p-3 pt-0.5 rounded-2xl relative overflow-hidden",
 				{
-					"bg-gradient-to-b from-purple-400 to-purple-400/20":
+					"bg-gradient-to-b from-purple-400/60 to-purple-400/20":
 						theme?.color === "purple",
-					"bg-gradient-to-b from-gold-400 to-gold-400/20":
+					"bg-gradient-to-b from-gold-400/60 to-gold-400/20":
 						theme?.color === "gold",
-					"bg-gradient-to-b from-teal-400 to-teal-400/20":
+					"bg-gradient-to-b from-teal-400/60 to-teal-400/20":
 						theme?.color === "teal",
-					"bg-gradient-to-b from-green-400 to-green-400/20":
+					"bg-gradient-to-b from-green-400/60 to-green-400/20":
 						theme?.color === "green",
-					"bg-gradient-to-b from-rose-400 to-rose-400/20":
+					"bg-gradient-to-b from-rose-400/60 to-rose-400/20":
 						theme?.color === "rose",
 				},
 				className,
 			)}
 		>
 			{theme?.image ? (
-				<div className="absolute inset-0 opacity-50">
-					<ThemeBackgroundImage
-						pattern={theme.image}
-						style={{
-							maskImage:
-								"linear-gradient(rgba(0,0,0,1) 0%, rgba(0,0,0,0.37) 51%, rgba(0,0,0,0) 100%)",
-						}}
-					/>
+				<div
+					className="absolute inset-0 opacity-50"
+					style={{
+						maskImage:
+							"linear-gradient(rgba(0,0,0,1) 0%, rgba(0,0,0,0.37) 51%, rgba(0,0,0,0) 100%)",
+					}}
+				>
+					<ThemeBackgroundImage pattern={theme.image} />
 				</div>
 			) : null}
 
