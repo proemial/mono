3e54c9ab6abc2ae1f596dc8cd564d3391ff02086
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-08-08 21:20:33 +0200

    remove dark mode from clerk provider

--- apps/proem/components/providers/auth-provider.tsx
+++ apps/proem/components/providers/auth-provider.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { ClerkProvider } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
-import { useTheme } from "@proemial/shadcn-ui";
 import { ReactNode } from "react";
 
 type Props = {
@@ -10,16 +8,5 @@ type Props = {
 };
 
 export const AuthProvider = ({ children }: Props) => {
-	const { resolvedTheme } = useTheme();
-	const clerkTheme = resolvedTheme === "dark" ? dark : undefined;
-
-	return (
-		<ClerkProvider
-			appearance={{
-				baseTheme: clerkTheme,
-			}}
-		>
-			{children}
-		</ClerkProvider>
-	);
+	return <ClerkProvider>{children}</ClerkProvider>;
 };
