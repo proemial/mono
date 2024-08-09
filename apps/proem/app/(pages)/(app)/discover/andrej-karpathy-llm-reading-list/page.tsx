53f0e576f03725e1399f319edde7d738ff24c370
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-08-09 10:22:16 +0200

    remove inlined nav-bar from space layout

--- apps/proem/app/(pages)/(app)/discover/andrej-karpathy-llm-reading-list/page.tsx
+++ apps/proem/app/(pages)/(app)/discover/andrej-karpathy-llm-reading-list/page.tsx
@@ -1,7 +1,5 @@
 import { getBookmarksByCollectionId } from "@/app/(pages)/(app)/space/(discover)/get-bookmarks-by-collection-id";
 import { Main } from "@/components/main";
-import { OpenSearchAction } from "@/components/nav-bar/actions/open-search-action";
-import { NavBar } from "@/components/nav-bar/nav-bar";
 import { PostService } from "@/services/post-service";
 import { auth } from "@clerk/nextjs";
 import { OpenAlexPaper } from "@proemial/repositories/oa/models/oa-paper";
