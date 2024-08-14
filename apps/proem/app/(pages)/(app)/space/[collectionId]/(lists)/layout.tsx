55b7f2960a4bae642356a7fc6876fb069dbfbc94
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-08-14 11:36:42 +0200

    add padding top on spaces

--- apps/proem/app/(pages)/(app)/space/[collectionId]/(lists)/layout.tsx
+++ apps/proem/app/(pages)/(app)/space/[collectionId]/(lists)/layout.tsx
@@ -28,7 +28,7 @@ export default async function ({ params: { collectionId }, children }: Props) {
 	const isDefaultCollection = collection.id === userId;
 
 	return (
-		<div className="flex flex-col gap-5 grow">
+		<div className="flex flex-col gap-5 grow pt-10">
 			{!isDefaultCollection && (
 				<div className="flex flex-col gap-2">
 					<div className="flex flex-col gap-3">
