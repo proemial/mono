c90080b8acc1680e99109d7efa74257765c48065
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-08-15 13:14:36 +0200

    [DEV-558]: persist paper-bot comments

--- apps/proem/app/api/ai/route.ts
+++ apps/proem/app/api/ai/route.ts
@@ -1,10 +1,18 @@
 import { UserContext, assistant } from "@/app/api/ai/ai-assistant";
+import {
+	PAPER_BOT_USER_ID,
+	getPersonalDefaultCollection,
+} from "@/app/constants";
+import { auth } from "@clerk/nextjs/server";
+import { findCollection } from "@proemial/data/repository/collection";
+import { savePostWithComment } from "@proemial/data/repository/post";
 import { convertToCoreMessages, streamText } from "ai";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
+	const { userId } = auth();
 	const { messages, title, paperId, spaceId, abstract } = await req.json();
 
 	const userContext: UserContext = paperId
@@ -13,15 +21,37 @@ export async function POST(req: Request) {
 			? "space"
 			: "global";
 
-	console.log("userContext", userContext);
-
 	const convertedMessages = convertToCoreMessages(messages);
 	const currentAssistant = assistant(userContext, title, abstract);
-	console.log(currentAssistant);
 
 	const result = await streamText({
 		...currentAssistant,
 		messages: convertedMessages,
+		onFinish: async ({ finishReason, text }) => {
+			if (finishReason === "stop") {
+				if (userId) {
+					// Save the post and the AI reply, if the user is signed in
+					const space =
+						spaceId === userId
+							? getPersonalDefaultCollection(userId)
+							: await findCollection(spaceId);
+					await savePostWithComment(
+						{
+							content: messages.at(-1)?.content,
+							authorId: userId,
+							paperId,
+							// Inherit the space's sharing setting, or `public` if no space
+							shared: space?.shared ?? "public",
+							spaceId: space?.id,
+						},
+						{
+							content: text,
+							authorId: PAPER_BOT_USER_ID,
+						},
+					);
+				}
+			}
+		},
 	});
 
 	return result.toDataStreamResponse();
