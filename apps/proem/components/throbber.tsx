e23c3049c51187481e2b6736595df39050c5aaff
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-08-19 20:53:53 +0200

    add theme color on throbber

--- apps/proem/components/throbber.tsx
+++ apps/proem/components/throbber.tsx
@@ -3,7 +3,7 @@ import { Icons } from "@proemial/shadcn-ui";
 export function Throbber() {
 	return (
 		<div className="w-full h-24 flex justify-center items-center">
-			<Icons.loader />
+			<Icons.loader className="fill-theme-800/70" />
 		</div>
 	);
 }
