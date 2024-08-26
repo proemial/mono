4560fac3e679d7de25afddbd32699fb3a3dd3f5d
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-08-26 13:08:17 +0200

    refactor: handleAskRequest for reuseability

---
+++ apps/proem/app/api/bot/ask2/handle-ask-request.ts
@@ -0,0 +1,68 @@
+import { chatInputMaxLength } from "@/app/api/bot/input-limit";
+import { getInternalUser } from "@/app/hooks/get-internal-user";
+import { INTERNAL_COOKIE_NAME } from "@/app/hooks/use-user";
+import { toLangChainChatHistory } from "@/app/llm/utils";
+import { Message as VercelChatMessage } from "ai";
+import { cookies } from "next/headers";
+import { answerEngine } from "./answer-engine";
+
+export async function handleAskRequest(requestData: {
+	slug?: string;
+	userId?: string;
+	messages?: VercelChatMessage[];
+	spaceId?: string;
+}) {
+	const { slug, userId: userIdFromHeader, messages, spaceId } = requestData;
+	const { name, userId } = nameAndIdFromCookie(userIdFromHeader);
+	const { input, chatHistory } = parseMessages(messages);
+
+	if (!input?.content.length) {
+		throw new Error("No question found");
+	}
+	if (input.content.length > chatInputMaxLength) {
+		throw new Error("Input too long");
+	}
+
+	return answerEngine({
+		chatHistory,
+		userId,
+		transactionId: input?.id,
+		userInput: input?.content,
+		existingSlug: slug || undefined,
+		tags: name ? [name] : undefined,
+		spaceId,
+	});
+}
+
+function parseMessages(chatMessages?: VercelChatMessage[]) {
+	const messages = (chatMessages ?? []).filter(
+		(message: VercelChatMessage) =>
+			message.role === "user" || message.role === "assistant",
+	);
+	const chatHistory = messages.slice(0, -1).map(toLangChainChatHistory);
+	const input = messages[messages.length - 1];
+
+	return { input, chatHistory };
+}
+
+function nameAndIdFromCookie(userIdFromHeader?: string) {
+	const cookie = cookies().get(INTERNAL_COOKIE_NAME)?.value ?? "";
+	if (!cookie) {
+		return {
+			isInternal: false,
+			userId: userIdFromHeader,
+		};
+	}
+
+	const { email, userId: userIdFromCookie } = JSON.parse(cookie) as {
+		email?: string;
+		userId?: string;
+	};
+	const { isInternal, proemialName: name } = getInternalUser(email);
+
+	return {
+		isInternal,
+		name,
+		userId: userIdFromHeader ?? userIdFromCookie,
+	};
+}
--- apps/proem/app/api/bot/ask2/route.ts
+++ apps/proem/app/api/bot/ask2/route.ts
@@ -1,83 +1,13 @@
-import { chatInputMaxLength } from "@/app/api/bot/input-limit";
-import { getInternalUser } from "@/app/hooks/get-internal-user";
-import { INTERNAL_COOKIE_NAME } from "@/app/hooks/use-user";
-import { toLangChainChatHistory } from "@/app/llm/utils";
-import { ratelimitByIpAddress } from "@/utils/ratelimiter";
-import { Message as VercelChatMessage } from "ai";
-import { cookies } from "next/headers";
+import { handleAskRequest } from "@/app/api/bot/ask2/handle-ask-request";
 import { NextRequest, NextResponse } from "next/server";
-import { answerEngine } from "./answer-engine";
 
 export const maxDuration = 30;
 
 export async function POST(req: NextRequest) {
-	const { success } = await ratelimitByIpAddress(req.ip);
-	if (!success) {
-		return NextResponse.json({ error: "Rate limited" }, { status: 429 });
-	}
-
 	try {
-		const {
-			slug,
-			userId: userIdFromHeader,
-			messages,
-			spaceId,
-		} = await req.json();
-		const { name, userId } = nameAndIdFromCookie(userIdFromHeader);
-		const { input, chatHistory } = parseMessages(messages);
-
-		if (!input?.content.length) {
-			throw new Error("No question found");
-		}
-		if (input.content.length > chatInputMaxLength) {
-			throw new Error("Input too long");
-		}
-
-		const stream = answerEngine({
-			chatHistory,
-			userId,
-			transactionId: input?.id,
-			userInput: input?.content,
-			existingSlug: slug || undefined,
-			tags: name ? [name] : undefined,
-			spaceId,
-		});
-
-		return stream;
+		const requestData = await req.json();
+		return await handleAskRequest(requestData);
 	} catch (e) {
 		return NextResponse.json(e, { status: 500 });
 	}
-}
-
-function parseMessages(chatMessages?: VercelChatMessage[]) {
-	const messages = (chatMessages ?? []).filter(
-		(message: VercelChatMessage) =>
-			message.role === "user" || message.role === "assistant",
-	);
-	const chatHistory = messages.slice(0, -1).map(toLangChainChatHistory);
-	const input = messages[messages.length - 1];
-
-	return { input, chatHistory };
-}
-
-function nameAndIdFromCookie(userIdFromHeader?: string) {
-	const cookie = cookies().get(INTERNAL_COOKIE_NAME)?.value ?? "";
-	if (!cookie) {
-		return {
-			isInternal: false,
-			userId: userIdFromHeader,
-		};
-	}
-
-	const { email, userId: userIdFromCookie } = JSON.parse(cookie) as {
-		email?: string;
-		userId?: string;
-	};
-	const { isInternal, proemialName: name } = getInternalUser(email);
-
-	return {
-		isInternal,
-		name,
-		userId: userIdFromHeader ?? userIdFromCookie,
-	};
 }
