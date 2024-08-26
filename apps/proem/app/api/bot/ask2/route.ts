b24dee8c47d0e8f7a3b1c1088c9901bd57ad2d51
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-08-26 13:38:04 +0200

    reapply ratelimit to ask2 endpoint

--- apps/proem/app/api/bot/ask2/route.ts
+++ apps/proem/app/api/bot/ask2/route.ts
@@ -1,4 +1,5 @@
 import { handleAskRequest } from "@/app/api/bot/ask2/handle-ask-request";
+import { ratelimitByIpAddress } from "@/utils/ratelimiter";
 import { NextRequest, NextResponse } from "next/server";
 
 export const maxDuration = 30;
@@ -6,6 +7,11 @@ export const maxDuration = 30;
 export async function POST(req: NextRequest) {
 	try {
 		const requestData = await req.json();
+		const { success } = await ratelimitByIpAddress(req.ip);
+		if (!success) {
+			return NextResponse.json({ error: "Rate limited" }, { status: 429 });
+		}
+
 		return await handleAskRequest(requestData);
 	} catch (e) {
 		return NextResponse.json(e, { status: 500 });
