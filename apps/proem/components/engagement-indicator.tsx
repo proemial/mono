9af0f27005074324c3335d589e7fe57af77ab6e6
Jon Sorensen <jon@proemial.ai>
2024-08-28 11:20:20 +0200

    engagement indicator in paper reader

---
+++ apps/proem/components/engagement-indicator.tsx
@@ -0,0 +1,45 @@
+import { PostWithCommentsAndAuthor } from "@/services/post-service";
+import { cn } from "@proemial/shadcn-ui";
+import { AuthorAvatar } from "./author-avatar";
+
+type Props = {
+	posts: PostWithCommentsAndAuthor[];
+	className?: string;
+};
+
+export const EngagementIndicator = ({ posts, className }: Props) => {
+	if (posts.length === 0) {
+		return undefined;
+	}
+
+	const distinctAuthors = posts.reduce(
+		(authors: PostWithCommentsAndAuthor["author"][], post) => {
+			if (!authors.find((author) => author && author.id === post.author?.id)) {
+				authors.push(post.author);
+			}
+			return authors;
+		},
+		[],
+	);
+
+	return (
+		<div className={cn("flex gap-2", className)}>
+			<div className="flex gap-2">
+				{distinctAuthors.map((author, index) => (
+					<AuthorAvatar
+						key={index}
+						firstName={author?.firstName ?? null}
+						lastName={author?.lastName ?? null}
+						imageUrl={author?.imageUrl}
+					/>
+				))}
+			</div>
+			<div className="text-sm opacity-90">
+				{formatQuestionsAskedLabel(posts.length)}
+			</div>
+		</div>
+	);
+};
+
+const formatQuestionsAskedLabel = (count: number) =>
+	count === 1 ? "1 question asked" : `${count} questions asked`;
