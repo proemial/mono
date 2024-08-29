92b4a9132754befc378f3de4dcba4a9bd741cfa9
proemian <brian@proemial.ai>
2024-08-29 19:35:27 +0200

    rough embed page

---
+++ apps/proem/app/embed/[space]/page.tsx
@@ -0,0 +1,48 @@
+import { getBookmarkedPapersByCollectionId } from "@/app/(pages)/(app)/space/(discover)/get-bookmarked-papers-by-collection-id";
+import { StreamList } from "@/app/(pages)/(app)/space/[collectionId]/(lists)/(latest)/stream-list";
+import { FEED_DEFAULT_DAYS } from "@/app/data/fetch-by-features";
+import { CollectionService } from "@/services/collection-service";
+import { getFeatureFilter } from "@proemial/repositories/oa/fingerprinting/features";
+import React from "react";
+import { fetchFingerprints } from "@proemial/repositories/oa/fingerprinting/fetch-fingerprints";
+import { Fingerprint } from "@proemial/repositories/oa/fingerprinting/fingerprints";
+import { notFound } from "next/navigation";
+
+type Props = {
+	params: {
+		space: string;
+	};
+	searchParams: {
+		colors?: string;
+	};
+};
+
+export default async function EmbedPage({
+	params: { space },
+	searchParams: { colors },
+}: Props) {
+	const collection = await CollectionService.getCollection(space);
+	if (!collection) {
+		notFound();
+	}
+
+	const bookmarkedPapers = await getBookmarkedPapersByCollectionId(space);
+	const paperIds = bookmarkedPapers?.map(({ paperId }) => paperId);
+
+	const fingerprints: Fingerprint[][] = [];
+	const fingerprintsBasedOnPapers = paperIds
+		? await fetchFingerprints(paperIds)
+		: [];
+	fingerprints.push(...fingerprintsBasedOnPapers);
+
+	const { filter: features } = getFeatureFilter(fingerprints);
+
+	return (
+		<StreamList
+			collectionId={space}
+			features={features}
+			days={FEED_DEFAULT_DAYS}
+			readonly={true}
+		/>
+	);
+}
