2df8856f34deedebe28a4003ca1645d5f618119d
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-09-02 13:37:07 +0200

    [DEV-581]: update sentry setup

--- apps/proem/sentry.client.config.ts
+++ apps/proem/sentry.client.config.ts
@@ -1,28 +1,31 @@
-import { env } from "@/env/client";
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
 	enabled: process.env.NODE_ENV === "production",
-	dsn: env.NEXT_PUBLIC_SENTRY_DSN,
-	// Replay may only be enabled for the client-side
-	integrations: [
+	dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+
+
+  // Add optional integrations for additional features
+  integrations: [
 		Sentry.browserTracingIntegration(),
-		Sentry.replayIntegration(),
-	],
+    Sentry.replayIntegration(),
+  ],
 
-	// Set tracesSampleRate to 1.0 to capture 100%
-	// of transactions for performance monitoring.
-	// We recommend adjusting this value in production
-	tracesSampleRate: 1.0,
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
 
-	// Capture Replay for 10% of all sessions,
-	// plus for 100% of sessions with an error
-	replaysSessionSampleRate: 0,
-	replaysOnErrorSampleRate: 1.0,
+  // Define how likely Replay events are sampled.
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
 
-	// ...
+  // Define how likely Replay events are sampled when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
 
-	// Note: if you want to override the automatic release value, do not set a
-	// `release` value here - use the environment variable `SENTRY_RELEASE`, so
-	// that it will also get attached to your source maps
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
 });
