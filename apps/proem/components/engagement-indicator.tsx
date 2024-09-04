4b04b2d1e6e2c2fd0455d84ce65fe2eb6e0a692f
Jon Sorensen <jon@proemial.ai>
2024-09-04 15:41:14 +0200

    fall back to showing post authors if no readers

--- apps/proem/components/engagement-indicator.tsx
+++ apps/proem/components/engagement-indicator.tsx
@@ -21,6 +21,19 @@ export const EngagementIndicator = ({ posts, readers, className }: Props) => {
 	const readCount = formatReadCount(readers.length);
 	const questions = formatQuestionsAsked(posts.length);
 
+	// Remove post author duplicates
+	const postAuthors = posts.reduce(
+		(authors: PostWithCommentsAndAuthor["author"][], post) => {
+			if (!authors.find((author) => author && author.id === post.author?.id)) {
+				authors.push(post.author);
+			}
+			return authors;
+		},
+		[],
+	);
+
+	const userAvatars = readers.length > 0 ? readers : postAuthors;
+
 	const handleClick = () => {
 		if (clickable) {
 			open();
@@ -32,13 +45,13 @@ export const EngagementIndicator = ({ posts, readers, className }: Props) => {
 			className={cn("flex gap-2", className, { "cursor-pointer": clickable })}
 			onClick={handleClick}
 		>
-			{readers.length > 0 && (
+			{userAvatars.length > 0 && (
 				<div className="flex gap-2">
-					{readers.map((reader, index) => (
+					{userAvatars.map((reader, index) => (
 						<AuthorAvatar
 							key={index}
-							firstName={reader.firstName}
-							lastName={reader.lastName}
+							firstName={reader.firstName ?? null}
+							lastName={reader.lastName ?? null}
 							imageUrl={reader.imageUrl}
 						/>
 					))}
