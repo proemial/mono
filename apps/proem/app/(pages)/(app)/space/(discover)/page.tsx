4ffdedb0e2a72ebd38ef1ca908f4c91c58e3065e
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-09-10 17:12:29 +0200

    [613]: (WIP) prefetch query in Server Component

--- apps/proem/app/(pages)/(app)/space/(discover)/page.tsx
+++ apps/proem/app/(pages)/(app)/space/(discover)/page.tsx
@@ -1,9 +1,35 @@
 import { Feed } from "@/app/(pages)/(app)/space/(discover)/feed";
 import { FEED_DEFAULT_DAYS } from "@/app/data/fetch-by-features";
+import { fetchFeedByFeaturesWithPostsAndReaders } from "@/app/data/fetch-feed";
 import { OnboardingCarousel } from "@/components/onboarding";
+import { getQueryClient } from "@/components/providers/get-query-client";
 import { routes } from "@/routes";
 import { auth } from "@clerk/nextjs/server";
+import { getFeedback } from "@sentry/nextjs";
+import {
+	HydrationBoundary,
+	QueryClient,
+	dehydrate,
+} from "@tanstack/react-query";
 import { redirect } from "next/navigation";
+import { Suspense } from "react";
+// import { getFeed } from "./get-feed";
+const getFeed = async () => ({
+	pages: [
+		{
+			count: 100,
+			nextOffset: 2,
+			rows: [
+				{ id: "1", from: "server" },
+				{ id: "2", from: "server" },
+				{ id: "3", from: "server" },
+				{ id: "4", from: "server" },
+				{ id: "5", from: "server" },
+			],
+		},
+	],
+	pageParams: [1],
+});
 
 export default async function DiscoverPage() {
 	const { userId } = auth();
@@ -11,6 +37,7 @@ export default async function DiscoverPage() {
 	if (userId) {
 		redirect(`${routes.space}/${userId}`);
 	}
+	const queryClient = getQueryClient();
 
 	const filter = {
 		features: [],
@@ -18,12 +45,25 @@ export default async function DiscoverPage() {
 		titles: undefined,
 	};
 
+	queryClient.prefetchQuery({
+		queryKey: ["test3"],
+		queryFn: getFeed,
+		// fetchFeedByFeaturesWithPostsAndReaders(
+		// 	{ features: filter.features, days: filter.days },
+		// 	{ offset: 0 },
+		// 	false,
+		// 	undefined,
+		// ),
+	});
+
 	return (
-		<>
+		// Neat! Serialization is now as easy as passing props.
+		// HydrationBoundary is a Client Component, so hydration will happen there.
+		<HydrationBoundary state={dehydrate(queryClient)}>
 			<OnboardingCarousel />
 			<div className="mt-4">
 				<Feed filter={filter} showThemeColors />
 			</div>
-		</>
+		</HydrationBoundary>
 	);
 }
