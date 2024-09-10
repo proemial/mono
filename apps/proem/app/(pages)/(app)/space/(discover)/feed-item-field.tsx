81cb79ec83ea8f02d35910898f75c55b0dbf701c
proemian <brian@proemial.ai>
2024-09-10 13:52:35 +0200

    roolback conditional field shrink

--- apps/proem/app/(pages)/(app)/space/(discover)/feed-item-field.tsx
+++ apps/proem/app/(pages)/(app)/space/(discover)/feed-item-field.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { OpenAlexTopic } from "@proemial/repositories/oa/models/oa-paper";
-import { createElement, useMemo } from "react";
+import { useMemo } from "react";
 import { getFieldFromOpenAlexTopics } from "./get-field-from-open-alex-topics";
 import { useSearchParams } from "next/navigation";
 
@@ -9,7 +9,6 @@ export function FeedItemField({ topics = [] }: { topics?: OpenAlexTopic[] }) {
 	const field = useMemo(() => getFieldFromOpenAlexTopics(topics), [topics]);
 	const searchParams = useSearchParams();
 
-	const breakPoint = 300;
 	const embedColor = searchParams.get("foreground");
 	const wideEmbedStyle = embedColor
 		? {
@@ -21,27 +20,17 @@ export function FeedItemField({ topics = [] }: { topics?: OpenAlexTopic[] }) {
 				alignItems: "center",
 			}
 		: {};
-	const narrowEmbedStyle =
-		embedColor && `!text-[#${embedColor}] !stroke-[1.5px]`;
 
 	if (!field) {
 		return null;
 	}
 
 	return (
-		<>
-			{createElement(field.icon.type, {
-				...field.icon.props,
-				className: `block min-[${breakPoint}px]:hidden ${narrowEmbedStyle}`,
-			})}
-			<div className={`max-[${breakPoint}px]:hidden`}>
-				<div className="flex items-center gap-2" style={wideEmbedStyle}>
-					{field.icon}
-					<div className="text-2xs  uppercase line-clamp-1">
-						{field.displayName}
-					</div>
-				</div>
+		<div className="flex items-center gap-2" style={wideEmbedStyle}>
+			{field.icon}
+			<div className="text-2xs  uppercase line-clamp-1">
+				{field.displayName}
 			</div>
-		</>
+		</div>
 	);
 }
