406cdb3232d41af4b79e4e825c97f728b24bf10b
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-09-13 11:25:58 +0200

    add suspense loading

---
+++ apps/proem/app/(pages)/(app)/space/(discover)/discover-feed.tsx
@@ -0,0 +1,37 @@
+import { Feed as FeedComponent } from "@/app/(pages)/(app)/space/(discover)/feed";
+import { Feed, defaultFeedFilter } from "@/app/data/feed";
+import { getQueryClient } from "@/components/providers/get-query-client";
+import { routes } from "@/routes";
+import { asInfiniteQueryData } from "@/utils/as-infinite-query-data";
+import { getFeedQueryKey } from "@/utils/get-feed-query-key";
+import { auth } from "@clerk/nextjs/server";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import { redirect } from "next/navigation";
+
+const getFeed = async () => {
+	const offset = 1;
+	const feed = await Feed.fromPublic(offset);
+
+	return asInfiniteQueryData(feed);
+};
+
+export default async function DiscoverFeed() {
+	const { userId } = auth();
+
+	if (userId) {
+		redirect(`${routes.space}/${userId}`);
+	}
+
+	const queryClient = getQueryClient();
+
+	queryClient.prefetchQuery({
+		queryKey: getFeedQueryKey(defaultFeedFilter),
+		queryFn: getFeed,
+	});
+
+	return (
+		<HydrationBoundary state={dehydrate(queryClient)}>
+			<FeedComponent filter={defaultFeedFilter} showThemeColors />
+		</HydrationBoundary>
+	);
+}
--- apps/proem/app/(pages)/(app)/space/(discover)/page.tsx
+++ apps/proem/app/(pages)/(app)/space/(discover)/page.tsx
@@ -1,41 +1,14 @@
-import { Feed as FeedComponent } from "@/app/(pages)/(app)/space/(discover)/feed";
-import { Feed, defaultFeedFilter } from "@/app/data/feed";
+import DiscoverFeed from "@/app/(pages)/(app)/space/(discover)/discover-feed";
 import { OnboardingCarousel } from "@/components/onboarding";
-import { getQueryClient } from "@/components/providers/get-query-client";
-import { routes } from "@/routes";
-import { asInfiniteQueryData } from "@/utils/as-infinite-query-data";
-import { getFeedQueryKey } from "@/utils/get-feed-query-key";
-import { auth } from "@clerk/nextjs/server";
-import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
-import { redirect } from "next/navigation";
-
-const getFeed = async () => {
-	const offset = 1;
-	const feed = await Feed.fromPublic(offset);
-
-	return asInfiniteQueryData(feed);
-};
+import { Suspense } from "react";
 
 export default async function DiscoverPage() {
-	const { userId } = auth();
-
-	if (userId) {
-		redirect(`${routes.space}/${userId}`);
-	}
-
-	const queryClient = getQueryClient();
-
-	queryClient.prefetchQuery({
-		queryKey: getFeedQueryKey(defaultFeedFilter),
-		queryFn: getFeed,
-	});
-
 	return (
 		<div className="space-y-4">
 			<OnboardingCarousel />
-			<HydrationBoundary state={dehydrate(queryClient)}>
-				<FeedComponent filter={defaultFeedFilter} showThemeColors />
-			</HydrationBoundary>
+			<Suspense fallback={<h3>suspense loading...</h3>}>
+				<DiscoverFeed />
+			</Suspense>
 		</div>
 	);
 }
