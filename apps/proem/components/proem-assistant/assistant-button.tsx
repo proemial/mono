98d60bbf672c353a1ea20b40f2ef0ba654ee6131
Michael Lajlev <lajlev@gmail.com>
2024-09-13 12:55:24 +0200

    Visual improvement

--- apps/proem/components/proem-assistant/assistant-button.tsx
+++ apps/proem/components/proem-assistant/assistant-button.tsx
@@ -12,7 +12,7 @@ export const AssistantButton = ({ onClick, variant = "dark" }: Props) => {
 			variant="suggestion"
 			size="suggestion"
 			className={cn(
-				"drop-shadow-xl hover:drop-shadow-lg pointer-events-auto mx-8 justify-center rounded-full p-3",
+				"drop-shadow-xl hover:drop-shadow-lg pointer-events-auto mx-8 justify-center rounded-full p-3 max-w-[480px]",
 				{
 					"bg-theme-900 hover:bg-theme-950 text-white": variant === "dark",
 					"hover:bg-theme-200": variant === "light",
