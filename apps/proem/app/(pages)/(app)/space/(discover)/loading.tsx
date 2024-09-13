cdaf46529b0c6d72327f63e9d02fa92c447f232f
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-09-13 11:33:43 +0200

    use throbber on page

--- apps/proem/app/(pages)/(app)/space/(discover)/loading.tsx
+++
@@ -1,6 +0,0 @@
-import { Throbber } from "@/components/throbber";
-import React from "react";
-
-export default function Loading() {
-	return <Throbber />;
-}
--- apps/proem/app/(pages)/(app)/space/(discover)/page.tsx
+++ apps/proem/app/(pages)/(app)/space/(discover)/page.tsx
@@ -1,12 +1,13 @@
 import DiscoverFeed from "@/app/(pages)/(app)/space/(discover)/discover-feed";
 import { OnboardingCarousel } from "@/components/onboarding";
+import { Throbber } from "@/components/throbber";
 import { Suspense } from "react";
 
 export default async function DiscoverPage() {
 	return (
 		<div className="space-y-4">
 			<OnboardingCarousel />
-			<Suspense fallback={<h3>suspense loading...</h3>}>
+			<Suspense fallback={<Throbber />}>
 				<DiscoverFeed />
 			</Suspense>
 		</div>
