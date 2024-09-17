73179bb81f9a5d8922a273481a097dc13a876b49
Michael Lajlev <lajlev@gmail.com>
2024-09-17 10:45:55 +0200

    better select icon

--- packages/shadcn-ui/components/ui/select.tsx
+++ packages/shadcn-ui/components/ui/select.tsx
@@ -125,9 +125,9 @@ const SelectItem = React.forwardRef<
 		)}
 		{...props}
 	>
-		<span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+		<span className="absolute left-4 flex h-3.5 w-3.5 items-center justify-center">
 			<SelectPrimitive.ItemIndicator>
-				<Check className="w-4 h-4" />
+				<ChevronRight className="w-4 h-4" />
 			</SelectPrimitive.ItemIndicator>
 		</span>
 
