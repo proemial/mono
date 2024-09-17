a0ec62d62414f0952ba3c5dfff5bab0297f947ef
Rasmus Hjulskov <hello@rasmushjulskov.com>
2024-09-17 14:34:11 +0200

    structure Paper.Preview structure

--- packages/data/paper/index.ts
+++ packages/data/paper/index.ts
@@ -1,8 +1,36 @@
 import { count, desc, eq, sql } from "drizzle-orm";
+import { z } from "zod";
 import { neonDb } from "../neon/db";
 import { paperReads, papers, posts } from "../neon/schema";
 
 export namespace Paper {
+	export const Preview = z.object({
+		id: z.string(),
+		data: z.object({
+			title: z.string(),
+			// authors: z.array(z.string()),
+			// topics: z.array(z.string()),
+			// date: created_date & publication_date
+		}),
+		generated: z.object({
+			tags: z.array(z.string()),
+			hasAbstract: z.boolean(),
+			title: z.string(),
+			field: z.string(),
+			provider: z.string(),
+		}),
+		// TODO! readers
+		// TODO! posts
+	});
+
+	export type Preview = z.infer<typeof Preview>;
+
+	export const Full = Preview.extend({
+		abstract: z.string(),
+	});
+
+	export type Full = z.infer<typeof Full>;
+
 	export const getByPopularity = ({
 		limit = 5,
 		offset = 0,
