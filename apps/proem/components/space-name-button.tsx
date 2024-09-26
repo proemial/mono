119a029691414422c8ee1d85d16658c8c8b5e108
proemian <brian@proemial.ai>
2024-09-26 15:21:38 +0200

    swap tracking and scroll

--- apps/proem/components/space-name-button.tsx
+++ apps/proem/components/space-name-button.tsx
@@ -20,7 +20,6 @@ export function GotoSpaceButton({ collectionId, userId, children }: Props) {
 	const router = useRouter();
 
 	const handleClick = () => {
-		trackHandler(analyticsKeys.read.click.spaceGoto)();
 		router.push(`/space/${collectionId}`);
 		setTimeout(
 			() =>
@@ -30,6 +29,7 @@ export function GotoSpaceButton({ collectionId, userId, children }: Props) {
 				}),
 			200,
 		);
+		trackHandler(analyticsKeys.read.click.spaceGoto)();
 	};
 
 	return (
