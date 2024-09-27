2bc7b2034c8600646cc1a5ebb70db0ae308d2f84
proemian <brian@proemial.ai>
2024-09-27 09:03:44 +0200

    Workaround for upstash ax request size exceeded

--- packages/redis/adapters/papers.ts
+++ packages/redis/adapters/papers.ts
@@ -25,7 +25,7 @@ export const OpenAlexPapers = {
 		}
 		try {
 			return (await UpStash.papers().mget(
-				ids.map((id) => `${prefix}:${id}`),
+				ids.slice(0, 20).map((id) => `${prefix}:${id}`),
 			)) as (OpenAlexPaper | null)[];
 		} catch (error) {
 			console.error(error);
