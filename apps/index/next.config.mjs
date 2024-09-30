6519cd9b49938515a70e03a488cc7d4d22850640
proemian <brian@proemial.ai>
2024-09-30 16:30:09 +0200

    more cleanup

--- apps/index/next.config.mjs
+++ apps/index/next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	transpilePackages: [
-		"@proemial/data",
 		"@proemial/models",
-		"@proemial/redis",
 		"@proemial/repositories",
-		"@proemial/shadcn-ui",
 		"@proemial/utils",
 	],
 };
