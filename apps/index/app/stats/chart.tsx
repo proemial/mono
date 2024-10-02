ff88057ae1030b7ef610dd6dc6fedf344c079604
proemian <brian@proemial.ai>
2024-10-02 14:17:32 +0200

    Switch between all/latest

--- apps/index/app/stats/chart.tsx
+++ apps/index/app/stats/chart.tsx
@@ -2,63 +2,91 @@
 import * as Charts from "recharts";
 import { IngestionStats } from "./stats.models";
 import { transformData } from "./transform-data";
+import { useState } from "react";
 
 type Props = {
 	stats: IngestionStats;
 };
 
 export default function IngestionChart({ stats }: Props) {
-	const data = transformData(stats, true);
+	const [showAll, setShowAll] = useState(true);
+	const chartData = transformData(stats, showAll);
 
 	return (
-		<Charts.ResponsiveContainer width="100%" height={600}>
-			<Charts.ComposedChart
-				data={data}
-				margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-			>
-				<Charts.XAxis dataKey="date" strokeWidth={2} />
-				<Charts.YAxis
-					label={{ value: "total", position: "insideLeft", angle: 270 }}
-					strokeWidth={2}
-				/>
-				<Charts.YAxis
-					yAxisId="right"
-					orientation="right"
-					tickFormatter={(v) => `${v}%`}
-					label={{
-						value: "diff",
-						position: "insideRight",
-						angle: 270,
-						fill: "#ea0413",
-					}}
-					stroke="#ea0413"
-				/>
-				<Charts.Tooltip />
-				<Charts.Legend
-					formatter={(value) => (value === "diff" ? "diff %" : value)}
-				/>
-				<Charts.Area
-					type="step"
-					dataKey="total"
-					fill="#F0E6D2"
-					stroke="#D2B48C"
-				/>
-				<Charts.Bar dataKey="fetched" fill="#DAA520" />
-				<Charts.Bar dataKey="embedded" fill="#CD853F" />
-				<Charts.Bar dataKey="upserted" fill="#8B4513" />
-				<Charts.Line
-					yAxisId="right"
-					type="monotone"
-					dataKey="diff"
-					stroke="#ea0413"
-					label={{
-						position: "top",
-						formatter: (value: number) => `${value}%`,
-						fill: "#ea0413",
-						fontSize: 12,
-					}}
-				/>
-			</Charts.ComposedChart>
-		</Charts.ResponsiveContainer>
+		<>
+			<div className="flex justify-center mb-4">
+				<div className="flex items-center space-x-4">
+					<label>
+						<input
+							type="radio"
+							value="all"
+							checked={showAll}
+							onChange={() => setShowAll(true)}
+							className="mr-1"
+						/>
+						All
+					</label>
+					<label>
+						<input
+							type="radio"
+							value="latest"
+							checked={!showAll}
+							onChange={() => setShowAll(false)}
+							className="mr-1"
+						/>
+						Latest
+					</label>
+				</div>
+			</div>
+			<Charts.ResponsiveContainer width="100%" height={600}>
+				<Charts.ComposedChart
+					data={chartData}
+					margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+				>
+					<Charts.XAxis dataKey="date" strokeWidth={2} />
+					<Charts.YAxis
+						label={{ value: "total", position: "insideLeft", angle: 270 }}
+						strokeWidth={2}
+					/>
+					<Charts.YAxis
+						yAxisId="right"
+						orientation="right"
+						tickFormatter={(v) => `${v}%`}
+						label={{
+							value: "diff",
+							position: "insideRight",
+							angle: 270,
+							fill: "#ea0413",
+						}}
+						stroke="#ea0413"
+					/>
+					<Charts.Tooltip />
+					<Charts.Legend
+						formatter={(value) => (value === "diff" ? "diff %" : value)}
+					/>
+					<Charts.Area
+						type="step"
+						dataKey="total"
+						fill="#F0E6D2"
+						stroke="#D2B48C"
+					/>
+					<Charts.Bar dataKey="fetched" fill="#DAA520" />
+					<Charts.Bar dataKey="embedded" fill="#CD853F" />
+					<Charts.Bar dataKey="upserted" fill="#8B4513" />
+					<Charts.Line
+						yAxisId="right"
+						type="monotone"
+						dataKey="diff"
+						stroke="#ea0413"
+						label={{
+							position: "top",
+							formatter: (value: number) => `${value}%`,
+							fill: "#ea0413",
+							fontSize: 12,
+						}}
+					/>
+				</Charts.ComposedChart>
+			</Charts.ResponsiveContainer>
+		</>
 	);
 }
