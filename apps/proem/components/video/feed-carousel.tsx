aed2c50cc5751b0540b9a644b91c82d6a8ef20b9
Jon Sorensen <jon@proemial.ai>
2024-10-11 11:17:50 +0200

    json data structure for videoes

--- apps/proem/components/video/feed-carousel.tsx
+++ apps/proem/components/video/feed-carousel.tsx
@@ -10,6 +10,7 @@ import { useEffect, useState } from "react";
 import { VideoCarouselItem } from "./video-carousel-item";
 import FeaturedLink from "@/app/(pages)/(app)/space/(discover)/featured-link";
 import { staticItems } from "@/app/(pages)/(app)/space/(discover)/random-static-item";
+import { staticVideos } from "./static-videos";
 
 export const FeedCarousel = () => {
 	const [api, setApi] = useState<CarouselApi>();
@@ -35,46 +36,17 @@ export const FeedCarousel = () => {
 			setApi={setApi}
 		>
 			<CarouselContent className="-mt-1 h-[calc(100dvh-72px)]">
-				<VideoCarouselItem
-					src="/videos/magic-mice-125x-compressed.mp4"
-					paperLink="/oa/W4402975017"
-					api={api}
-					isCurrent={current === 0}
-					muted={muted}
-					setMuted={setMuted}
-				/>
-				<VideoCarouselItem
-					src="/videos/american-friendship-w4401124912.mp4"
-					paperLink="/oa/W4401124912"
-					api={api}
-					isCurrent={current === 1}
-					muted={muted}
-					setMuted={setMuted}
-				/>
-				<VideoCarouselItem
-					src="/videos/chubby-cheek-babies-w4400380733.mp4"
-					paperLink="/oa/W4400380733"
-					api={api}
-					isCurrent={current === 2}
-					muted={muted}
-					setMuted={setMuted}
-				/>
-				<VideoCarouselItem
-					src="/videos/covid-lockdowns-w4402349677.mp4"
-					paperLink="/oa/W4402349677"
-					api={api}
-					isCurrent={current === 3}
-					muted={muted}
-					setMuted={setMuted}
-				/>
-				<VideoCarouselItem
-					src="/videos/quitting-smoking-w4400000163.mp4"
-					paperLink="/oa/W4400000163"
-					api={api}
-					isCurrent={current === 4}
-					muted={muted}
-					setMuted={setMuted}
-				/>
+				{staticVideos.map((video, index) => (
+					<VideoCarouselItem
+						key={index}
+						src={video.src}
+						paperLink={video.paperLink}
+						api={api}
+						isCurrent={current === index}
+						muted={muted}
+						setMuted={setMuted}
+					/>
+				))}
 				{/* {staticItems.map((item, index) => (
 					<CarouselItem key={index} className="py-1">
 						<FeaturedLink item={item} />
