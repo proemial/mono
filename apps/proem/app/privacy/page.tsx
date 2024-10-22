ffffc85add574f47ef96f50c4c50aecd5bb42b67
Jon Sorensen <jon@proemial.ai>
2024-10-22 15:39:14 +0200

    fix: content on terms and privacy pages have no margins

--- apps/proem/app/privacy/page.tsx
+++ apps/proem/app/privacy/page.tsx
@@ -1,5 +1,7 @@
+import { cn } from "@proemial/shadcn-ui";
 import Privacy from "./privacy";
+import { screenMaxWidth } from "../constants";
 
 export default function PrivacyPage() {
-	return <Privacy className="py-20" />;
+	return <Privacy className={cn("py-20 mx-auto", screenMaxWidth)} />;
 }
--- apps/proem/app/terms/page.tsx
+++ apps/proem/app/terms/page.tsx
@@ -1,5 +1,7 @@
+import { cn } from "@proemial/shadcn-ui";
+import { screenMaxWidth } from "../constants";
 import Terms from "./terms";
 
 export default function TermsPage() {
-	return <Terms className="py-20" />;
+	return <Terms className={cn("py-20 mx-auto", screenMaxWidth)} />;
 }
