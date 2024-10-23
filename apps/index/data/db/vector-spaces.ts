23d11209c8cfc052141415e5c740fa111ac30bbb
proemian <brian@proemial.ai>
2024-10-23 08:54:32 +0200

    remove mistral index

--- apps/index/data/db/vector-spaces.ts
+++ apps/index/data/db/vector-spaces.ts
@@ -5,16 +5,6 @@ export type VectorSpace = {
 };
 
 export const vectorSpaces: Record<string, VectorSpace> = {
-	// "0.5k": {
-	// 	collection: "o3s512alpha",
-	// 	model: "text-embedding-3-small",
-	// 	dimensions: 512,
-	// },
-	"1k": {
-		collection: "mist1024alpha",
-		model: "mistral-embed",
-		dimensions: 1024,
-	},
 	"1.5k": {
 		collection: "o3s1536alpha",
 		model: "text-embedding-3-small",
