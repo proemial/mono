b29fc3d2c680c22b2296add493740633e18c50b0
proemian <brian@proemial.ai>
2024-10-28 18:42:50 +0100

    Use url from query parameter

--- apps/proem/app/(experiments)/news/generate/facts-and-questions.tsx
+++ apps/proem/app/(experiments)/news/generate/facts-and-questions.tsx
@@ -1,37 +1,59 @@
 "use client";
 
+import { Throbber } from "@/components/throbber";
 import { toTitleCaseIfAllCaps } from "@/utils/string-utils";
 import dayjs from "dayjs";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import relativeTime from "dayjs/plugin/relativeTime";
+import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import { z } from "zod";
 import { annotateWithScienceAction } from "./actions";
 import { InputForm } from "./form";
 import { PrimaryItemSchema } from "./types";
-import Image from "next/image";
 
 dayjs.extend(localizedFormat);
 dayjs.extend(relativeTime);
 
-export const FactsAndQuestions = () => {
+export const FactsAndQuestions = ({ url }: { url?: string }) => {
 	const [factsAndQuestions, setFactsAndQuestions] = useState<
 		Awaited<ReturnType<typeof annotateWithScienceAction>> | undefined
 	>();
+	const [isLoading, setIsLoading] = useState(false);
 
 	const handleSubmit = async (data: z.infer<typeof PrimaryItemSchema>) => {
 		const result = await annotateWithScienceAction(data);
 		setFactsAndQuestions(result);
 	};
 
+	useEffect(() => {
+		if (url) {
+			void (async () => {
+				setIsLoading(true);
+				const result = await annotateWithScienceAction({ url });
+				setFactsAndQuestions(result);
+				setIsLoading(false);
+			})();
+		}
+	}, [url]);
+
 	// For generating sources of static news articles
 	console.log(JSON.stringify(factsAndQuestions?.papers));
 
 	return (
 		<div className="flex flex-col gap-6">
-			<InputForm onSubmit={handleSubmit} />
+			{!url && <InputForm onSubmit={handleSubmit} />}
+			{isLoading && (
+				<div className="flex flex-col justify-center items-center gap-2">
+					<Throbber />
+					<div>
+						Annotating <span className="font-bold underline">{url}</span> with
+						science...
+					</div>
+				</div>
+			)}
 			{factsAndQuestions && (
 				<div className="flex flex-col gap-2">
 					{factsAndQuestions.artwork && (
--- apps/proem/app/(experiments)/news/generate/page.tsx
+++ apps/proem/app/(experiments)/news/generate/page.tsx
@@ -1,10 +1,14 @@
 import { FactsAndQuestions } from "./facts-and-questions";
 
-export default async function GenerateNewsPage() {
+export default async function GenerateNewsPage({
+	searchParams,
+}: {
+	searchParams: { url?: string };
+}) {
 	return (
 		<div className="flex flex-col gap-4 mb-10 p-3 max-w-3xl mx-auto">
 			<h1 className="text-center">Annotate with Science</h1>
-			<FactsAndQuestions />
+			<FactsAndQuestions url={searchParams.url} />
 		</div>
 	);
 }
