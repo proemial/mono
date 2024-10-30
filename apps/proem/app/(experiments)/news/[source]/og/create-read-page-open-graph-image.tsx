67e44735411af2a159e1e3decd2ebff22f0bb210
proemian <brian@proemial.ai>
2024-10-30 14:59:22 +0100

    simple OG

---
+++ apps/proem/app/(experiments)/news/[source]/og/create-read-page-open-graph-image.tsx
@@ -0,0 +1,43 @@
+import { ImageResponse } from "next/og";
+import { ReadOpenGraphCard } from "./read-og-card";
+import { Redis } from "@proemial/adapters/redis";
+
+export async function createReadPageOpenGraphImage(
+	url: string,
+	size: { width: number; height: number },
+) {
+	const paper = await Redis.news.get(url);
+	if (!paper) {
+		throw new Error("no item found for url");
+	}
+
+	const latoBold = await fetch(
+		new URL("../../../../../assets/fonts/lato/Lato-Bold.ttf", import.meta.url),
+	).then((res) => res.arrayBuffer());
+
+	const latoRegular = await fetch(
+		new URL(
+			"../../../../../assets/fonts/lato/Lato-Regular.ttf",
+			import.meta.url,
+		),
+	).then((res) => res.arrayBuffer());
+
+	return new ImageResponse(
+		<ReadOpenGraphCard item={paper} classNameAttr="tw" />,
+		{
+			...size,
+			fonts: [
+				{
+					name: "lato",
+					data: latoRegular,
+					weight: 400,
+				},
+				{
+					name: "lato",
+					data: latoBold,
+					weight: 700,
+				},
+			],
+		},
+	);
+}
---
+++ apps/proem/app/(experiments)/news/[source]/og/route.tsx
@@ -0,0 +1,18 @@
+import { createReadPageOpenGraphImage } from "./create-read-page-open-graph-image";
+
+export const runtime = "edge";
+
+export async function GET(request: Request) {
+	const { searchParams } = new URL(request.url);
+	const url = searchParams.get("source");
+	console.log("SEARCH PARAMS", searchParams, url);
+
+	if (!url) {
+		throw new Error("No url provided");
+	}
+
+	return await createReadPageOpenGraphImage(url, {
+		width: 1200,
+		height: 630,
+	});
+}
