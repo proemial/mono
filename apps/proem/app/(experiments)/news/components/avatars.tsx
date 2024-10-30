b5027a60c86f6a8845bb5a05ba5a62d2681c1865
proemian <brian@proemial.ai>
2024-10-30 20:40:22 +0100

    cleanup

--- apps/proem/app/(experiments)/news/components/avatars.tsx
+++ apps/proem/app/(experiments)/news/components/avatars.tsx
@@ -1,5 +1,15 @@
-import { cn, Icons } from "@proemial/shadcn-ui";
+import { Icons } from "@proemial/shadcn-ui";
 import { numberFrom } from "@proemial/utils/string";
+import { sha256 } from "js-sha256";
+
+export module Gravatar {
+	export const getGravatarURL = (email: string) => {
+		const address = String(email).trim().toLowerCase();
+		const hash = sha256(address);
+
+		return `https://www.gravatar.com/avatar/${hash}`;
+	};
+}
 
 export function Avatar({ seed }: { seed: string }) {
 	return (
