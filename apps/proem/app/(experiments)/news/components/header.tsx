7641257afb74fc4ed5d2dacfa0db5ff70d9950d3
Michael Lajlev <lajlev@gmail.com>
2024-10-30 16:36:05 +0100

    Add article btn to header

--- apps/proem/app/(experiments)/news/components/header.tsx
+++ apps/proem/app/(experiments)/news/components/header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export function Header() {
 	return (
-		<div className="flex items-center gap-2 p-4 relative self-stretch w-full flex-[0_0_auto] bg-[#000000]">
+		<div className="flex items-center gap-2 p-4 relative self-stretch w-full flex-[0_0_auto] bg-black">
 			<div className="items-center gap-2 flex-1 grow flex relative">
 				<Image
 					className="relative w-[10.51px] h-4"
@@ -13,10 +13,16 @@ export function Header() {
 
 				<div className="relative flex-1 font-semibold text-[#f6f5e8] text-base tracking-[0] leading-4">
 					proem
-					<span className="text-[#93938f] pl-3 font-normal">
+					<span className="text-[#93938f] pl-2 font-normal text-sm">
 						trustworthy perspectives
 					</span>
 				</div>
+				<a
+					href="/news/annotate"
+					className=" text-[#f6f5e8] text-sm font-medium hover:underline"
+				>
+					+ Add Article
+				</a>
 			</div>
 		</div>
 	);
