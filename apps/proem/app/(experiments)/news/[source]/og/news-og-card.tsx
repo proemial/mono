93b9858eb4ac3226d21841ea4a24ec34aacd96ff
Michael Lajlev <lajlev@gmail.com>
2024-10-31 15:25:43 +0100

    better still wip open graph image

--- apps/proem/app/(experiments)/news/[source]/og/news-og-card.tsx
+++ apps/proem/app/(experiments)/news/[source]/og/news-og-card.tsx
@@ -25,7 +25,7 @@ export function ReadOpenGraphCard({
 	return (
 		<div
 			{...twcl(
-				"flex flex-col w-full h-full font-sans text-[48px] leading-[48px]",
+				"flex flex-col w-full h-full px-8 font-sans text-[40px] leading-[54px]",
 			)}
 			style={{
 				backgroundImage: `url(${item.source?.image})`,
@@ -34,36 +34,50 @@ export function ReadOpenGraphCard({
 				backgroundColor: "#474747", // Fallback color
 			}}
 		>
-			<div {...twcl("flex flex-1")} />
+			<div {...twcl("flex flex-col h-full  justify-end")}>
+				<div {...twcl("flex flex-grow flex-col")} />
 
-			<div {...twcl("flex flex-1 items-end")}>
-				<div {...twcl("flex w-1/2 mx-8 p-8 bg-white rounded-lg shadow-2xl")}>
-					{truncatedQuestion}
-					{questionIsTruncated && " ..."}
+				<div {...twcl("flex flex-row w-2/3 items-start")}>
+					<div
+						{...twcl(
+							"w-[72px] h-[72px] p-4 mt-4 bg-black/90 rounded-full flex items-center justify-center shadow-2xl",
+						)}
+					>
+						<div {...twcl("w-[24px] flex items-center justify-center")}>🤔</div>
+					</div>
+					<div
+						{...twcl(
+							"flex flex-col flex-1 w-20 ml-4 p-8 bg-white rounded-[48px] shadow-2xl",
+						)}
+					>
+						{truncatedQuestion}
+						{questionIsTruncated && " ..."}
+					</div>
 				</div>
-			</div>
 
-			<div {...twcl("flex flex-row flex-1 pl-16 pb-8 items-end")}>
 				<div
-					{...twcl(
-						"w-[72px] h-[72px] p-4 bg-black rounded-full flex items-center justify-center",
-					)}
+					{...twcl("flex flex-row w-2/3 flex-1 pl-24 pb-8 mt-8 items-start")}
 				>
-					<img
-						{...twcl("w-[24px]")}
-						src={`${env.NEXT_PUBLIC_VERCEL_URL}/open-graph/logo-green.svg`}
-						alt=""
-					/>
+					<div
+						{...twcl(
+							"w-[72px] h-[72px] p-4 mt-4 bg-black rounded-full flex items-center justify-center shadow-2xl",
+						)}
+					>
+						<img
+							{...twcl("w-[24px]")}
+							src={`${env.NEXT_PUBLIC_VERCEL_URL}/open-graph/logo-green.svg`}
+							alt=""
+						/>
+					</div>
+					<div
+						{...twcl(
+							"flex flex-col flex-1 w-20 ml-4 p-8 bg-white rounded-[48px] shadow-2xl",
+						)}
+					>
+						{truncatedAnswer}
+						{answerIsTruncated && " ..."}
+					</div>
 				</div>
-				<div
-					{...twcl(
-						"flex flex-col flex-1 w-1/2 ml-16 mb-[-92px] p-8 bg-white rounded-lg shadow-2xl",
-					)}
-				>
-					{truncatedAnswer}
-					{answerIsTruncated && " ..."}
-				</div>
-				<div {...twcl("flex flex-1")} />
 			</div>
 		</div>
 	);
