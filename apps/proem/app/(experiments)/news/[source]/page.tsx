543d2b6fc8512a362b08e68c65611a2557ee97bd
proemian <brian@proemial.ai>
2024-11-01 11:52:03 +0100

    dynamic matadata

--- apps/proem/app/(experiments)/news/[source]/page.tsx
+++ apps/proem/app/(experiments)/news/[source]/page.tsx
@@ -15,18 +15,30 @@ type Props = {
 // Max out the duration to allow for long running tasks
 export const maxDuration = 60;
 
-export const metadata: Metadata = {
-	title: "proem - trustworthy perspectives",
-	description:
-		"Proem takes any piece of online content and enriches it with scientific insights from the latest research papers.",
-};
+export async function generateMetadata(props: Props) {
+	const { item } = await fetchItem(props);
 
-export default async function UrlPage({ params, searchParams }: Props) {
-	const decodedUrl =
+	return {
+		title: item?.scrape?.title
+			? `${item?.scrape?.title} | proem`
+			: "proem - trustworthy perspectives",
+		description:
+			"Proem takes any piece of online content and enriches it with scientific insights from the latest research papers.",
+	};
+}
+
+export default async function UrlPage(props: Props) {
+	const { item, url } = await fetchItem(props);
+	return <Scaffold url={url} data={item} />;
+}
+
+async function fetchItem({ params, searchParams }: Props) {
+	const url =
 		params.source === "annotate" && searchParams?.url
 			? decodeURIComponent(searchParams.url)
 			: decodeURIComponent(params.source);
 
-	const item = await Redis.news.get(decodedUrl);
-	return <Scaffold url={decodedUrl} data={item} />;
+	const item = await Redis.news.get(url);
+
+	return { url, item };
 }
