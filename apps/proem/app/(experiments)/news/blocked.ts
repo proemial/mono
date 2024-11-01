892cd12e85c5b7da1fcb66fd71d4fc26d524460c
proemian <brian@proemial.ai>
2024-11-01 08:56:10 +0100

    also block insta and twitter

--- apps/proem/app/(experiments)/news/blocked.ts
+++ apps/proem/app/(experiments)/news/blocked.ts
@@ -2,5 +2,11 @@ export function isBlockedUrl(url: string) {
 	if (url.includes("facebook.com") || url.includes("fb.com")) {
 		return "We cannot make it past the Facebook login wall 😔";
 	}
+	if (url.includes("instagram.com")) {
+		return "We cannot make it past the Instagram login wall 😔";
+	}
+	if (url.includes("twitter.com") || url.includes("x.com")) {
+		return "We cannot make it past the Twitter/X login wall 😔";
+	}
 	return undefined;
 }
