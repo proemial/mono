b09d20ceb16c5dc3b968386c16f3078075709ef8
mads rydahl <mads@proemial.ai>
2024-11-04 14:43:29 +0100

    references, show date first, fallback to show author name

--- apps/proem/app/(experiments)/news/[source]/components/references/paper.tsx
+++ apps/proem/app/(experiments)/news/[source]/components/references/paper.tsx
@@ -3,16 +3,18 @@ import { ReferencedPaper } from "@proemial/adapters/redis/news";
 export function Paper({ paper }: { paper?: ReferencedPaper }) {
 	const source =
 		paper?.primary_location?.source?.display_name ||
-		paper?.primary_location?.source?.host_organization_name;
+		paper?.primary_location?.source?.host_organization_name ||
+		paper?.authorships[0]?.author.display_name;
 
+		console.log(paper.authorships[0]);
 	return (
 		<div className="flex flex-col items-start gap-[3px] relative flex-1 grow">
 			<p className="relative self-stretch mt-[-1.00px] font-medium text-[#08080a] text-xs tracking-[0] leading-[normal] overflow-hidden text-ellipsis [display:-webkit-box] [-webkit-line-clamp:1] [-webkit-box-orient:vertical]">
 				{paper?.title}
 			</p>
 
-			<p className="relative self-stretch font-normal text-[#757989] text-xs tracking-[0] leading-[normal]">
-				{source} ({paper?.published})
+			<p className="relative self-stretch font-normal text-[#757989] text-xs tracking-[0] leading-[normal] overflow-hidden text-ellipsis  [display:-webkit-box] [-webkit-line-clamp:1] [-webkit-box-orient:vertical]">
+				{paper?.published}, {source}
 			</p>
 		</div>
 	);
