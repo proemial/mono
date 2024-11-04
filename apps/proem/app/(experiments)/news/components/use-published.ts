f4e9e4f4a0832d2ab95441e8179a9d6bc2f32a5f
proemian <brian@proemial.ai>
2024-11-04 09:51:45 +0100

    fix parameter bug

--- apps/proem/app/(experiments)/news/components/use-published.ts
+++ apps/proem/app/(experiments)/news/components/use-published.ts
@@ -2,5 +2,5 @@ import { useSearchParams } from "next/navigation";
 
 export const useFromFeedSearchParam = () => {
 	const searchParams = useSearchParams();
-	return { fromFeedParam: searchParams.get("f") === "1" };
+	return { fromFeedParam: searchParams.get("p") === "1" };
 };
