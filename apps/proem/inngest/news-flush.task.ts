5a401f04094d36ef468f494dec8522295c44d2ae
proemian <brian@proemial.ai>
2024-11-07 17:11:17 +0100

    cache flush task

---
+++ apps/proem/inngest/news-flush.task.ts
@@ -0,0 +1,29 @@
+import { Time } from "@proemial/utils/time";
+import { inngest } from "./client";
+import { getItems } from "@/app/(experiments)/news/cached-items";
+import { revalidateTag } from "next/dist/server/web/spec-extension/revalidate";
+
+const eventName = "news/flush";
+const eventId = "news/flush/fn";
+
+export const NewsFlushTask = {
+	name: eventName,
+	worker: inngest.createFunction(
+		{ id: eventId, concurrency: 1 },
+		{ event: eventName },
+		async ({ event }) => {
+			const begin = Time.now();
+
+			revalidateTag("news-feed");
+			const items = await getItems(true);
+
+			return {
+				event,
+				body: {
+					count: items.length,
+					elapsed: Time.elapsed(begin),
+				},
+			};
+		},
+	),
+};
--- apps/proem/inngest/scheduler.task.ts
+++ apps/proem/inngest/scheduler.task.ts
@@ -3,6 +3,19 @@ import { inngest } from "./client";
 import { streamScheduledCacheUpdate } from "./populator.task";
 import { getAllSpaceIds } from "@proemial/data/repository/collection";
 import { getAllUserIds } from "@proemial/data/repository/user";
+import { NewsFlushTask } from "./news-flush.task";
+
+export const newsFlushScheduler = inngest.createFunction(
+	{ id: `schedule/${NewsFlushTask.name}` },
+	{ cron: "*/5 * * * *" },
+	async ({ event }) => {
+		const result = await inngest.send({
+			name: NewsFlushTask.name,
+		});
+
+		return { event, body: { result: result.ids } };
+	},
+);
 
 export const streamCacheUpdateScheduler = inngest.createFunction(
 	{ id: "streams/cache-update-scheduler" },
