6b6ef80b5a7b28c582b75983768693a483439b53
mads rydahl <mads@proemial.ai>
2024-11-14 17:02:06 +0100

    larger annotate splash, less focus on past stories

--- apps/proem/app/(pages)/news/components/annotate-form.tsx
+++ apps/proem/app/(pages)/news/components/annotate-form.tsx
@@ -19,74 +19,70 @@ export function AnnotateForm() {
 
 	return (
 		<>
-			{isOpen && (
+			{(
 				<div
-					className="flex flex-col gap-8 lg:mx-4 items-center justify-center self-stretch px-4 mb-4 bg-gradient-to-b from-[#7DFA86] from-[-25%] via-[#3732916b] via-[70%] to-[#24215f6b] to-[100%] rounded-2xl"
-					style={{
-						animation: isOpen ? "expand 0.6s ease-in-out forwards" : "none",
-					}}
+					className="flex flex-col items-center justify-between self-stretch p-4 mb-4 lg:mx-8 bg-gradient-to-b from-[#7DFA86] from-[-25%] via-[#3732916b] via-[75%] to-[#312d7b6b] to-[100%] rounded-2xl h-[75vh]"
 				>
-					<style jsx>{`
-						@keyframes expand {
-							from { height: 0;  }
-							to { height: 250px;  }
-						}
-					`}</style>
-					<div className="w-5/6 text-xl font-semibold leading-7 text-center text-white drop-shadow-md">
-						Enrich news articles with insights
-						<br />
-						from the latest research papers.
-					</div>
-					<div className="flex gap-1 justify-center items-center w-4/5">
-						<form
-							onSubmit={(e) => {
-								console.log("submit");
-								Tracker.track(analyticsKeys.experiments.news.clickGenerate);
+					<div className="flex-1 flex flex-col items-center justify-center gap-8 w-full">
+						<div className="w-5/6 text-xl font-semibold leading-7 text-center text-white drop-shadow-md">
+							Enrich news articles with insights
+							<br />
+							from the latest research papers.
+						</div>
+						<div className="flex gap-1 justify-center items-center w-full px-4">
+							<form
+								onSubmit={(e) => {
+									console.log("submit");
+									Tracker.track(analyticsKeys.experiments.news.clickGenerate);
 
-								e.preventDefault();
-								const form = e.target as HTMLFormElement;
-								const url = form.url.value;
-								const isBlockedError = isBlockedUrl(url);
+									e.preventDefault();
+									const form = e.target as HTMLFormElement;
+									const url = form.url.value;
+									const isBlockedError = isBlockedUrl(url);
 
-								// Check if it's a blocked URL
-								if (isBlockedError) {
-									const errorDiv = form.querySelector(
-										".error-message",
-									) as HTMLDivElement;
-									errorDiv.textContent = isBlockedError;
-									return;
-								}
+									// Check if it's a blocked URL
+									if (isBlockedError) {
+										const errorDiv = form.querySelector(
+											".error-message",
+										) as HTMLDivElement;
+										errorDiv.textContent = isBlockedError;
+										return;
+									}
 
-								form.querySelectorAll("button, input").forEach((el) => {
-									(el as HTMLElement).setAttribute("disabled", "true");
-								});
-								window.location.href = `/news/${encodeURIComponent(url)}`;
-							}}
-							className="flex flex-col gap-4"
-						>
-							<div className="flex w-80 lg:w-[500px] items-center gap-2.5 p-4 rounded-full border border-white/80 bg-black/50">
-								<input
-									type="url"
-									name="url"
-									placeholder="Enter url"
-									pattern="https?://.*"
-									title="Please enter a valid URL starting with http:// or https://"
-									className="font-normal text-white/50 text-sm leading-[14px] bg-transparent border-none outline-none w-full"
-									onChange={(e) => setUrl(e.target.value)} // Add this line
-									value={url} // Add this line
-									required
-								/>
-								<button type="submit" disabled={!url.match(/^https?:\/\/.+/)}>
-									<PlusCircle className="text-[#f6f5e8] hsize-6 block hover:animate-[spin_1s_ease-in-out] cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed" />
-								</button>
-							</div>
-							{isApp && (
-								<div className="text-center italic text-xs mt-[-12px] text-gray-500">
-									Pro tip: Share to proem directly from your browser.
+									form.querySelectorAll("button, input").forEach((el) => {
+										(el as HTMLElement).setAttribute("disabled", "true");
+									});
+									window.location.href = `/news/${encodeURIComponent(url)}`;
+								}}
+								className="flex flex-col gap-4 w-full max-w-[500px]"
+							>
+								<div className="flex w-full items-center gap-2.5 p-4 pl-5 rounded-full border border-white bg-black/50">
+									<input
+										type="url"
+										name="url"
+										placeholder="Paste news article URL"
+										pattern="https?://.*"
+										title="Please enter a valid URL starting with http:// or https://"
+										className="font-normal text-white/50 text-sm leading-[14px] bg-transparent border-none outline-none w-full"
+										onChange={(e) => setUrl(e.target.value)}
+										value={url}
+										required
+									/>
+									<button type="submit" disabled={!url.match(/^https?:\/\/.+/)}>
+										<PlusCircle className="text-[#f6f5e8] hsize-6 block hover:animate-[spin_1s_ease-in-out] cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed" />
+									</button>
 								</div>
-							)}
-							<div className="error-message text-sm text-red-500" />
-						</form>
+								{isApp && (
+									<div className="text-center italic text-xs mt-[-12px] text-gray-500">
+										Pro tip: Share to proem directly from your browser.
+									</div>
+								)}
+								<div className="error-message text-sm text-red-500" />
+							</form>
+						</div>
+					</div>
+					<div className="w-5/6 text-base pb-4 font-semibold leading-7 text-center text-white/60 drop-shadow-md">
+						Or browse articles enriched by other users:
 					</div>
 				</div>
 			)}
