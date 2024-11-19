653e2f2eafef5e02e5f4c2ad6ba4b901d80c856e
proemian <brian@proemial.ai>
2024-11-19 22:35:57 +0100

    summarise using sonnet

--- apps/proem/app/api/ai/models/index.ts
+++ apps/proem/app/api/ai/models/index.ts
@@ -12,22 +12,22 @@ const gpt4o = openai("gpt-4o");
 const sonnet = anthropic("claude-3-5-sonnet-latest");
 
 const defaultModel = flash;
-const answersModels = sonnet;
+const answersModel = sonnet;
 
 const LlmModels = {
 	ask: {
 		rephrase: () => logModel("ask.rephrase", defaultModel) as LlmModel,
-		answer: () => logModel("ask.answer", answersModels) as LlmModel,
+		answer: () => logModel("ask.answer", answersModel) as LlmModel,
 		followups: () => logModel("ask.followups", defaultModel) as LlmModel,
 	},
 	news: {
 		rephrase: () => logModel("news.rephrase", defaultModel) as LlmModel,
-		summarise: () => logModel("news.summarise", defaultModel) as LlmModel,
-		answer: () => logModel("news.answer", answersModels) as LlmModel,
+		summarise: () => logModel("news.summarise", answersModel) as LlmModel,
+		answer: () => logModel("news.answer", answersModel) as LlmModel,
 		followups: () => logModel("news.followups", defaultModel) as LlmModel,
 	},
 	assistant: {
-		answer: () => logModel("assistant.answer", answersModels) as LlmModel,
+		answer: () => logModel("assistant.answer", answersModel) as LlmModel,
 	},
 };
 
