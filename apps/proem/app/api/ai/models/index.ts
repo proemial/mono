0c813590c63ef1f14734fd614167cf1b16a30af1
proemian <brian@proemial.ai>
2024-11-20 12:14:10 +0100

    add bedrock

--- apps/proem/app/api/ai/models/index.ts
+++ apps/proem/app/api/ai/models/index.ts
@@ -1,15 +1,18 @@
 import { google } from "@ai-sdk/google";
 import { openai } from "@ai-sdk/openai";
 import { anthropic } from "@ai-sdk/anthropic";
+import { createAmazonBedrock } from "@ai-sdk/amazon-bedrock";
 
 export type LlmModel =
 	| ReturnType<typeof google>
 	| ReturnType<typeof openai>
-	| ReturnType<typeof anthropic>;
+	| ReturnType<typeof anthropic>
+	| ReturnType<typeof bedrock>;
 
 const flash = google("gemini-1.5-flash");
 const gpt4o = openai("gpt-4o");
 const sonnet = anthropic("claude-3-5-sonnet-20240620");
+const bedrockSonnet = bedrock("us.anthropic.claude-3-5-sonnet-20241022-v2:0");
 
 const defaultModel = gpt4o;
 const answersModel = sonnet;
@@ -30,6 +33,13 @@ const LlmModels = {
 		answer: () => logModel("assistant.answer", answersModel) as LlmModel,
 	},
 };
+
+function bedrock(modelId: string) {
+	const awsBedrock = createAmazonBedrock({
+		region: "us-east-1",
+	});
+	return awsBedrock(modelId) as ReturnType<typeof awsBedrock>;
+}
 
 function logModel(key: string, model: LlmModel) {
 	console.log(`[${key}]: ${model.modelId}`);
