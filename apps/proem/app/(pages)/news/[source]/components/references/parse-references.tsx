0945ca99139785b2cf82edff29790f4b02e20189
proemian <brian@proemial.ai>
2024-11-22 13:01:52 +0100

    test static hashes fix

--- apps/proem/app/(pages)/news/[source]/components/references/parse-references.tsx
+++ apps/proem/app/(pages)/news/[source]/components/references/parse-references.tsx
@@ -2,6 +2,7 @@ import { analyticsKeys } from "@/components/analytics/tracking/tracking-keys";
 import { Trackable } from "@/components/trackable";
 import { uuid } from "@proemial/utils/uid";
 import { useEffect, useState } from "react";
+import { splitAndSanitize } from "./santise-references";
 
 export function useTextWithReferences(text?: string) {
 	const [withReferences, setWithReferences] =
@@ -23,55 +24,47 @@ export function useTextWithReferences(text?: string) {
 	);
 }
 
-function processTextWithReferences(children: string) {
+export function processTextWithReferences(text: string) {
 	const references = new Set<number>();
 	const prefix = uuid();
 
-	const markup = children
-		.replace(/^\s*-\s*/gm, "")
-		.replace(/\[#(\d+)\]/g, "[$1]")
-		.split(/(\[.*?\])/)
-		// Untested: replace static hashes [#] with a running index in brackets, ie. [#], [#], [#]  should become [1], [2], [3]
-		.map((segment, i) => segment.replace("[#]", `[${i + 1}]`))
-		.map((segment, i) => {
-			const match = segment.match(/\[(.*?)\]/);
-			if (match) {
-				const numbers = match[1]?.split(",").map((n) => n.trim());
-				numbers?.forEach((n) => references.add(Number.parseInt(n)));
+	const markup = splitAndSanitize(text).map((segment, i) => {
+		const match = segment.match(/\[(.*?)\]/);
+		if (match) {
+			const numbers = match[1]?.split(",").map((n) => n.trim());
+			numbers?.forEach((n) => references.add(Number.parseInt(n)));
 
-				return numbers?.map((num, j) => (
-					<Trackable
-						key={`${i}-${j}`}
-						trackingKey={
-							analyticsKeys.experiments.news.item.clickInlineReference
-						}
-					>
-						<span className="">
-							<a
-								href={`#${prefix}-${num}`}
-								key={`${i}-${j}`}
-								onClick={() =>
-									document
-										.getElementById("sources")
-										?.scrollIntoView({ behavior: "smooth" })
-								}
-								className="items-center justify-center rounded-full bg-[#0A161C] text-white text-[10px] font-[1000] cursor-pointer hover:bg-gray-800 mb-4"
-								style={{
-									padding: "2px 5px",
-									marginRight: "2px",
-									position: "relative",
-									top: "-2px",
-								}}
-							>
-								{num}
-							</a>
-						</span>
-					</Trackable>
-				));
-			}
-			// Return regular text for non-link segments
-			return segment;
-		});
+			return numbers?.map((num, j) => (
+				<Trackable
+					key={`${i}-${j}`}
+					trackingKey={analyticsKeys.experiments.news.item.clickInlineReference}
+				>
+					<span className="">
+						<a
+							href={`#${prefix}-${num}`}
+							key={`${i}-${j}`}
+							onClick={() =>
+								document
+									.getElementById("sources")
+									?.scrollIntoView({ behavior: "smooth" })
+							}
+							className="items-center justify-center rounded-full bg-[#0A161C] text-white text-[10px] font-[1000] cursor-pointer hover:bg-gray-800 mb-4"
+							style={{
+								padding: "2px 5px",
+								marginRight: "2px",
+								position: "relative",
+								top: "-2px",
+							}}
+						>
+							{num}
+						</a>
+					</span>
+				</Trackable>
+			));
+		}
+		// Return regular text for non-link segments
+		return segment;
+	});
 
 	return { markup, prefix, references: Array.from(references) };
 }
