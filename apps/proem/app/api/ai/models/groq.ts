a17e6444d4169e77943a19759183962d7f508c11
Jon Sorensen <jon@proemial.ai>
2024-11-26 08:23:26 +0100

    groq llama provider

---
+++ apps/proem/app/api/ai/models/groq.ts
@@ -0,0 +1,15 @@
+import { createOpenAI } from "@ai-sdk/openai";
+import { LanguageModelV1 } from "ai";
+
+// https://console.groq.com/docs/models
+type ModelId =
+	| "llama-3.1-70b-specdec"
+	| "llama-3.1-70b-versatile"
+	| "llama-3.2-90b-vision-preview";
+
+export const groqProvider = (modelId: ModelId): LanguageModelV1 =>
+	createOpenAI({
+		name: "groq",
+		baseURL: "https://api.groq.com/openai/v1",
+		apiKey: process.env.GROQ_API_KEY,
+	})(modelId);
