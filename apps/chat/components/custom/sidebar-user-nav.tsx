f283ce663af51ecb6344e35827113917f5de8ebf
proemian <brian@proemial.ai>
2024-12-05 13:03:23 +0100

    remove UUID menu

--- apps/chat/components/custom/sidebar-user-nav.tsx
+++ apps/chat/components/custom/sidebar-user-nav.tsx
@@ -1,17 +1,7 @@
 "use client";
-import { ChevronUp } from "lucide-react";
-import Image from "next/image";
-import { type User } from "next-auth";
-import { signIn, signOut } from "next-auth/react";
 import { useTheme } from "next-themes";
+import { Toggle } from "@proemial/shadcn-ui/components/ui/toggle";
 
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuSeparator,
-	DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import {
 	SidebarMenu,
 	SidebarMenuButton,
@@ -24,61 +14,13 @@ export function SidebarUserNav({ sessionId }: { sessionId: string }) {
 	return (
 		<SidebarMenu>
 			<SidebarMenuItem>
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<SidebarMenuButton className="data-[state=open]:bg-sidebar-accent bg-background data-[state=open]:text-sidebar-accent-foreground h-10">
-							<Image
-								src={`https://avatar.vercel.sh/${sessionId}`}
-								alt={sessionId ?? "User Avatar"}
-								width={24}
-								height={24}
-								className="rounded-full"
-							/>
-							<span className="truncate">{sessionId}</span>
-							<ChevronUp className="ml-auto" />
-						</SidebarMenuButton>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent
-						side="top"
-						className="w-[--radix-popper-anchor-width]"
-					>
-						<DropdownMenuItem
-							className="cursor-pointer"
-							onSelect={() => setTheme(theme === "dark" ? "light" : "dark")}
-						>
-							{`Toggle ${theme === "light" ? "dark" : "light"} mode`}
-						</DropdownMenuItem>
-						{/* <DropdownMenuSeparator />
-						<DropdownMenuItem asChild>
-							<>
-								{user?.email && (
-									<button
-										type="button"
-										className="w-full cursor-pointer"
-										onClick={() => {
-											signOut({
-												redirectTo: "/",
-											});
-										}}
-									>
-										Sign out
-									</button>
-								)}
-								{!user?.email && (
-									<button
-										type="button"
-										className="w-full cursor-pointer"
-										onClick={() => {
-											signIn();
-										}}
-									>
-										Log in
-									</button>
-								)}
-							</>
-						</DropdownMenuItem> */}
-					</DropdownMenuContent>
-				</DropdownMenu>
+				<Toggle
+					variant="outline"
+					className="w-full"
+					onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+				>
+					{`Toggle ${theme === "light" ? "dark" : "light"} mode`}
+				</Toggle>
 			</SidebarMenuItem>
 		</SidebarMenu>
 	);
