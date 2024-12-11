28e41f6050c2ae5c7a198c3477e0a48803824542
proemian <brian@proemial.ai>
2024-12-11 14:28:19 +0100

    use refetch instead of streaming when fetching/annotating papers

---
+++ apps/chat/app/(chat)/api/paper/[id]/redis.ts
@@ -0,0 +1,46 @@
+import { Time } from "@proemial/utils/time";
+import { Redis } from "@upstash/redis";
+
+const redis = new Redis({
+	url: process.env.REDIS_PAPERS_STATE_URL,
+	token: process.env.REDIS_PAPERS_STATE_TOKEN,
+});
+
+export const RedisPaperState = {
+	get: async (identifier: string): Promise<RedisPaperState> => {
+		const begin = Time.now();
+
+		try {
+			const state = (await redis.get(`${identifier}`)) as RedisPaperState;
+
+			return state ?? { completed: [] };
+		} catch (error) {
+			console.error(error);
+			throw error;
+		} finally {
+			Time.debug(begin, `[redis][summaries][get] ${identifier}`);
+		}
+	},
+
+	update: async (
+		identifier: string,
+		state: RedisPaperState,
+	): Promise<RedisPaperState> => {
+		await redis.set(identifier, state);
+		console.log("[redis][state] updated: ", state);
+
+		return state;
+	},
+
+	isDone: (state: RedisPaperState): boolean => {
+		return (
+			state.completed.includes("fetch") &&
+			state.completed.includes("title") &&
+			state.completed.includes("description")
+		);
+	},
+};
+
+export type RedisPaperState = {
+	completed: Array<"fetch" | "title" | "description">;
+};
