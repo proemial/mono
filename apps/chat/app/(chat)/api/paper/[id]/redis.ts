7b0fdae717b3d5f25bcf8968697817e9f796fe88
proemian <brian@proemial.ai>
2024-12-11 14:37:27 +0100

    debugging

--- apps/chat/app/(chat)/api/paper/[id]/redis.ts
+++ apps/chat/app/(chat)/api/paper/[id]/redis.ts
@@ -1,6 +1,11 @@
 import { Time } from "@proemial/utils/time";
 import { Redis } from "@upstash/redis";
 
+console.log("Redis config", {
+	url: process.env.REDIS_PAPERS_STATE_URL,
+	token: process.env.REDIS_PAPERS_STATE_TOKEN,
+});
+
 const redis = new Redis({
 	url: process.env.REDIS_PAPERS_STATE_URL,
 	token: process.env.REDIS_PAPERS_STATE_TOKEN,
