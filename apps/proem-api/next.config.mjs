306c0948c703539f4d340793d8b7e8654e2e97f7
Jon Sorensen <jon@proemial.ai>
2025-01-14 10:14:53 +0100

    apply server-side fix

--- apps/proem-api/next.config.mjs
+++ apps/proem-api/next.config.mjs
@@ -4,6 +4,11 @@ const nextConfig = {
 		"@proemial/adapters",
 		"@proemial/utils",
 	],
+	// Indicate that these packages should not be bundled by webpack
+	// Source: https://github.com/huggingface/transformers.js/blob/main/examples/next-server/next.config.js
+	experimental: {
+		serverComponentsExternalPackages: ["sharp", "onnxruntime-node"],
+	},
 };
 
 export default nextConfig;
