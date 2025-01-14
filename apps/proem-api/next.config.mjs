e207be488f591c4b49fd0736b34823ab5d1b2c1f
Jon Sorensen <jon@proemial.ai>
2025-01-14 11:06:10 +0100

    remove "client" fix as we're not doing inference in the browser

--- apps/proem-api/next.config.mjs
+++ apps/proem-api/next.config.mjs
@@ -4,18 +4,10 @@ const nextConfig = {
 		"@proemial/adapters",
 		"@proemial/utils",
 	],
-	// LlamaIndex (and Huggingface transformers.js) requires sharp and onnxruntime-node to build
+	// LlamaIndex (and Huggingface transformers.js) requires sharp and onnxruntime-node to build on Vercel
 	// Source: https://huggingface.co/docs/transformers.js/en/tutorials/next
 	experimental: {
 		serverComponentsExternalPackages: ["sharp", "onnxruntime-node"],
-	},
-	webpack: (config) => {
-		config.resolve.alias = {
-			...config.resolve.alias,
-			sharp$: false,
-			"onnxruntime-node$": false,
-		};
-		return config;
 	},
 };
 
