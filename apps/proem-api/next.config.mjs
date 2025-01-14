a93d764282c19c5a60c9f91527c841a031a2b51b
Jon Sorensen <jon@proemial.ai>
2025-01-14 10:38:11 +0100

    add sharp and onnxruntime-node as peer deps

--- apps/proem-api/next.config.mjs
+++ apps/proem-api/next.config.mjs
@@ -4,11 +4,6 @@ const nextConfig = {
 		"@proemial/adapters",
 		"@proemial/utils",
 	],
-	// Indicate that these packages should not be bundled by webpack
-	// Source: https://github.com/huggingface/transformers.js/blob/main/examples/next-server/next.config.js
-	experimental: {
-		serverComponentsExternalPackages: ["sharp", "onnxruntime-node"],
-	},
 };
 
 export default nextConfig;
