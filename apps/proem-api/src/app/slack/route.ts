261c4ec93a028d828bcff96bb1f2290a960d2605
proemian <brian@proemial.ai>
2025-01-20 20:36:14 +0700

    fix returned data

--- apps/proem-api/src/app/slack/route.ts
+++ apps/proem-api/src/app/slack/route.ts
@@ -5,7 +5,39 @@ export async function POST(request: Request) {
 	const url = request.url;
 
 	console.log(url);
-	console.log(body);
+	console.log(JSON.stringify(body));
 
-	return NextResponse.json({ url, body });
+	return NextResponse.json({
+		blocks: [
+			{
+				type: "header",
+				text: {
+					type: "plain_text",
+					text: "This was not what you expected",
+					emoji: true,
+				},
+			},
+			{
+				type: "section",
+				text: {
+					type: "mrkdwn",
+					text: "... neither was this",
+				},
+			},
+			{
+				type: "actions",
+				elements: [
+					{
+						type: "button",
+						text: {
+							type: "plain_text",
+							text: "↕️ Do stuff",
+							emoji: true,
+						},
+						value: "do_stuff",
+					},
+				],
+			},
+		],
+	});
 }
