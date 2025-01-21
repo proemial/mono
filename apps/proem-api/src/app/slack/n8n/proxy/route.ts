3bd25f78e4b692c284b8579e9ec4e27b05b952b4
proemian <brian@proemial.ai>
2025-01-21 19:06:44 +0700

    forward button actions to separate workflow

--- apps/proem-api/src/app/slack/n8n/proxy/route.ts
+++ apps/proem-api/src/app/slack/n8n/proxy/route.ts
@@ -11,6 +11,23 @@ export async function POST(request: Request) {
 	const body = JSON.parse(unencoded);
 	console.log(body);
 
+	if (body.type === "block_actions") {
+		console.log("block_actions");
+		const result = await fetch(
+			"https://proem.app.n8n.cloud/webhook-test/99b44999-b7f8-499b-87f2-645063bbfd99",
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(body),
+			},
+		);
+		console.log(result);
+
+		return NextResponse.json({ body, result });
+	}
+
 	const result = await fetch(
 		"https://proem.app.n8n.cloud/webhook/51715013-5cfe-4d24-bf80-51e3d2dbe18c/webhook",
 		{
@@ -23,5 +40,8 @@ export async function POST(request: Request) {
 	);
 	console.log(result);
 
-	return NextResponse.json({ body, result });
+	return NextResponse.json(body, {
+		status: result.status,
+		statusText: result.statusText,
+	});
 }
