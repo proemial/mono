8231a5caadadb057267dda813d021e8c91982d37
proemian <brian@proemial.ai>
2025-01-24 21:41:57 +0700

    add try/catch

--- apps/proem-api/src/app/slack/broker/out/route.ts
+++ apps/proem-api/src/app/slack/broker/out/route.ts
@@ -3,25 +3,37 @@ import { NextResponse } from "next/server";
 export const revalidate = 0;
 
 export async function POST(request: Request) {
-	const text = await request.text();
-	const data = JSON.parse(text) as {
-		id: string;
-		payload: unknown;
-	};
+	try {
+		const text = await request.text();
+		const data = JSON.parse(text) as {
+			id: string;
+			payload: unknown;
+		};
 
-	console.log("/slack/broker/out");
-	console.log(JSON.stringify(data));
+		if (!data?.id || !data?.payload) {
+			return NextResponse.json(
+				{ error: "Invalid request, missing id or payload" },
+				{ status: 400 },
+			);
+		}
 
-	const result = await fetch(
-		"https://hooks.slack.com/services/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",
-		{
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
+		console.log("/slack/broker/out");
+		console.log(JSON.stringify(data));
+
+		const result = await fetch(
+			"https://hooks.slack.com/services/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",
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(data.payload),
 			},
-			body: JSON.stringify(data.payload),
-		},
-	);
+		);
 
-	return NextResponse.json({ body: data, result });
+		return NextResponse.json({ body: data, result });
+	} catch (error) {
+		console.error(error);
+		return NextResponse.json({ error: JSON.stringify(error) }, { status: 500 });
+	}
 }
