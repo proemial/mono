89594c7c5560386535d43574fbbb74bed951eec5
Jon Sorensen <jon@proemial.ai>
2025-02-11 09:26:09 +0100

    adapt from video id to url param

--- apps/proem-api/src/app/slack/helpers/scrape/yt/route.ts
+++ apps/proem-api/src/app/slack/helpers/scrape/yt/route.ts
@@ -3,13 +3,13 @@ import { z } from "zod";
 import { fetchTranscript } from "@proemial/adapters/youtube/transcript";
 
 const RequestBodySchema = z.object({
-	videoId: z.string(),
+	url: z.string(),
 });
 
 export async function POST(request: Request) {
 	try {
-		const { videoId } = RequestBodySchema.parse(await request.json());
-		const transcript = await fetchTranscript(videoId);
+		const { url } = RequestBodySchema.parse(await request.json());
+		const transcript = await fetchTranscript(url);
 		return NextResponse.json(transcript);
 	} catch (error) {
 		console.error(error);
