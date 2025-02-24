9a6d23775cef39768071242657528ec52a456a90
proemian <brian@proemial.ai>
2025-02-24 05:39:57 +0100

    increase maxDuration to max

--- apps/assistant/src/app/api/inngest/route.ts
+++ apps/assistant/src/app/api/inngest/route.ts
@@ -3,6 +3,7 @@ import { workers, schedulers } from "../../../inngest/workers";
 import { serve } from "inngest/next";
 
 export const revalidate = 0;
+export const maxDuration = 300; // seconds
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
