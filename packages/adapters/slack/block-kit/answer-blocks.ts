56df449c6ab1b329f06f8470f977ee99310c0587
Michael Lajlev <lajlev@gmail.com>
2025-03-04 13:30:54 +0100

    Copywriting

--- packages/adapters/slack/block-kit/answer-blocks.ts
+++ packages/adapters/slack/block-kit/answer-blocks.ts
@@ -10,6 +10,54 @@ export function answer(text: string, url?: string, title?: string) {
 					},
 				],
 			},
+			{
+				type: "actions",
+				elements: [
+					{
+						type: "static_select",
+						placeholder: {
+							type: "plain_text",
+							text: "Ask about this",
+							emoji: true,
+						},
+						options: [
+							{
+								text: {
+									type: "plain_text",
+									text: "First followup questions",
+									emoji: true,
+								},
+								value: "value-0",
+							},
+							{
+								text: {
+									type: "plain_text",
+									text: "Second followup question",
+									emoji: true,
+								},
+								value: "value-1",
+							},
+							{
+								text: {
+									type: "plain_text",
+									text: "Third followup question",
+									emoji: true,
+								},
+								value: "value-2",
+							},
+							{
+								text: {
+									type: "plain_text",
+									text: "↔️ Tell me more",
+									emoji: true,
+								},
+								value: "value-3",
+							},
+						],
+						action_id: "static_select-action",
+					},
+				],
+			},
 		],
 	};
 }
