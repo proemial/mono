0ddf4c74cc244c4e562298144b3f5b229c10b1a7
Michael Lajlev <lajlev@gmail.com>
2025-03-04 13:23:57 +0100

    Followups slack block kit syntax added to links

--- packages/adapters/slack/block-kit/link-blocks.ts
+++ packages/adapters/slack/block-kit/link-blocks.ts
@@ -29,6 +29,54 @@ export function link(text: string, url?: string, title?: string) {
 							},
 						],
 					},
+					{
+						type: "actions",
+						elements: [
+							{
+								type: "static_select",
+								placeholder: {
+									type: "plain_text",
+									text: "Ask question",
+									emoji: true,
+								},
+								options: [
+									{
+										text: {
+											type: "plain_text",
+											text: "First followup questions",
+											emoji: true,
+										},
+										value: "value-0",
+									},
+									{
+										text: {
+											type: "plain_text",
+											text: "Second followup question",
+											emoji: true,
+										},
+										value: "value-1",
+									},
+									{
+										text: {
+											type: "plain_text",
+											text: "Third followup question",
+											emoji: true,
+										},
+										value: "value-2",
+									},
+									{
+										text: {
+											type: "plain_text",
+											text: "↔️ Tell me more",
+											emoji: true,
+										},
+										value: "value-3",
+									},
+								],
+								action_id: "static_select-action",
+							},
+						],
+					},
 				],
 			},
 		],
