dbace8145db0999b3d71360b974cefadb2ee9863
Michael Lajlev <lajlev@gmail.com>
2025-03-06 08:39:35 +0100

    Welcome page v0.2

--- apps/assistant/src/app/(pages)/slack/welcome/[teamid]/page.tsx
+++ apps/assistant/src/app/(pages)/slack/welcome/[teamid]/page.tsx
@@ -1,44 +1,63 @@
-import checkmark from "./checkmark.svg";
-import proem from "./logo.svg";
+import iconBulb from "./iconBulb.svg";
+import logos from "./logos.svg";
 import Image from "next/image";
+import { Lato } from "next/font/google";
+
+// Initialize the Lato font with the weights we need
+const lato = Lato({
+	weight: ["400", "700", "900"],
+	subsets: ["latin"],
+	display: "swap",
+});
 
 export default function WelcomePage() {
 	return (
-		<div className="flex flex-col min-h-[780px] items-start relative bg-black">
-			<div className="flex flex-col items-start relative self-stretch w-full flex-[0_0_auto] bg-[#020d07]">
-				<div className="flex flex-col items-start relative self-stretch w-full flex-[0_0_auto] bg-[#020d07]">
-					<div className="flex items-center justify-start pt-6 pb-1 px-4 relative self-stretch w-full flex-[0_0_auto]">
-						<Image
-							className="relative w-9 h-9"
-							alt="Proem"
-							src={proem.src}
-							width={36}
-							height={36}
-						/>
-					</div>
+		<div
+			className={`flex items-center justify-center min-h-screen bg-white ${lato.className}`}
+		>
+			<div className="w-[460px] flex flex-col items-center gap-12 p-6">
+				<Image
+					alt="Proem and Slack Logo"
+					src={logos.src}
+					width={265}
+					height={36}
+				/>
+				<h1 className="text-5xl font-black text-zinc-900 text-center font-sans">
+					Success!
+				</h1>
+
+				<div className="text-center flex gap-6 flex-col">
+					<p className="text-lg text-zinc-700">
+						<span className="font-black">@proem</span> has joined your Slack
+						workspace.
+					</p>
+
+					<p className="text-lg text-zinc-700">
+						<span className="font-bold">@proem</span> will join in and write
+						crisp summaries of the links and files you share, and will answer
+						any question when you message it.
+					</p>
 				</div>
 
-				<div className="flex flex-wrap items-start gap-[16px_16px] p-4 relative self-stretch w-full flex-[0_0_auto]">
-					<div className="flex flex-col min-w-[140px] min-h-[164px] items-start gap-3 p-4 relative flex-1 grow bg-[#b7e0ca1f] rounded-2xl overflow-hidden">
-						<Image
-							className="relative w-6 h-6"
-							alt="Checkmark"
-							src={checkmark.src}
-							width={24}
-							height={24}
-						/>
+				<a
+					href="slack://open"
+					className="w-full py-2.5 px-16 bg-fuchsia-950 rounded-xl text-white text-lg font-bold text-center inline-block"
+				>
+					Return to Slack
+				</a>
 
-						<div className="flex flex-col items-start gap-1 relative self-stretch w-full flex-[0_0_auto]">
-							<div className="relative self-stretch mt-[-1.00px] [font-family:'Outfit-SemiBold',Helvetica] font-semibold text-[#d7e4dd] text-[15px] tracking-[0] leading-[20.3px] overflow-hidden text-ellipsis [display:-webkit-box] [-webkit-line-clamp:3] [-webkit-box-orient:vertical]">
-								Personal Summaries activated
-							</div>
-
-							<p className="relative self-stretch [font-family:'Outfit-Regular',Helvetica] font-normal text-[#d0ebdc9e] text-[15px] tracking-[0] leading-[20.3px] overflow-hidden text-ellipsis [display:-webkit-box] [-webkit-line-clamp:7] [-webkit-box-orient:vertical]">
-								Personal Summaries is now active and working seamlessly in Slack
-								channels where @proem is added. You can now close the window.
-							</p>
-						</div>
-					</div>
+				<div className="flex items-center gap-3 p-3 bg-zinc-900/5 rounded-lg w-full">
+					<Image
+						className="relative w-[19px] h-[18px]"
+						alt="Icon Bulb"
+						src={iconBulb.src}
+						width={19}
+						height={18}
+					/>
+					<p className="text-zinc-600 text-base">
+						Tag <span className="font-bold">@proem</span> in a channel to add it
+						to your conversation.
+					</p>
 				</div>
 			</div>
 		</div>
