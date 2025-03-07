9285953f5d8eb20988d7a1d90dfaa9b7d86e4156
proemian <brian@proemial.ai>
2025-03-07 16:04:23 +0100

    update install link

--- apps/assistant/src/app/(pages)/slack/install/route.tsx
+++ apps/assistant/src/app/(pages)/slack/install/route.tsx
@@ -2,6 +2,6 @@ import { redirect } from "next/navigation";
 
 export async function GET() {
 	return redirect(
-		"https://slack.com/oauth/v2/authorize?client_id=5345137174018.8353049907607&scope=app_mentions:read,assistant:write,channels:history,channels:read,chat:write,files:read,groups:history,groups:read,groups:write,im:history,im:read,im:write,links:read,links:write,mpim:history,reactions:read,bookmarks:read,users:read,team:read,reactions:write,metadata.message:read&user_scope=",
+		"https://slack.com/oauth/v2/authorize?client_id=5345137174018.8353049907607&scope=app_mentions:read,assistant:write,bookmarks:read,channels:history,channels:read,chat:write,files:read,groups:history,groups:read,groups:write,im:history,im:read,im:write,links:read,links:write,metadata.message:read,mpim:history,reactions:read,reactions:write,team:read,users:read,chat:write.customize&user_scope=",
 	);
 }
