1cb940f5df86e3eadb7c200c3751b5d992b9df19
proemian <brian@proemial.ai>
2025-03-27 19:32:48 +0100

    add caching and increase timeout

--- apps/assistant/src/app/api/helpers/[teamid]/user/[userid]/digest/route.ts
+++ apps/assistant/src/app/api/helpers/[teamid]/user/[userid]/digest/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 import channels from "./channels.json";
 import { getDigest } from "../../../channel/[channelId]/digest/channel-digest";
 
-export const revalidate = 0;
+export const maxDuration = 300; // seconds
 
 export async function GET(
 	request: Request,
@@ -13,19 +13,24 @@ export async function GET(
 	const { teamid, userid } = params;
 
 	const feeds = await Promise.all(
-		channels.map(async (channel) =>
-			getDigest({
-				appId: "A08AD1FSPHV",
-				teamId: teamid,
-				channelId: channel.id,
-			}),
-		),
+		channels.map(async (channel) => {
+			try {
+				return getDigest({
+					appId: "A08AD1FSPHV",
+					teamId: teamid,
+					channelId: channel.id,
+				});
+			} catch (error) {
+				console.error(error);
+				return null;
+			}
+		}),
 	);
 
 	const mappedFeeds = feeds
-		.filter((f) => f.annotations.length > 0)
+		.filter((f) => f?.annotations.length)
 		.flatMap((feed) =>
-			feed.annotations.map((annotation) => ({
+			feed?.annotations.map((annotation) => ({
 				channel: feed.channel,
 				createdAt: annotation.createdAt.toISOString(),
 				message: annotation.message,
