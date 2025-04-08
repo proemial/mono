7d3b40d7885ada8edfed85fbc887af682c3a2e13
Jon Sorensen <jon@proemial.ai>
2025-04-08 12:31:00 +0200

    news: use scrapfly instead of diffbot

--- apps/proem/app/api/news/annotate/scrape/steps/article.ts
+++ apps/proem/app/api/news/annotate/scrape/steps/article.ts
@@ -1,18 +1,10 @@
 import { NewsAnnotatorScrapeStep } from "@proemial/adapters/redis/news";
+import { scrapflyScraper } from "@proemial/adapters/scrapfly/scraper";
 
 export const parseArticle = async (
 	url: string,
 ): Promise<NewsAnnotatorScrapeStep> => {
-	const rawTranscript = await fetchUniversalArticleTranscript(url);
-
-	if (!rawTranscript?.objects?.length) {
-		console.error("[scrape] failed to fetch article transcript", rawTranscript);
-		throw new Error("[news][scrape] Scraping failed", { cause: url });
-	}
-
-	const { title, text, date, images } = rawTranscript.objects.at(
-		0,
-	) as ArticleTranscriptObject;
+	const { title, text, images } = await scrapflyScraper(url);
 
 	const transcript = text?.replaceAll("\n", " ");
 	const artworkUrl = images.at(0)?.url;
@@ -25,45 +17,6 @@ export const parseArticle = async (
 		transcript,
 		artworkUrl,
 		title,
-		date,
+		date: undefined,
 	};
-};
-
-type ArticleTranscriptPayload = {
-	objects: ArticleTranscriptObject[];
-};
-
-type ArticleTranscriptObject = {
-	title: string;
-	text: string;
-	date: string;
-	images: {
-		url: string;
-	}[];
-};
-
-const fetchUniversalArticleTranscript = async (
-	url: string,
-): Promise<ArticleTranscriptPayload> => {
-	console.log("[scrape] fetching article transcript", url);
-
-	try {
-		const result = await fetch(
-			`https://api.diffbot.com/v3/article?url=${encodeURIComponent(url)}&token=${process.env.DIFFBOT_API_TOKEN}`,
-			{
-				method: "GET",
-				headers: { accept: "application/json" },
-			},
-		);
-
-		return await result.json();
-	} catch (error) {
-		console.error("[scrape] failed to fetch article transcript", error);
-		throw new Error("[news][scrape] diffbot error", {
-			cause: {
-				url,
-				error,
-			},
-		});
-	}
 };
