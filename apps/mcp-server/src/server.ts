65bc84b2738d1d5ec4014af3ee0afc15ea95c3d6
proemian <brian@proemial.ai>
2025-04-25 08:36:25 +0200

    add a tool description

--- apps/mcp-server/src/server.ts
+++ apps/mcp-server/src/server.ts
@@ -9,30 +9,35 @@ const server = new McpServer({
 	version: "1.0.0",
 });
 
-server.tool("fetch-papers", { query: z.string() }, async ({ query }) => {
-	try {
-		const papers = await fetchPapers(query);
-		const formattedPapers: string = papers
-			.map((paper) => {
-				return `Title: ${paper.title}\nURL: ${paper.primary_location.landing_page_url}\n${paper.abstract}`;
-			})
-			.join("\n\n");
-		return {
-			content: [{ type: "text", text: formattedPapers }],
-			_meta: {
-				query,
-			},
-		};
-	} catch (error) {
-		return {
-			isError: true,
-			content: [{ type: "text", text: "Error fetching papers" }],
-			_meta: {
-				query,
-			},
-		};
-	}
-});
+server.tool(
+	"fetch-papers",
+	"Fetch scientific research papers matching the query",
+	{ query: z.string() },
+	async ({ query }) => {
+		try {
+			const papers = await fetchPapers(query);
+			const formattedPapers: string = papers
+				.map((paper) => {
+					return `Title: ${paper.title}\nURL: ${paper.primary_location.landing_page_url}\n${paper.abstract}`;
+				})
+				.join("\n\n");
+			return {
+				content: [{ type: "text", text: formattedPapers }],
+				_meta: {
+					query,
+				},
+			};
+		} catch (error) {
+			return {
+				isError: true,
+				content: [{ type: "text", text: "Error fetching papers" }],
+				_meta: {
+					query,
+				},
+			};
+		}
+	},
+);
 
 // Connect the MCP server to a transport - either STDIO or HTTP
 // runHTTPServer(server);
